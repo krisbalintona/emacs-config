a2d71d629565c4140523c7505f9e78262ffd35be
PreciousPudding <krisbalintona@gmail.com>
2020-10-31 19:45:06 -0500

    Regular update of Emacs files

---
+++ data/bookmark-default.el
@@ -0,0 +1,15 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: utf-8-emacs; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("org-capture-last-stored"
+ (filename . "~/Documents/org-database/agenda/inbox.org")
+ (front-context-string . "* TODO Fix compa")
+ (rear-context-string . "0-31 Sat 00:32]\n")
+ (position . 6159))
+("Org database"
+ (filename . "~/Documents/org-database/")
+ (front-context-string . "agenda\n  drwxr-x")
+ (rear-context-string . "96 Oct 30 02:54 ")
+ (position . 290))
+)
