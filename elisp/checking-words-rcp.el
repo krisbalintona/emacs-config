e9b3a4b65b578432942206e037cf72680283a3d7
Kristoffer Balintona <krisbalintona@PuddingMachine.localdomain>
2020-12-08 13:48:12 -0600

    Fixed indentation and typos in this rcp

--- elisp/checking-words-rcp.el
+++ elisp/checking-words-rcp.el
@@ -11,35 +11,36 @@
 ;; https://github.com/gromnitsky/wordnut
 (use-package define-word
   :config
-  (general-define-kye
+  (general-define-key
    "C-c d" '(define-word-at-point :which-key "Definition at point")
    "C-c D" '(define-word :which-key "Definition lookup")
    )
+  )
 
 ;;;;; Powerthesaurus
-  ;; Insert or search words in thesaurus. Offline version is
-  ;; https://github.com/hpdeifel/synosaurus
-  (use-package powerthesaurus
-    :config
-    (general-define-key
-     "C-c l" '(powerthesaurus-lookup-word-at-point :which-key "Thesaurus at point")
-     "C-c L" '(powerthesaurus-lookup-word :which-key "Thesuarus lookup")
-     )
-    )
+;; Insert or search words in thesaurus. Offline version is
+;; https://github.com/hpdeifel/synosaurus
+(use-package powerthesaurus
+  :config
+  (general-define-key
+   "C-c l" '(powerthesaurus-lookup-word-at-point :which-key "Thesaurus at point")
+   "C-c L" '(powerthesaurus-lookup-word :which-key "Thesuarus lookup")
+   )
+  )
 
 ;;;;; Google-translate
-  ;; Translate strings via Google Translate
-  (use-package google-translate
-    :config
-    (general-define-key
-     "C-c t" '(google-translate-smooth-translate :which-key "Google translate")
-     )
-    )
-  
+;; Translate strings via Google Translate
+(use-package google-translate
+  :config
+  (general-define-key
+   "C-c t" '(google-translate-smooth-translate :which-key "Google translate")
+   )
+  )
+
 ;;;;; Offline
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (provide 'checking-words-rcp)
+(provide 'checking-words-rcp)
 ;;; Commentary:
-  ;;
+;;
 ;;; checking-words-rcp.el ends here
