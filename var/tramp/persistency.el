2384dd8bf4c48e9e12bf71d59cc974a4d5b25c7f
Kristoffer Balintona <krisbalintona@localhost.localdomain>
2020-12-29 10:46:45 -0600

    Update files

--- var/tramp/persistency.el
+++ var/tramp/persistency.el
@@ -1,5 +1,22 @@
-;; -*- emacs-lisp -*- <20/12/26 05:56:41 /home/krisbalintona/main-emacs/var/tramp/persistency.el>
+;; -*- emacs-lisp -*- <20/12/28 10:49:51 /home/krisbalintona/main-emacs/var/tramp/persistency.el>
 ;; Tramp connection history.  Don't change this file.
 ;; Run `M-x tramp-cleanup-all-connections' instead.
 
-(nil)
+(((tramp-file-name "sudo" "root" nil "localhost.localdomain" nil nil nil)
+  ("readlink" "\\readlink")
+  ("null-device" "/dev/null")
+  ("uname" "Linux 5.9.16-200.fc33.x86_64")
+  ("locale" "LC_ALL=en_US.utf8")
+  ("test" "test")
+  ("remote-path"
+   ("/usr/bin" "/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin"))
+  ("pipe-buf" 4096)
+  ("remote-shell" "/bin/sh")
+  ("~root" "/root")
+  ("stat" "env QUOTING_STYLE=locale \\stat")
+  ("file-exists" "test -e")
+  ("id" "/usr/bin/id")
+  ("uid-string" "root")
+  ("gid-string" "root")
+  ("uid-integer" 0)
+  ("gid-integer" 0)))
