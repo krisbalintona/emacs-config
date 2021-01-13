a7a027cf6820026649b9d77455fe9f259316fc75
Kristoffer Balintona <krisbalintona@gmail.com>
2021-01-12 19:28:00 -0500

    Update emacs files

--- var/projectile/known-projects.el
+++ var/projectile/known-projects.el
@@ -1,1 +1,1 @@
-("~/main-emacs/var/" "~/main-emacs/transient/" "~/main-emacs/straight/" "~/main-emacs/snippets/" "~/main-emacs/request/" "~/main-emacs/elisp/" "~/main-emacs/data/" "~/main-emacs/configs/" "~/main-emacs/.git/")
+("~/main-emacs/var/" "~/main-emacs/transient/" "~/main-emacs/straight-old/" "~/main-emacs/straight/" "~/main-emacs/snippets/" "~/main-emacs/request/" "~/main-emacs/etc/" "~/main-emacs/elisp/" "~/main-emacs/data/" "~/main-emacs/configs/" "~/main-emacs/.git/")
