3f2929abee08d7e2b3f60918eb2504ddda104d34
Kristoffer Balintona <krisbalintona@gmail.com>
2021-07-30 02:44:31 -0400

    Update files

--- var/project-list.el
+++ var/project-list.el
@@ -1,5 +1,5 @@
 ;;; -*- lisp-data -*-
-(("~/ghost-master/content/themes/personal/")
- ("~/main-emacs/")
+(("~/main-emacs/")
  ("~/ghost-devel/content/themes/personal/")
+ ("~/ghost-master/content/themes/personal/")
  ("~/dotfiles/"))
