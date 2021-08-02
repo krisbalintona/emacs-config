a7dc81a74d55a1c93a0f7886841e0d2ea74a41aa
Kristoffer Balintona <krisbalintona@gmail.com>
2021-08-02 04:10:48 -0400

    Update files

--- var/transient/history.el
+++ var/transient/history.el
@@ -1,13 +1,20 @@
-((magit-branch nil)
+((magit-bisect nil)
+ (magit-branch nil)
  (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-gitignore nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
  (magit-merge nil)
  (magit-pull nil)
- (magit-push nil)
+ (magit-push nil
+             ("--dry-run"))
+ (magit-rebase nil)
  (magit-remote
   ("-f"))
  (magit-reset nil)
+ (magit-revert
+  ("--edit"))
  (magit-stash nil))
