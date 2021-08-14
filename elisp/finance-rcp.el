1c0249599759330bf9a077b10c60d145f10399ad
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-13 20:53:41 -0500

    Create finance-rcp

---
+++ elisp/finance-rcp.el
@@ -0,0 +1,17 @@
+;;; finance-rcp.el --- Summary
+;;
+;;; Commentary:
+;;
+;; Finance-related packages
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Code:
+
+;;;; Ledger-mode
+(use-package ledger-mode
+  :ensure-system-package (ledger . "sudo apt install ledger")
+  )
+
+;;; finance-rcp.el ends here
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'finance-rcp)
