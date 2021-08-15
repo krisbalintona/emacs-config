1337e471821097fd825bd1c3e9d2e7ac50b8ee89
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-15 15:17:17 -0500

    Redefine flycheck-ledger checker to use ledger-master-file

--- elisp/finance-rcp.el
+++ elisp/finance-rcp.el
@@ -41,6 +41,45 @@
   :custom
   (flycheck-ledger-explicit t) ; Check even cleared transactions
   (flycheck-ledger-pedantic t) ; Check account names
+  :config
+  ;; Redefine the checker to use `ledger-master-file' rather than
+  ;; `source-inplace' (current buffer)
+  (flycheck-define-checker ledger
+    "A checker for ledger files, showing unmatched balances and failed checks."
+    :command ("ledger"
+              (option-flag "--explicit" flycheck-ledger-explicit)
+              (option-flag "--pedantic" flycheck-ledger-pedantic)
+              (eval (when (eq flycheck-ledger-pedantic 'check-payees) "--check-payees"))
+
+              ;; "-f" source-inplace
+              "-f" (eval ledger-master-file)
+              ;; (setq-local ledger-master-file (concat no-littering-var-directory "ledger/2020.ledger"))
+
+              "balance"
+              ;; to find non-zero zero accounts:
+              "--flat" "--no-total"
+              "--balance-format" "%(scrub(display_total))\t\t%(account())\n"
+              (eval flycheck-ledger-zero-accounts))
+    :error-patterns
+    ((error line-start "While parsing file \"" (file-name) "\", line " line ":" (zero-or-more whitespace) "\n"
+            (zero-or-more line-start (or "While " "> ") (one-or-more not-newline) "\n" )
+            (message (minimal-match (zero-or-more line-start (zero-or-more not-newline) "\n"))
+                     "Error: " (one-or-more not-newline) "\n")))
+    :error-parser
+    (lambda (output checker buffer)
+      (let ((pattern-errors (flycheck-parse-with-patterns output checker buffer)))
+        (or pattern-errors
+            (when (> (length flycheck-ledger-zero-accounts) 0)
+              (flycheck-ledger--zero-error-parser output checker buffer)))))
+    :verify
+    (lambda (checker)
+      (let ((has-accounts (> (length flycheck-ledger-zero-accounts) 0)))
+        (list
+         (flycheck-verification-result-new
+          :label "accounts"
+          :message (if has-accounts (format "%s" flycheck-ledger-zero-accounts) "none")
+          :face 'success))))
+    :modes ledger-mode)
   )
 
 ;;; finance-rcp.el ends here
