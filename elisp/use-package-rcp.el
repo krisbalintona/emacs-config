d4eb837807ebea2269cd585ce69758f7606ac9e5
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-15 21:28:47 -0500

    Fixed use-package-rcp and straight-package-management-rcp

--- elisp/use-package-rcp.el
+++ elisp/use-package-rcp.el
@@ -2,39 +2,22 @@
 ;;
 ;;; Commentary:
 ;;
-;; Install use-package, set it up, and load repositories
+;; Install use-package, set it up, and load repositories.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
-
-;;;; Install use-package
 (require 'straight)
+
+;;;; Use-package
 (straight-use-package 'use-package)
+(require 'use-package)
+;; (setq use-package-always-ensure t) ; May cause issues with straight.el
+(setq use-package-expand-minimally t) ; Less verbose
+;; (setq use-package-compute-statistics t) ; Need this at "loadup time" or else errors about undefined variables will appear
 
-;;;; Settings
-(use-package use-package
-  :custom
-  ;; (setq use-package-always-ensure t) ; May cause issues with straight.el
-  (use-package-expand-minimally t) ; Less verbose
-  ;; (setq use-package-compute-statistics t) ; Need this at "loadup time" or else errors about undefined variables will appear
-  (use-package-enable-imenu-support t)
-
-  ;; Set use-package-verbose to t for interpreted .emacs, and to nil for
-  ;; byte-compiled .emacs.elc.
-  (use-package-verbose (not (bound-and-true-p byte-compile-current-file))))
-
-;;;; Load repos
-(require 'package)
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ;; ("org" . "http://orgmode.org/elpa/")
-        ;; ("gnu"   . "https://elpa.gnu.org/packages/")
-        ("cselpa" . "https://elpa.thecybershadow.net/packages/")
-        ;; ;; Chinese servers
-        ;; ("melpa-cn" . "http://mirrors.cloud.tencent.com/elpa/melpa/")
-        ;; ("gnu-cn"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")
-        ))
+;; Set use-package-verbose to t for interpreted .emacs, and to nil for
+;; byte-compiled .emacs.elc.
+(use-package-verbose (not (bound-and-true-p byte-compile-current-file)))
 
 ;;; use-package-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
