4afb1665c41a05d36da774c924ffe051b71318a5
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-15 22:07:13 -0500

    squash! Fixed use-package-rcp and straight-package-management-rcp

--- elisp/use-package-rcp.el
+++ elisp/use-package-rcp.el
@@ -6,18 +6,17 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
-(require 'straight)
+(require 'straight-package-management-rcp)
 
 ;;;; Use-package
 (straight-use-package 'use-package)
-(require 'use-package)
 ;; (setq use-package-always-ensure t) ; May cause issues with straight.el
 (setq use-package-expand-minimally t) ; Less verbose
 ;; (setq use-package-compute-statistics t) ; Need this at "loadup time" or else errors about undefined variables will appear
 
 ;; Set use-package-verbose to t for interpreted .emacs, and to nil for
 ;; byte-compiled .emacs.elc.
-(use-package-verbose (not (bound-and-true-p byte-compile-current-file)))
+(setq use-package-verbose (not (bound-and-true-p byte-compile-current-file)))
 
 ;;; use-package-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
