8dffc9dcb39ad628a4f89a293c0a1a81e9db869c
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-18 05:28:47 -0500

    Move trash directory

--- elisp/better-defaults-rcp.el
+++ elisp/better-defaults-rcp.el
@@ -7,6 +7,8 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
+(require 'use-package-rcp)
+(require 'early-packages-rcp)
 
 ;;;; Remove unnecessary UI
 (menu-bar-mode -1)
@@ -28,8 +30,8 @@
               cursor-in-non-selected-windows nil                                                  ; Hide the cursor in inactive windows
               mouse-yank-at-point t                                                               ; Yank at point rather than pointer
 
+              trash-directory (concat no-littering-var-directory "trash")                         ; Trash directory
               delete-by-moving-to-trash t                                                         ; Delete files to trash
-              trash-directory (concat user-emacs-directory "trash")                               ; Trash directory
 
               find-file-visit-truename t                                                          ; Follow symlink to actual file
 
