6156425051b0f1e469a396c6d7c8ac105eab71fe
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-18 05:47:47 -0500

    Remove /var/ directory

--- var/projectile/known-projects.el
+++
@@ -1,1 +0,0 @@
-("~/main-emacs/var/" "~/main-emacs/transient/" "~/main-emacs/straight/" "~/main-emacs/request/" "~/main-emacs/etc/" "~/main-emacs/elisp/" "~/main-emacs/data/" "~/main-emacs/configs/" "~/main-emacs/.git/")
