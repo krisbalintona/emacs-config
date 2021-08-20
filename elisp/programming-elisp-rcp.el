9692f278639befada853935cec3ff62ff9cb3250
Kristoffer Balintona <krisbalintona+windows10@gmail.com>
2021-08-19 19:20:31 -0500

    Fixed programming-elisp-rcp

--- elisp/programming-elisp-rcp.el
+++ elisp/programming-elisp-rcp.el
@@ -2,23 +2,28 @@
 ;;
 ;;; Commentary:
 ;;
-;; These are packages that are helpful for programming or working in elisp
+;; These are packages that are helpful for programming or working in elisp.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
 (require 'use-package-rcp)
 (require 'keybinds-frameworks-rcp)
+(require 'buffer-and-window-management-rcp)
 
 ;;;; Elisp-mode
 ;; Elisp-mode overwrites my eyebrowse-last-window-config binding
 (use-package elisp-mode
   :straight nil
-  :general (:keymaps 'emacs-lisp-mode-map
-                     :states '(normal visual motion)
-                     "gz" nil)
   :general
   (:keymaps 'emacs-lisp-mode-map
-            "C-x n s" 'outshine-narrow-to-subtree)
+            :states '(normal visual motion)
+            "gz" nil)
+  )
+
+;;;; Eros-mode
+;; Overlay lisp evaluations into the current buffer (near cursor)
+(use-package eros
+  :ghook 'emacs-lisp-mode-hook
   )
 
 ;;;; Syntax highlighting
@@ -38,12 +43,6 @@
 ;; Highlight matching delimiters (e.g. parenthesis)
 (use-package rainbow-delimiters
   :ghook 'prog-mode-hook
-  )
-
-;;;;; Eros
-;; Overlay lisp evaluations into the current buffer (near cursor)
-(use-package eros
-  :ghook 'emacs-lisp-mode-hook
   )
 
 ;;; programming-elisp-rcp.el ends here
