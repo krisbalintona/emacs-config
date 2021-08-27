fa3b2f1c70b86a8f87711d002e5ac86fc3bf5f98
Kristoffer Balintona <krisbalintona@gmail.com>
2021-08-26 19:24:21 -0500

    Trying out vertico

--- elisp/completion-default-rcp.el
+++ elisp/completion-default-rcp.el
@@ -57,7 +57,9 @@
   ;; https://github.com/minad/vertico#org-refile
   (org-refile-use-outline-path 'file)
   (org-outline-path-complete-in-steps nil)
-  :config
+  :custom-face
+  (vertico-current ((t (:background "#3a3f5a"))))
+  :init
   ;; Workaround for problem with `tramp' hostname completions. This overrides
   ;; the completion style specifically for remote files! See
   ;; https://github.com/minad/vertico#tramp-hostname-completion
