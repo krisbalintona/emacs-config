9b1a7d64a4242f90db9ab0ffae7e14b9edfabdaf
Kristoffer Balintona <krisbalintona@gmail.com>
2021-08-28 13:50:46 -0500

    Move heaven-and-hell use-package position

--- elisp/themes-rcp.el
+++ elisp/themes-rcp.el
@@ -44,6 +44,70 @@
 
 (use-package apropospriate-theme)
 
+;;;; Heaven-and-hell
+;; Toggle between light and dark themes
+(use-package heaven-and-hell
+  :hook ((after-init . heaven-and-hell-init-hook)
+         (window-configuration-change . kb/theme-faces)
+         )
+  :general
+  ("<f6>" '((lambda ()
+              (interactive)
+              (heaven-and-hell-toggle-theme)
+              (highlight-indent-guides-auto-set-faces)
+              (kb/doom-modeline-font-setup)
+              (kb/theme-faces)
+              )
+            :which-key "Toggle theme"
+            ))
+  :init
+  (setq custom--inhibit-theme-enable nil)
+  :config
+  (setq heaven-and-hell-theme-type 'dark) ; Use dark by default
+  (setq heaven-and-hell-themes ;; Themes can be the list: (dark . (tsdh-dark wombat))
+        '((dark . atom-one-dark)
+          (light . apropospriate-light))
+        )
+
+  ;; Load themes without asking for confirmation
+  (setq heaven-and-hell-load-theme-no-confirm t)
+
+  ;; Cleanly load themes
+  (heaven-and-hell-clean-load-themes '(atom-one-dark apropospriate-light))
+  )
+
+;;;;; Set faces based on theme
+(defun kb/theme-faces ()
+  "Set light and dark theme faces."
+  (interactive)
+  (defvar heaven-and-hell-themes)
+  (require 'heaven-and-hell)
+  (custom-theme-set-faces ; Dark theme
+   (cdr (car (cdr heaven-and-hell-themes)))
+   `(org-level-1 ((t (:inherit outline-1 :height 210 :font ,kb/variable-pitch-font))) t)
+   `(org-level-2 ((t (:inherit outline-2 :height 198 :font ,kb/variable-pitch-font))) t)
+   `(org-level-3 ((t (:inherit outline-3 :height 185 :font ,kb/variable-pitch-font))) t)
+   `(org-level-4 ((t (:inherit outline-4 :height 170 :foreground "medium aquamarine" :font ,kb/variable-pitch-font))) t)
+   `(org-level-5 ((t (:inherit outline-5 :height 165 :foreground "light sea green" :font ,kb/variable-pitch-font))) t)
+
+   `(org-block ((t (:foreground nil :inherit fixed-pitch :background "#232635" :extend t))) t)
+   `(org-quote ((t (:inherit org-block :height 143))) t)
+   `(org-code ((t (:inherit (shadow fixed-pitch)))) t)
+   `(org-verbatim ((t (:inherit (shadow fixed-pitch)))) t)
+   `(org-table ((t (:inherit (shadow fixed-pitch)))) t)
+   `(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))) t)
+   `(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))) t)
+   `(org-checkbox ((t (:inherit fixed-pitch))) t)
+   `(org-tag ((t (:height 153 :bold t :italic t))) t)
+   `(org-document-title ((t (:bold t :height 1.7 :foreground "goldenrod"))) nil)
+   `(org-link ((t (:foreground "goldenrod3" :bold nil :italic t :font ,kb/variable-pitch-font :height 145 :underline nil))))
+   `(bookmark-face ((t (:foreground nil :background nil))))
+   )
+  ;; (custom-theme-set-faces ; Light theme
+  ;;  (cdr (car heaven-and-hell-themes))
+  ;;  )
+  )
+
 ;;;; Modeline
 ;;;;; Doom-modeline
 ;; Sleek modeline from Doom Emacs
@@ -265,70 +329,6 @@ UTF-8."
   (unless (equal "Battery status not available"
                  (battery))
     (display-battery-mode t)) ; Show battery in modeline
-  )
-
-;;;; Heaven-and-hell
-;; Toggle between light and dark themes
-(use-package heaven-and-hell
-  :hook ((after-init . heaven-and-hell-init-hook)
-         (window-configuration-change . kb/theme-faces)
-         )
-  :general
-  ("<f6>" '((lambda ()
-              (interactive)
-              (heaven-and-hell-toggle-theme)
-              (highlight-indent-guides-auto-set-faces)
-              (kb/doom-modeline-font-setup)
-              (kb/theme-faces)
-              )
-            :which-key "Toggle theme"
-            ))
-  :init
-  (setq custom--inhibit-theme-enable nil)
-  :config
-  (setq heaven-and-hell-theme-type 'dark) ; Use dark by default
-  (setq heaven-and-hell-themes ;; Themes can be the list: (dark . (tsdh-dark wombat))
-        '((dark . atom-one-dark)
-          (light . apropospriate-light))
-        )
-
-  ;; Load themes without asking for confirmation
-  (setq heaven-and-hell-load-theme-no-confirm t)
-
-  ;; Cleanly load themes
-  (heaven-and-hell-clean-load-themes '(atom-one-dark apropospriate-light))
-  )
-
-;;;;; Set faces based on theme
-(defun kb/theme-faces ()
-  "Set light and dark theme faces."
-  (interactive)
-  (defvar heaven-and-hell-themes)
-  (require 'heaven-and-hell)
-  (custom-theme-set-faces ; Dark theme
-   (cdr (car (cdr heaven-and-hell-themes)))
-   `(org-level-1 ((t (:inherit outline-1 :height 210 :font ,kb/variable-pitch-font))) t)
-   `(org-level-2 ((t (:inherit outline-2 :height 198 :font ,kb/variable-pitch-font))) t)
-   `(org-level-3 ((t (:inherit outline-3 :height 185 :font ,kb/variable-pitch-font))) t)
-   `(org-level-4 ((t (:inherit outline-4 :height 170 :foreground "medium aquamarine" :font ,kb/variable-pitch-font))) t)
-   `(org-level-5 ((t (:inherit outline-5 :height 165 :foreground "light sea green" :font ,kb/variable-pitch-font))) t)
-
-   `(org-block ((t (:foreground nil :inherit fixed-pitch :background "#232635" :extend t))) t)
-   `(org-quote ((t (:inherit org-block :height 143))) t)
-   `(org-code ((t (:inherit (shadow fixed-pitch)))) t)
-   `(org-verbatim ((t (:inherit (shadow fixed-pitch)))) t)
-   `(org-table ((t (:inherit (shadow fixed-pitch)))) t)
-   `(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))) t)
-   `(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))) t)
-   `(org-checkbox ((t (:inherit fixed-pitch))) t)
-   `(org-tag ((t (:height 153 :bold t :italic t))) t)
-   `(org-document-title ((t (:bold t :height 1.7 :foreground "goldenrod"))) nil)
-   `(org-link ((t (:foreground "goldenrod3" :bold nil :italic t :font ,kb/variable-pitch-font :height 145 :underline nil))))
-   `(bookmark-face ((t (:foreground nil :background nil))))
-   )
-  ;; (custom-theme-set-faces ; Light theme
-  ;;  (cdr (car heaven-and-hell-themes))
-  ;;  )
   )
 
 ;;;; Buffer display
