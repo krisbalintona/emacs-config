2a6247bcc96fe73966e642f1f95a9b7a98aaf93b
Kristoffer Balintona <krisbalintona@gmail.com>
2021-08-27 20:08:45 -0500

    Add FIXME note for evil getting in the way of shells

--- elisp/evil-rcp.el
+++ elisp/evil-rcp.el
@@ -3,6 +3,7 @@
 ;;; Commentary:
 ;;
 ;; Evil packages!
+;; FIXME 2021-08-27: For some reason, something relevant to `evil' or is getting in the way of `comint-mode' and `eshell-mode'.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
