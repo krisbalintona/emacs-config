2f598bb868b114f4bd5d92b94934f3ac72a46163
Kristoffer Balintona <krisbalintona@gmail.com>
2021-09-02 10:55:05 -0400

    Move my custom doom-modeline-segments into its own file

---
+++ elisp/kb-doom-modeline-segments.el
@@ -0,0 +1,152 @@
+;;; kb-doom-modeline-segments.el --- Summary
+;;
+;;; Commentary:
+;;
+;; Where I define my own Doom modeline segments and modelines.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Code:
+(require 'doom-modeline)
+
+;;; Buffer-info
+(doom-modeline-def-segment kb/buffer-info
+  "The standard `buffer-info' but without the 'unsaved' icon and major mode
+icon."
+  (concat
+   (doom-modeline-spc)
+   (doom-modeline--buffer-name)
+   ))
+
+;;; Major-mode-icon
+(doom-modeline-def-segment kb/major-mode-icon
+  "Simply the major-mode icon."
+  (doom-modeline--buffer-mode-icon)
+  )
+
+;;; Matches
+(doom-modeline-def-segment kb/matches
+  "The standard `matches' segment but doesn't show buffer size (if unused)."
+  (let ((meta (concat (doom-modeline--macro-recording)
+                      (doom-modeline--anzu)
+                      (doom-modeline--phi-search)
+                      (doom-modeline--evil-substitute)
+                      (doom-modeline--iedit)
+                      (doom-modeline--symbol-overlay)
+                      (doom-modeline--multiple-cursors))))
+    (or (and (not (equal meta "")) meta)
+        ""))
+  )
+
+;;; VCS
+(doom-modeline-def-segment kb/vcs
+  "Standard `vcs' but don't show branch if it's 'master'."
+  (let ((active (doom-modeline--active)))
+    (when-let ((icon doom-modeline--vcs-icon)
+               (text doom-modeline--vcs-text))
+      (concat
+       (doom-modeline-spc)
+       (propertize
+        (concat
+         (if active
+             icon
+           (doom-modeline-propertize-icon icon 'mode-line-inactive))
+         (doom-modeline-vspc))
+        'mouse-face 'mode-line-highlight
+        'help-echo (get-text-property 1 'help-echo vc-mode)
+        'local-map (get-text-property 1 'local-map vc-mode))
+
+       ;; If the current branch is the main one, then don't show in modeline
+       (if (string= (substring-no-properties (substring vc-mode (+ (if (eq (vc-backend buffer-file-name) 'Hg) 2 3) 2))) (magit-main-branch))
+           ""
+         text)
+       (doom-modeline-spc))
+      )))
+
+;;; Eyebrowse
+(doom-modeline-def-segment kb/eyebrowse
+  "Show eyebrowse workspace information."
+  (when (and (doom-modeline--active)
+             (not doom-modeline--limited-width-p))
+    '((eyebrowse-mode (:eval (eyebrowse-mode-line-indicator))))
+    ))
+
+;;; Time
+(doom-modeline-def-segment kb/time
+  "Display time."
+  (when (and (doom-modeline--active)
+             (not doom-modeline--limited-width-p))
+    '(("" display-time-string)
+      " ")
+    ))
+
+;;; Mu4e
+(doom-modeline-def-segment kb/mu4e
+  "Display mu4e mail. Require `mu4e-alert'."
+  (when (and (doom-modeline--active)
+             (not doom-modeline--limited-width-p))
+    '((:eval mu4e-alert-mode-line))
+    ))
+
+;;; Buffer-encoding
+(doom-modeline-def-segment kb/buffer-encoding
+  "Standard `buffer-encoding' but don't show encoding on modeline if it is
+UTF-8."
+  (when doom-modeline-buffer-encoding
+    (let ((face (if (doom-modeline--active) 'mode-line 'mode-line-inactive))
+          (mouse-face 'mode-line-highlight))
+      (concat
+       (doom-modeline-spc)
+
+       ;; Check for UTF-8. If so then do these 2 propertize sections.If not,
+       ;; then nothing is propertized and thus shown.
+       (if (or (eq buffer-file-coding-system 'utf-8-unix)
+               (eq buffer-file-coding-system 'utf-8)
+               (eq buffer-file-coding-system 'prefer-utf-8-unix)
+               (eq buffer-file-coding-system 'undecided-unix) ; Not sure what this is but it appears in my org-roam files
+               )
+           nil
+
+         ;; coding system
+         (let* ((sys (coding-system-plist buffer-file-coding-system))
+                (cat (plist-get sys :category))
+                (sym (if (memq cat
+                               '(coding-category-undecided coding-category-utf-8))
+                         'failure
+                       (plist-get sys :name))))
+           (when (or (eq doom-modeline-buffer-encoding t)
+                     (and (eq doom-modeline-buffer-encoding 'nondefault)
+                          (not (eq cat 'coding-category-undecided))
+                          (not (eq sym doom-modeline-default-coding-system))))
+             (propertize
+              (upcase (symbol-name sym))
+              'face face
+              'mouse-face mouse-face
+              'help-echo 'mode-line-mule-info-help-echo
+              'local-map mode-line-coding-system-map)))
+         )
+
+       (doom-modeline-spc)))))
+
+;;; Buffer-default-directory
+(doom-modeline-def-segment kb/buffer-default-directory
+  "Standard `buffer-default-directory' without the state, icon, and color change."
+  (let* ((active (doom-modeline--active))
+         (face (if active 'bold-italic 'mode-line-inactive))) ; Don't use here
+    (concat (doom-modeline-spc)
+            (propertize (cond ((stringp (vc-git-root (buffer-file-name)))
+                               (abbreviate-file-name (vc-git-root (buffer-file-name))))
+                              (buffer-file-name ; Connected to file?
+                               default-directory)
+                              (t "Not file!"))
+                        'face face)
+            (doom-modeline-spc))))
+
+;;; Modeline definitions
+;;;; Main
+(doom-modeline-def-modeline 'main
+  '(kb/matches "   " kb/major-mode-icon "  " bar "  " kb/eyebrowse kb/vcs kb/buffer-default-directory kb/buffer-info remote-host buffer-position " " selection-info)
+  '(input-method process debug kb/time battery " " bar " " kb/buffer-encoding checker))
+
+;;; kb-doom-modeline-segments.el ends here
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'kb-doom-modeline-segments)
--- recipes/themes-rcp.el
+++ recipes/themes-rcp.el
@@ -89,14 +89,13 @@ Additionally, run `kb/themes-hooks'."
 ;; Sleek modeline from Doom Emacs
 (use-package doom-modeline
   :hook (window-configuration-change . doom-modeline-refresh-font-width-cache) ; Prevent modeline from being cut off
-  :ghook 'server-after-make-frame-hook 'window-setup-hook
-  :gfhook 'kb/set-doom-modeline-segments
+  :ghook 'emacs-startup-hook
   :custom
   ;; Modeline settings
   (doom-modeline-window-width-limit fill-column) ; The limit of the window width.
   (doom-modeline-project-detection 'project)
   (doom-modeline-buffer-file-name-style 'buffer-name)
-  (doom-modeline-icon (or(display-graphic-p) (server-running-p)) ; Show icons if in Emacs GUI or server
+  (doom-modeline-icon (or(display-graphic-p) (server-running-p))) ; Show icons if in Emacs GUI or server
   (doom-modeline-major-mode-icon t)
   (doom-modeline-major-mode-color-icon t)
   (doom-modeline-buffer-state-icon t)
@@ -113,172 +112,10 @@ Additionally, run `kb/themes-hooks'."
   (doom-modeline-height 33)
   (doom-modeline-bar-width 2) ; Width (in number of columns) of window until information (on the right) starts to disappear
   (doom-modeline-window-width-limit 100) ; Width of the bar segment
+  :config (require 'kb-doom-modeline-segments)
   )
 
-;;;;; Modeline segments
-;; (Re)defining my own modeline segments
-(defun kb/set-doom-modeline-segments ()
-  "Define relevant doom modeline segments and define segment."
-  (require 'doom-modeline-segments)
-
-  (doom-modeline-def-segment kb/buffer-info
-    "The standard `buffer-info' but without the 'unsaved' icon and major mode
-icon."
-    (concat
-     (doom-modeline-spc)
-     (doom-modeline--buffer-name)
-     ))
-  (doom-modeline-def-segment kb/major-mode-icon
-    "Simply the major-mode icon."
-    (doom-modeline--buffer-mode-icon)
-    )
-  (doom-modeline-def-segment kb/matches
-    "The standard `matches' segment but doesn't show buffer size (if unused)."
-    (let ((meta (concat (doom-modeline--macro-recording)
-                        (doom-modeline--anzu)
-                        (doom-modeline--phi-search)
-                        (doom-modeline--evil-substitute)
-                        (doom-modeline--iedit)
-                        (doom-modeline--symbol-overlay)
-                        (doom-modeline--multiple-cursors))))
-      (or (and (not (equal meta "")) meta)
-          ""))
-    )
-  (doom-modeline-def-segment kb/vcs
-    "Standard `vcs' but don't show branch if it's 'master'."
-    (let ((active (doom-modeline--active)))
-      (when-let ((icon doom-modeline--vcs-icon)
-                 (text doom-modeline--vcs-text))
-        (concat
-         (doom-modeline-spc)
-         (propertize
-          (concat
-           (if active
-               icon
-             (doom-modeline-propertize-icon icon 'mode-line-inactive))
-           (doom-modeline-vspc))
-          'mouse-face 'mode-line-highlight
-          'help-echo (get-text-property 1 'help-echo vc-mode)
-          'local-map (get-text-property 1 'local-map vc-mode))
-
-         ;; If the current branch is the main one, then don't show in modeline
-         (if (string= (substring-no-properties (substring vc-mode (+ (if (eq (vc-backend buffer-file-name) 'Hg) 2 3) 2))) (magit-main-branch))
-             ""
-           text)
-         (doom-modeline-spc))
-        )))
-  (doom-modeline-def-segment kb/eyebrowse
-    "Show eyebrowse workspace information."
-    (when (and (doom-modeline--active)
-               (not doom-modeline--limited-width-p))
-      '((eyebrowse-mode (:eval (eyebrowse-mode-line-indicator))))
-      ))
-  (doom-modeline-def-segment kb/time
-    "Display time."
-    (when (and (doom-modeline--active)
-               (not doom-modeline--limited-width-p))
-      '(("" display-time-string)
-        " ")
-      ))
-  (doom-modeline-def-segment kb/mu4e
-    "Display mu4e mail. Require `mu4e-alert'."
-    (when (and (doom-modeline--active)
-               (not doom-modeline--limited-width-p))
-      '((:eval mu4e-alert-mode-line))
-      ))
-  (doom-modeline-def-segment kb/buffer-encoding
-    "Standard `buffer-encoding' but don't show encoding on modeline if it is
-UTF-8."
-    (when doom-modeline-buffer-encoding
-      (let ((face (if (doom-modeline--active) 'mode-line 'mode-line-inactive))
-            (mouse-face 'mode-line-highlight))
-        (concat
-         (doom-modeline-spc)
-
-         ;; Check for UTF-8. If so then do these 2 propertize sections.If not,
-         ;; then nothing is propertized and thus shown.
-         (if (or (eq buffer-file-coding-system 'utf-8-unix)
-                 (eq buffer-file-coding-system 'utf-8)
-                 (eq buffer-file-coding-system 'prefer-utf-8-unix)
-                 (eq buffer-file-coding-system 'undecided-unix) ; Not sure what this is but it appears in my org-roam files
-                 )
-             nil
-
-           ;; coding system
-           (let* ((sys (coding-system-plist buffer-file-coding-system))
-                  (cat (plist-get sys :category))
-                  (sym (if (memq cat
-                                 '(coding-category-undecided coding-category-utf-8))
-                           'failure
-                         (plist-get sys :name))))
-             (when (or (eq doom-modeline-buffer-encoding t)
-                       (and (eq doom-modeline-buffer-encoding 'nondefault)
-                            (not (eq cat 'coding-category-undecided))
-                            (not (eq sym doom-modeline-default-coding-system))))
-               (propertize
-                (upcase (symbol-name sym))
-                'face face
-                'mouse-face mouse-face
-                'help-echo 'mode-line-mule-info-help-echo
-                'local-map mode-line-coding-system-map)))
-           )
-
-         (doom-modeline-spc)))))
-
-  (doom-modeline-def-segment kb/buffer-default-directory
-    "Standard `buffer-default-directory' without the state, icon, and color change."
-    (let* ((active (doom-modeline--active))
-           (face (if active 'bold-italic 'mode-line-inactive))) ; Don't use here
-      (concat (doom-modeline-spc)
-              (propertize (cond ((stringp (vc-git-root (buffer-file-name)))
-                                 (abbreviate-file-name (vc-git-root (buffer-file-name))))
-                                (buffer-file-name ; Connected to file?
-                                 default-directory)
-                                (t "Not file!"))
-                          'face face)
-              (doom-modeline-spc))))
-  (doom-modeline-def-segment me/major-mode
-    "The current major mode, including environment information."
-    (let* ((active (doom-modeline--active))
-           (face (if active 'doom-modeline-buffer-major-mode 'mode-line-inactive)))
-      (concat (doom-modeline-spc)
-              (propertize (format-mode-line mode-name) 'face face)
-              (doom-modeline-spc))))
-  (doom-modeline-def-segment kb/major-mode
-    "Standard `major-mode' but bolded."
-    (propertize
-     (concat
-      (doom-modeline-spc)
-      (propertize (format-mode-line
-                   (or (and (boundp 'delighted-modes)
-                            (cadr (assq major-mode delighted-modes)))
-                       mode-name))
-                  'help-echo "Major mode\n\
-  mouse-1: Display major mode menu\n\
-  mouse-2: Show help for major mode\n\
-  mouse-3: Toggle minor modes"
-                  'mouse-face 'mode-line-highlight
-                  'local-map mode-line-major-mode-keymap)
-      (when (and doom-modeline-env-version doom-modeline-env--version)
-        (format " %s" doom-modeline-env--version))
-      (and (boundp 'text-scale-mode-amount)
-           (/= text-scale-mode-amount 0)
-           (format
-            (if (> text-scale-mode-amount 0)
-                " (%+d)"
-              " (%-d)")
-            text-scale-mode-amount))
-      (doom-modeline-spc))
-     'face (if (doom-modeline--active)
-               '(doom-modeline-buffer-major-mode bold) ; Make bold
-             'mode-line-inactive)))
-
-  (doom-modeline-def-modeline 'main
-    '(kb/matches "   " kb/major-mode-icon "  " bar "  " kb/eyebrowse kb/vcs kb/buffer-default-directory kb/buffer-info remote-host buffer-position " " selection-info)
-    '(input-method process debug kb/time battery " " bar " " kb/buffer-encoding checker))
-  )
-
-;;;;;  Time
+;;;;; Time
 ;; Enable time in the mode-line
 (use-package time
   :after doom-modeline
@@ -288,7 +125,7 @@ UTF-8."
   (display-time-default-load-average 1) ; Don't show load average along with time
   )
 
-;;;;;  Battery
+;;;;; Battery
 ;; Display batter percentage
 (use-package battery
   :straight nil
