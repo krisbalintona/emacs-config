4e2484cb2765d8f88a7a6e8d20e4077c2ab9cee9
Kristoffer Balintona <krisbalintona@gmail.com>
2021-09-02 03:36:16 -0400

    Avoid reliance on after-init-hook in undoing-rcp

--- elisp/undoing-rcp.el
+++ elisp/undoing-rcp.el
@@ -26,12 +26,13 @@
 ;;;; Undo-fu-session
 ;; Keep undo history across sessions
 (use-package undo-fu-session
-  :after evil
-  :ghook ('after-init-hook 'global-undo-fu-session-mode)
+  :demand t
+  :requires undo-fu
   :custom
   (undo-fu-session-incompatible-files '("\\.gpg$" "/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'"))
   (undo-fu-session-directory (concat no-littering-var-directory "undo-fu-session/"))
   (undo-fu-session-file-limit 15000)
+  :config (global-undo-fu-session-mode)
   )
 
 ;;; undoing-rcp.el ends here
