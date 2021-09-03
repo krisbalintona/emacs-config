72c021393bfd56345880bfdefd4c76215ee1e2ac
Kristoffer Balintona <krisbalintona@gmail.com>
2021-09-02 20:02:20 -0400

    Fix indentation

--- recipes/keybinds-evil-rcp.el
+++ recipes/keybinds-evil-rcp.el
@@ -161,8 +161,8 @@
 (use-package better-jumper
   :after evil
   :general (:states '(normal visual normal)
-                    [remap evil-jump-backward] '(better-jumper-jump-backward :which-key "Jump backward")
-                    [remap evil-jump-forward] '(better-jumper-jump-forward :which-key "Jump forward"))
+                     [remap evil-jump-backward] '(better-jumper-jump-backward :which-key "Jump backward")
+                     [remap evil-jump-forward] '(better-jumper-jump-forward :which-key "Jump forward"))
   :custom
   (better-jumper-max-length 200)
   (better-jumper-use-evil-jump-advice t) ; Add evil-jump jumps
@@ -174,7 +174,7 @@
   :config
   ;; Add evil navigation commands to the better-jumper jump list
   (general-advice-add '(evil-forward-WORD-end evil-backward-WORD-begin evil-jump-item evil-first-non-blank evil-end-of-visual-line)
-                      :after 'better-jumper-set-jump)
+                       :after 'better-jumper-set-jump)
   (general-add-hook '(ace-jump-mode-before-jump-hook ace-jump-mode-end-hook) 'better-jumper-set-jump)
   )
 
