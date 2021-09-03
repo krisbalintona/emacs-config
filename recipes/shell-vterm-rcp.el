cb207fcc51e3d4e3cc171c7e310a25d3be96c9ab
Kristoffer Balintona <krisbalintona@gmail.com>
2021-09-02 23:18:12 -0400

    Hide modeline in vterm and more efficient use-package declaration

--- recipes/shell-vterm-rcp.el
+++ recipes/shell-vterm-rcp.el
@@ -12,19 +12,20 @@
 ;;;; Vterm
 ;; Full-fledged terminal emulator
 (use-package vterm
-  :demand t
   :ensure-system-package (("/usr/lib64/libvterm.so.0" . libvterm) ; Specifically for Fedora
                           (libtool)
                           ("/usr/include/vterm.h" . "libvterm-devel")
                           (cmake))
-  :gfhook '(lambda ()
-             (set (make-local-variable 'buffer-face-mode-face) 'fixed-pitch)
-             (buffer-face-mode t)
-             (face-remap-add-relative 'default :height 119)) ; Change default face size
+  :gfhook
+  '(lambda ()
+     (set (make-local-variable 'buffer-face-mode-face) 'fixed-pitch)
+     (buffer-face-mode t)
+     (face-remap-add-relative 'default :height 119)) ; Change default face size
+  'hide-mode-line-mode
+  :general (kb/leader-keys "ot" '(vterm :which-key "Vterm"))
   :custom
   (vterm-kill-buffer-on-exit nil)
   (vterm-copy-exclude-prompt t)
-  (vterm-min-window-width 50)
   )
 
 ;;; shell-vterm-rcp.el ends here
