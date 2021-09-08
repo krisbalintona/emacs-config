f2e05660d0b137890de8426afadfa0c03c873f12
Kristoffer Balintona <krisbalintona@gmail.com>
2021-09-07 23:53:54 -0400

    Make vterm font smaller and have lower display delay

--- recipes/shell-vterm-rcp.el
+++ recipes/shell-vterm-rcp.el
@@ -20,12 +20,13 @@
   '(lambda ()
      (set (make-local-variable 'buffer-face-mode-face) 'fixed-pitch)
      (buffer-face-mode t)
-     (face-remap-add-relative 'default :height 119)) ; Change default face size
+     (face-remap-add-relative 'default :height 107)) ; Change default face size
   'hide-mode-line-mode
   :general (kb/leader-keys "ot" '(vterm :which-key "Vterm"))
   :custom
   (vterm-kill-buffer-on-exit nil)
   (vterm-copy-exclude-prompt t)
+  (vterm-timer-delay 0.01)              ; Make vterm appear less "slow"
   )
 
 ;;; shell-vterm-rcp.el ends here
