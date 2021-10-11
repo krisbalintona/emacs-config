9008b3bc01094a6e37b1778188704272444fa237
Kristoffer Balintona <krisbalintona@gmail.com>
2021-10-10 21:24:48 -0400

    Install and configure wucuo to shadow flyspell

--- recipes/checking-spelling-rcp.el
+++ recipes/checking-spelling-rcp.el
@@ -14,9 +14,6 @@
 (use-package flyspell
   :ensure-system-package ((aspell . "sudo apt install aspell aspell-en")
                           (proselint . "sudo add-apt-repository universe && sudo apt install python3-proselint"))
-  :ghook
-  ('text-mode-hook 'flyspell-mode)
-  ('prog-mode-hook 'flyspell-prog-mode)
   :custom
   (flyspell-issue-message-flag nil)     ; Disable to prevent massive slowdown
   (flyspell-issue-welcome-flag nil)     ; Don't display welcome message
@@ -30,9 +27,38 @@
   (ispell-personal-dictionary (concat no-littering-var-directory "flyspell/flyspell-ispell-personal-dict-en"))
   )
 
+;;; Wucuo
+;; A complete solution to the lag of flyspell
+(use-package wucuo
+  :after flyspell
+  :hook ((text-mode . wucuo-start)
+         (prog-mode . wucuo-start)
+         )
+  :general (kb/leader-keys
+             "/" '(wucuo-spell-check-visible-region :which-key "Spellcheck buffer"))
+  :custom
+  (wucuo-flyspell-start-mode "fast")
+  ;; (ispell-extra-args "--run-together")  ; Faster aspell?
+  (wucuo-spell-check-buffer-predicate
+   (lambda ()
+     (not (memq major-mode ; Skip spell checking under these conditions
+                '(dired-mode
+                  log-edit-mode
+                  compilation-mode
+                  help-mode
+                  profiler-report-mode
+                  speedbar-mode
+                  gud-mode
+                  calc-mode
+                  Info-mode
+                  ))
+          )))
+  )
+
 ;;; Flyspell-lazy
 ;; Check on idle instead of instantly
 (use-package flyspell-lazy
+  :disabled t ; Not necessary since flyspell is fast enough, especially with wucuo
   :after flyspell
   :ghook 'flyspell-mode-hook
   :custom
@@ -49,8 +75,6 @@
   :general (kb/leader-keys
              "." '(flyspell-correct-next :which-key "Flyspell next")
              "," '(flyspell-correct-previous :which-key "Flyspell prev")
-             "/" '(flyspell-buffer :which-key "Flyspell check buffer")
-             "tc" '(flyspell-mode :which-key "Flyspell toggle")
              )
   :custom
   (flyspell-correct-interface 'flyspell-correct-completing-read)
