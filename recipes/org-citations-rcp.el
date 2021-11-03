3f3cd7eeae437ee9df73d7c4988392f2fe06574f
Kristoffer Balintona <krisbalintona@gmail.com>
2021-11-03 18:05:20 -0400

    Change config to accommate package name change from bibtex-actions to citar

--- recipes/org-citations-rcp.el
+++ recipes/org-citations-rcp.el
@@ -49,10 +49,10 @@
 ;; Built-in citations in org-mode
 (use-package oc
   :straight nil
-  :after (bibtex-completion bibtex-actions)
+  :after (bibtex-completion citar)
   :general
   (:keymaps 'org-mode-map
-            [remap bibtex-actions-insert-citation] '(org-cite-insert :which-key "Insert citation"))
+            [remap citar-insert-citation] '(org-cite-insert :which-key "Insert citation"))
   :custom
   (org-cite-global-bibliography bibtex-completion-bibliography)
   (org-cite-csl-locales-dir (expand-file-name (concat user-emacs-directory "locales/")))
@@ -87,52 +87,52 @@
   :ghook 'org-mode-hook ; FIXME 2021-09-14: Make so that I don't need to call in this way
   :custom
   (orb-preformat-keywords '("citekey" "author" "date"))
-  (bibtex-actions-file-open-note-function 'orb-bibtex-actions-edit-note)
+  (citar-file-open-note-function 'orb-citar-edit-note)
   )
 
-;;; Bibtex-actions
+;;; Citar
 ;; Alternative to `ivy-bibtex' and `helm-bibtex'
-(use-package bibtex-actions
+(use-package citar
   :demand t
   :after (bibtex-completion embark)
   :general
-  ("C-c bb" '(bibtex-actions-insert-citation :which-key "Insert citation")
-   "C-c br" '(bibtex-actions-insert-reference :which-key "Insert reference")
-   "C-c bo" '(bibtex-actions-open-notes :which-key "Open note")
+  ("C-c bb" '(citar-insert-citation :which-key "Insert citation")
+   "C-c br" '(citar-insert-reference :which-key "Insert reference")
+   "C-c bo" '(citar-open-notes :which-key "Open note")
    )
   :custom
-  (bibtex-actions-bibliography bibtex-completion-bibliography)
-  (bibtex-actions-presets '("has:note")) ; A list of predefined searches
-  (bibtex-actions-templates
+  (citar-bibliography bibtex-completion-bibliography)
+  (citar-presets '("has:note")) ; A list of predefined searches
+  (citar-templates
    '((main . "${author editor:40}   ${date year issued:4}    ${title:115}")
      (suffix . "     ${=type=:20}    ${tags keywords keywords:*}")
      (note . "#+title: Notes on ${author editor}, ${title}") ; For new notes
      ))
-  (bibtex-actions-at-point-function 'embark-act)
+  (citar-at-point-function 'embark-act)
   :config
-  ;; Make the 'bibtex-actions' bindings and targets available to `embark'.
-  (add-to-list 'embark-target-finders 'bibtex-actions-citation-key-at-point)
-  (add-to-list 'embark-keymap-alist '(bibtex . bibtex-actions-map))
-  (add-to-list 'embark-keymap-alist '(citation-key . bibtex-actions-buffer-map))
+  ;; Make the 'citar' bindings and targets available to `embark'.
+  (add-to-list 'embark-target-finders 'citar-citation-key-at-point)
+  (add-to-list 'embark-keymap-alist '(bibtex . citar-map))
+  (add-to-list 'embark-keymap-alist '(citation-key . citar-buffer-map))
 
   ;; Configuring all-the-icons. From
-  ;; https://github.com/bdarcus/bibtex-actions#rich-ui
-  (setq bibtex-actions-symbols
+  ;; https://github.com/bdarcus/citar#rich-ui
+  (setq citar-symbols
         `((pdf . (,(all-the-icons-icon-for-file "foo.pdf" :face 'all-the-icons-dred) .
-                  ,(all-the-icons-icon-for-file "foo.pdf" :face 'bibtex-actions-icon-dim)))
+                  ,(all-the-icons-icon-for-file "foo.pdf" :face 'citar-icon-dim)))
           (note . (,(all-the-icons-icon-for-file "foo.txt") .
-                   ,(all-the-icons-icon-for-file "foo.txt" :face 'bibtex-actions-icon-dim)))
+                   ,(all-the-icons-icon-for-file "foo.txt" :face 'citar-icon-dim)))
           (link .
                 (,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'all-the-icons-dpurple) .
-                 ,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'bibtex-actions-icon-dim)))))
+                 ,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'citar-icon-dim)))))
   ;; Here we define a face to dim non 'active' icons, but preserve alignment
-  (defface bibtex-actions-icon-dim
+  (defface citar-icon-dim
     '((((background dark)) :foreground "#282c34")
       (((background light)) :foreground "#fafafa"))
     "Face for obscuring/dimming icons"
     :group 'all-the-icons-faces)
 
-  ;; Set my own formatting for `bibtex-actions-insert-reference'
+  ;; Set my own formatting for `citar-insert-reference'
   (defun kb/bibtex-completion-insert-reference (keys)
     "Insert references for entries in KEYS without \"-\" on new line."
     (let* ((refs (--map
@@ -143,16 +143,17 @@
   (advice-add 'bibtex-completion-insert-reference :override 'kb/bibtex-completion-insert-reference)
   )
 
-;;; Oc-bibtex-actions
-;; Bibtex-actions compatible with `org-cite'
-(use-package oc-bibtex-actions
+;;; Oc-citar
+;; citar compatible with `org-cite'
+(use-package citar-org
   :demand t                            ; Otherwise it won't be required anywhere
   :straight nil
-  :after (bibtex-actions oc)
+  :after (citar oc)
   :custom
-  ;; Use `bibtex-actions'
-  (org-cite-insert-processor 'oc-bibtex-actions)
-  (org-cite-follow-processor 'oc-bibtex-actions)
+  ;; Use `citar'
+  (org-cite-insert-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-activate-processor 'citar)
   )
 
 ;;; Ox-pandoc
