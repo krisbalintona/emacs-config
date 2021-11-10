6f5647a511051f8520e6d64f1ef6be5ff798ceb2
Kristoffer Balintona <krisbalintona@gmail.com>
2021-11-10 18:29:07 -0500

    Update lsp and dap configurations

--- recipes/programming-ide-rcp.el
+++ recipes/programming-ide-rcp.el
@@ -12,30 +12,21 @@
 ;;; Lsp-mode
 ;; Use the language server protocol as a backend for Emacs.
 (use-package lsp-mode
+  :commands (lsp lsp-deferred)
   ;; Lsp-mode only when buffer is visible
-  :hook ((lua-mode . lsp-deferred)
-         (haskell-mode . lsp-deferred)
-         (js2-mode . lsp-deferred)
-         )
   :gfhook
   'lsp-enable-which-key-integration
   'lsp-headerline-breadcrumb-mode
-  :general
-  (:keymaps 'lsp-ui-mode-map
-            [remap xref-find-definitions] #'lsp-ui-peek-find-definitions
-            [remap xref-find-references] #'lsp-ui-peek-find-references
-            [remap imenu-list] #'lsp-ui-imenu
-            )
   :custom
   (lsp-keymap-prefix "C-x l")
-  (lsp-auto-guess-root t)
+  (lsp-auto-guess-root nil)
   (lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-semantic-tokens-enable t)
   (lsp-symbol-highlighting-skip-current t) ; When highlighting, don't highlight symbol on point
   (lsp-modeline-diagnostics-scope :workspace)
   (lsp-modeline-code-actions-segments '(count icon name))
-  (lsp-enable-file-watchers nil) ; Don't watch files - affects performance. Enable if I do actual programming
-  (lsp-log-io nil)               ; If set to true can cause a performance hit
+  (lsp-enable-file-watchers t) ; Affects performance. Enable if I do actual programming
+  (lsp-log-io nil)             ; If set to true can cause a performance hit
   )
 
 ;;; Lsp-ui
@@ -43,13 +34,22 @@
 ;; on the point.
 (use-package lsp-ui
   :ghook 'lsp-mode-hook
-  :hook (lsp-ui-imenu-mode . hide-mode-line-mode)
+  :hook ((lsp-ui-imenu-mode . hide-mode-line-mode)
+         (lsp-mode . lsp-ui-mode))
+  :general (:keymaps 'lsp-ui-mode-map
+                     [remap xref-find-definitions] #'lsp-ui-peek-find-definitions
+                     [remap xref-find-references] #'lsp-ui-peek-find-references
+                     [remap imenu-list] #'lsp-ui-imenu
+                     )
   :custom
   ;; Lsp-ui-peek - Peek in a child frame
   (lsp-ui-peek-enable t)
   (lsp-ui-peek-show-directory t)
 
-  ;; Lsp-ui-doc - Show documentation on the side
+  ;; Lsp-ui-sideline - Info at the side
+  (lsp-ui-sideline-enable t)
+
+  ;; Lsp-ui-doc - Show documentation
   (lsp-ui-doc-enable t)
   (lsp-ui-doc-position 'top)
   (lsp-ui-doc-delay 1.5)
@@ -61,30 +61,22 @@
   ;; (lsp-ui-imenu-refresh-delay 1)        ; Variable doesn't exist?
   )
 
-
-;;; Lsp-treemacs
-;; Treemacs-like buffer that shows files, errors, symbol hierarchy, etc.
-(use-package lsp-treemacs
-  :hook ((lsp-mode . lsp-treemacs-sync-mode)
-         (lsp-treemacs-generic-mode . hide-mode-line-mode)
-         (lsp-treemacs-error-list-mode . hide-mode-line-mode)
-         (lsp-treemacs-deps-list-mode . hide-mode-line-mode)
-         )
-  :general (:keymaps 'lsp-treemacs-error-list-mode-map
-                     :states 'normal
-                     "x" 'lsp-treemacs-quick-fix)
-  )
-
 ;;; Dap-mode
 (use-package dap-mode
-  ;; :ensure-system-package ("pip install \"ptvsd>=4.2\"")
+  :demand t
+  :gfhook
+  'dap-ui-mode      ; Mouse hover support
+  'dap-tooltip-mode ; Use tooltips for mouse hover, otherwise use the minibuffer
+  'tooltip-mode     ; Displays floating panel with debug buttons
+  'dap-ui-controls-mode
   )
 
 ;;; Tree-sitter
 ;; Create a syntax tree (e.g. the role of each piece of code) and add syntax
 ;; highlighting from it (rather than regex and indentation). Additionally, the
-;; syntax tree itsel can help debug and quick editing in some cases.
-;; The following are the currently supported languages (provided by `tree-sitter-langs')
+;; syntax tree itself can help debug and quick editing in some cases. The
+;; following are the currently supported languages (provided by
+;; `tree-sitter-langs')
 ;; C
 ;; C++
 ;; CSS
@@ -98,20 +90,20 @@
 ;; Rust
 ;; TypeScript
 (use-package tree-sitter
-  :defer 10
-  :gfhook 'tree-sitter-hl-mode          ; Enable syntax highlighting
-  :config (global-tree-sitter-mode) ; Enable for all supported tree-sitter languages
-  )
-
-;;; Tree-sitter-langs
-;; Supported languge bundle for tree-sitter
-(use-package tree-sitter-langs
-  :after tree-sitter
+  :demand t
+  :hook (tree-sitter-after-on . tree-sitter-hl-mode) ; Enable syntax highlighting whenever possible
   :config
-  (tree-sitter-langs-install-grammars
-   t
-   tree-sitter-langs--bundle-version
-   tree-sitter-langs--os)
+  (global-tree-sitter-mode)     ; Enable for all supported tree-sitter languages
+
+  ;; Supported language bundle for tree-sitter
+  (use-package tree-sitter-langs
+    :demand t
+    :config
+    (tree-sitter-langs-install-grammars
+     t
+     tree-sitter-langs--bundle-version
+     tree-sitter-langs--os)
+    )
   )
 
 ;;; Ancillary
