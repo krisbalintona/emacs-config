37190897298020d67145f394e3b5dad8c08ef93e
Kristoffer Balintona <krisbalintona@gmail.com>
2022-01-29 15:24:47 -0500

    Only use orderless matching style

--- recipes/completion-vanilla-rcp.el
+++ recipes/completion-vanilla-rcp.el
@@ -203,7 +203,7 @@
 ;; Alternative and powerful completion style (i.e. filters candidates)
 (use-package orderless
   :custom
-  (completion-styles '(orderless flex))
+  (completion-styles '(orderless))
   (completion-category-defaults nil)    ; I want to be in control!
   (completion-category-overrides
    '((file (styles basic-remote ; For `tramp' hostname completion with `vertico'
