9352ae746c55e4b96919849f33d02207dc33237b
Kristoffer Balintona <krisbalintona@gmail.com>
2022-01-30 20:34:34 -0500

    Add keybind to dap-java-debug; ensure helm is used for lsp code actions

--- recipes/programming-java-rcp.el
+++ recipes/programming-java-rcp.el
@@ -14,13 +14,19 @@
 ;;; Lsp-java
 (use-package lsp-java
   :hook (java-mode . lsp-deferred)
-  :general (:keymaps 'java-mode-map
-                     "C-c C-c" '(lsp-java-build-project :wk "Lsp build project"))
+  :config
+  (require 'helm)
   )
 
 ;;; Dap-java
 (use-package dap-java
   :straight nil
+  :general (:keymaps 'java-mode-map
+                     "C-c C-c" '(dap-java-debug :wk "Dap-java-debug"))
+;;; Helm-lsp
+;; Helm interface -- really the only option...
+(use-package helm-lsp
+  :after 'lsp
   )
 
 ;;; programming-java-rcp.el ends here
