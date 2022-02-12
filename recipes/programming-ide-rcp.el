c2a4e16228f9d1a9bf3efd73445de24622fe509a
Kristoffer Balintona <krisbalintona@gmail.com>
2022-02-12 17:53:11 -0500

    Allow apheleia to format javadocs

--- recipes/programming-ide-rcp.el
+++ recipes/programming-ide-rcp.el
@@ -206,7 +206,7 @@
      (clang-format "clang-format")
      (mix-format "mix" "format" "-")
      (gofmt "gofmt")
-     (google-java-format "google-java-format" "--aosp" "--skip-javadoc-formatting" "--skip-removing-unused-imports" "-")
+     (google-java-format "google-java-format" "--aosp" "--skip-removing-unused-imports" "-")
      (isort "isort" "--stdout" "-")
      (ocamlformat "ocamlformat" "-" "--name" filepath)
      (prettier npx "prettier" "--stdin-filepath" filepath)
