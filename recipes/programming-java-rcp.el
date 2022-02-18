0b0a0c67cd5022009519347f2b37adbde913cb6c
Kristoffer Balintona <krisbalintona@gmail.com>
2022-02-18 03:13:50 -0500

    Fix debugging in java with lsp-java

--- recipes/programming-java-rcp.el
+++ recipes/programming-java-rcp.el
@@ -11,21 +11,31 @@
 ;;; Lsp-java
 (use-package lsp-java
   :hook (java-mode . lsp-deferred)
+  :custom
+  (lsp-java-project-referenced-libraries ["lib/**/*.jar" "src/**/*.jar"])
   :config
-  (require 'helm)
-  )
+  (dap-register-debug-template
+   "Java Run Configuration"
+   (list :name "Java Run Configuration"
+         :vmArgs "--enable-preview"     ; Needed
+         :host "localhost"
+         :console "internalConsole")))
 
 ;;; Dap-java
 (use-package dap-java
   :straight nil
   :general (:keymaps 'java-mode-map
-                     "C-c C-c" '(dap-java-debug :wk "Dap-java-debug"))
+                     "C-c C-c" '((lambda () (dap-java-debug (dap-java--populate-default-args (list :vmArgs "--enable-preview"))))
+                                 :wk "Dap-java-debug"))
+
   )
 
 ;;; Helm-lsp
 ;; Helm interface -- really the only option...
 (use-package helm-lsp
   :after lsp-mode
+  :config
+  (require 'helm)
   )
 
 ;;; programming-java-rcp.el ends here
