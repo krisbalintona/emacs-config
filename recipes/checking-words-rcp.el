b815d948529a649db05d1163611823ca73354e77
Kristoffer Balintona <krisbalintona@gmail.com>
2022-06-04 16:39:10 -0500

    Set up my dwim dictionary and thesaurus commands

--- recipes/checking-words-rcp.el
+++ recipes/checking-words-rcp.el
@@ -14,40 +14,12 @@
 ;; See definitions of words from an online dictionary.
 (use-package dictionary
   :gfhook 'hide-mode-line-mode
-  :general  ; `:chords' doesn't have support for multiple keymaps per expression
-  (:keymaps 'dictionary-mode-map
-            "q" 'dictionary-close)
-  (:keymaps 'text-mode-map
-            (general-chord "jj") 'dictionary-lookup-definition
-            (general-chord "JJ") 'dictionary-search)
-  (:keymaps 'prog-mode-map
-            (general-chord "jj") '(lambda () ; Only when in comment
-                                    (interactive)
-                                    (when (nth 4 (syntax-ppss))
-                                      (dictionary-lookup-definition)))
-            (general-chord "JJ") '(lambda () ; Only when in comment
-                                    (interactive)
-                                    (when (nth 4 (syntax-ppss))
-                                      (dictionary-search))))
   :custom
   (dictionary-use-single-buffer t))     ; Reuse dictionary buffers
 
 ;;;; Powerthesaurus
 ;; Search for synonyms using an online thesaurus.
-(use-package powerthesaurus
-  :general  ; `:chords' doesn't have support for multiple keymaps per expression
-  (:keymaps 'text-mode-map
-            (general-chord "kk") 'powerthesaurus-lookup-synonyms-dwim
-            (general-chord "KK") 'powerthesaurus-lookup-dwim)
-  (:keymaps 'prog-mode-map
-            (general-chord "kk") '(lambda () ; Only when in comment
-                                    (interactive)
-                                    (when (nth 4 (syntax-ppss))
-                                      (powerthesaurus-lookup-synonyms-dwim)))
-            (general-chord "KK") '(lambda () ; Only when in comment
-                                    (interactive)
-                                    (when (nth 4 (syntax-ppss))
-                                      (powerthesaurus-lookup-dwim)))))
+(use-package powerthesaurus)
 
 ;;; Offline
 ;;;; Wordnut
@@ -56,12 +28,7 @@
   :after define-word
   ;; TODO 2021-08-20: Have this changed depending on Linux distribution
   ;; Make sure the install a dictnary, in case it is a separate package
-  :ensure-system-package (wn . wordnet-cli)
-  :config
-  (unless (featurep 'define-word)
-    (general-define-key
-     "C-c d" '(wordnut-lookup-current-word :wk "Wordnut lookup this word")
-     "C-c D" '(wordnut-search :wk "Wordnut search"))))
+  :ensure-system-package (wn . wordnet-cli))
 
 ;;;; Synosaurus
 ;; Offline thesaurus
@@ -71,14 +38,7 @@
   :ensure-system-package (wn . wordnet) ; Make sure English dictionary is also installed
   :custom
   (synosaurus-backend 'synosaurus-backend-wordnet) ; Offline thesaurus that relies on `wordnet'
-  (synosaurus-choose-method 'default)
-  :config
-  (unless (featurep 'powerthesaurus)
-    (synosaurus-mode)
-    (general-define-key
-     "C-c l" '(synosaurus-choose-and-replace :wk "Synosaurus at point")
-     "C-c L" '(synosaurus-choose-and-insert :wk "Synosaurus lookup")))
-  )
+  (synosaurus-choose-method 'default))
 
 ;;;; kb/{dictionary,thesaurus}-at-point kb/{dictionary,thesaurus}-lookup
 ;; Change which packages are used depending on internet connection
@@ -88,33 +48,54 @@ otherwise. Credit to https://emacs.stackexchange.com/a/18515"
   (= 0 (call-process "ping" nil nil nil "-c" "1" "-W" "1"
                      (if host host "www.google.com"))))
 
+;; TODO 2022-06-04: Better integrate `helpful' somehow
 (defun kb/dictionary-at-point ()
-  "Use `dictionary' if online, and `wordnet' if offline."
+  "When in a text-mode major mode, use `dictionary' if online, and
+`wordnet' if offline. Also do this when in a comment or string in
+a prog-mode derived mode; otherwise call `helpful-at-point'."
   (interactive)
-  (if (kb/internet-up-p)
-      (dictionary-lookup-definition)
-    (wordnut-lookup-current-word)))
+  (cond
+   ((derived-mode-p 'text-mode)
+    (if (kb/internet-up-p)
+        (dictionary-lookup-definition)
+      (wordnut-lookup-current-word)))
+   ((derived-mode-p 'prog-mode)
+    (if (or (nth 4 (syntax-ppss))       ; In comment
+            (nth 3 (syntax-ppss)))      ; In string
+        (if (kb/internet-up-p)
+            (dictionary-lookup-definition)
+          (wordnut-lookup-current-word))
+      (helpful-at-point)))))
 
 (defun kb/thesaurus-at-point ()
   "Use `powerthesaurus' if online, and `synosaurus' if offline."
   (interactive)
-  (if (kb/internet-up-p)
-      (powerthesaurus-lookup-synonyms-dwim)
-    (synosaurus-choose-and-replace)))
+  (when (or (derived-mode-p 'prog-mode) (nth 4 (syntax-ppss)))
+    (if (kb/internet-up-p)
+        (powerthesaurus-lookup-synonyms-dwim)
+      (synosaurus-choose-and-replace))))
 
 (defun kb/dictionary-lookup ()
   "Use `dictionary' if online, and `wordnet' if offline."
   (interactive)
   (if (kb/internet-up-p)
-      (dictionary-search)
+      (funcall 'dictionary-search)
     (wordnut-search)))
 
-(defun kb/dictionary-lookup ()
+(defun kb/thesuarus-lookup ()
   "Use `powerthesaurus' if online, and `synosaurus' if offline."
   (interactive)
   (if (kb/internet-up-p)
       (powerthesaurus-lookup-dwim)
     (synosaurus-choose-and-insert)))
+
+;; TODO 2022-06-04: Integrate devdocs
+(general-define-key
+ :keymaps '(prog-mode-map text-mode-map)
+ (general-chord "jj") 'kb/dictionary-at-point
+ (general-chord "JJ") 'kb/dictionary-lookup
+ (general-chord "kk") 'kb/thesaurus-at-point
+ (general-chord "KK") 'kb/thesuarus-lookup)
 
 ;;; checking-words-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
