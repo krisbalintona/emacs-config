ff59bb8a241d7afe10614c6eae6f4797bff9131a
Kristoffer Balintona <krisbalintona@gmail.com>
2022-06-04 19:26:20 -0500

    Remove my current text-editing native keybinds

--- recipes/keybinds-native-rcp.el
+++ recipes/keybinds-native-rcp.el
@@ -9,25 +9,9 @@
 (require 'use-package-rcp)
 (require 'keybinds-general-rcp)
 
-;;; Global-map
 (general-define-key
- "C-x C-c" 'nil ; Unbinds `save-buffers-kill-emacs'.
- [remap newline] '(lambda ()                 ; Newline with indent
-                    (interactive)
-                    (insert "\n")
-                    (indent-according-to-mode))
- "M-<return>" '(lambda ()                    ; Insert newline above and go to it
-                 (interactive)
-                 (move-beginning-of-line 1)
-                 (insert "\n")
-                 (forward-line -1)
-                 (indent-according-to-mode))
- "C-S-k" '(lambda ()                         ; Join line above
-            (interactive)
-            (save-excursion (join-line)))
- "C-S-j" '(lambda ()                         ; Join line below
-            (interactive)
-            (save-excursion (join-line 1))))
+ "C-x C-c" nil                          ; Unbinds `save-buffers-kill-emacs'.
+ (general-chord "xx") 'save-buffer)
 
 ;;; keybinds-native-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
