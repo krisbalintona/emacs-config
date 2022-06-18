9fced426d8ee83c36920d148eb92c523e0871054
Kristoffer Balintona <krisbalintona@gmail.com>
2022-06-18 06:50:58 -0500

    Start messing around with eglot

--- recipes/programming-eglot-rcp.el
+++ recipes/programming-eglot-rcp.el
@@ -10,7 +10,33 @@
 (require 'keybinds-general-rcp)
 
 ;;; Eglot
-(use-package eglot)
+(use-package eglot
+  :ensure-system-package pyright
+  ;; :hook ((python-mode) . eglot-ensure)
+  :config
+  (setf (alist-get 'python-mode eglot-server-programs)
+        `("pyright-langserver" "--stdio" "--tcp" "--host" "localhost" "--port" :autoport)))
+
+;;; Languages
+;;;; Eglot-java
+(use-package eglot-java
+  :ensure-system-package jdtls
+  :requires eglot
+  :custom
+  (eglot-java-server-install-dir (no-littering-expand-var-file-name "eglot-java/"))
+  (eglot-java-workspace-folder (expand-file-name eglot-java-server-install-dir "workspace/"))
+  (eglot-java-eclipse-jdt-args (list "--enable-preview"))
+  (eglot-java-prefix-key "C-c e")
+  (eglot-java-default-bindings-enabled t) ; See `eglot-java--setup'
+  :config
+  (eglot-java-init))
+
+;;; Consult-eglot
+;; Equivalent to `consult-lsp'; adds `consult-eglot-symbols'.
+(use-package consult-eglot
+  :after eglot
+  :general (:keymaps 'eglot-mode-map
+                     "C-c e" '(consult-eglot-symbols :wk "Consult-eglot")))
 
 ;;; programming-eglot-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
