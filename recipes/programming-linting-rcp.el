6b6743009b533c39c6110d2434232ed20ad41b39
Kristoffer Balintona <krisbalintona@gmail.com>
2022-06-18 21:52:42 -0500

    Disable consult-flycheck so it doesn't overshadow consult-flymake binding

--- recipes/programming-linting-rcp.el
+++ recipes/programming-linting-rcp.el
@@ -43,6 +43,7 @@
 ;;; Consult-flycheck
 ;; List flycheck errors in minibuffer with consult
 (use-package consult-flycheck
+  :disabled t
   :after (consult flycheck)
   :general (kb/nav-keys
              "e" '(consult-flycheck :wk "Consult flycheck")))
