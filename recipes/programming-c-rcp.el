8fd6bdcbff50b87a89fc6449e623184b0c6195fb
Kristoffer Balintona <krisbalintona@gmail.com>
2022-07-21 03:38:56 -0500

    Install and configure gdb-bp-session

--- recipes/programming-c-rcp.el
+++ recipes/programming-c-rcp.el
@@ -38,6 +38,69 @@ regarding \"target-async\"."
       (gdb-input "-list-target-features" 'gdb-check-target-async)))
   (advice-add 'gdb-non-stop-handler :override #'kb/gdb-non-stop-handler))
 
+;;; Gdb-bp-session
+(use-package gdb-bp-session
+  :requires no-littering gud
+  :hook (gdb-inferior-io-mode . kb/gdb-bp-session-ask-restore-breakpoints)
+  :config
+  (defvar kb/gdb-bp-session--save-dir
+    (no-littering-expand-var-file-name "gdb-bp-session/"))
+
+  (defun kb/gdb-bp-session--save-path-file ()
+    (let* ((gud-process-name (file-name-sans-extension (gud-get-process-name)))
+           (save-file-name (concat gud-process-name "-breakpoints.gdb"))
+           (save-file-dir kb/gdb-bp-session--save-dir)
+           (save-file-path (expand-file-name save-file-name save-file-dir)))
+      (unless (file-exists-p save-file-dir)
+        (make-directory save-file-dir))
+      (if gud-process-name
+          save-file-path
+        (user-error "Not in GDB!"))))
+
+  (defun kb/gdb-save-breakpoints ()
+    "Save current breakpoint definitions as a script."
+    (interactive)
+    (let* ((save-file-path (kb/gdb-bp-session--save-path-file)))
+      (gud-basic-call (concat "save breakpoints " save-file-path))))
+  (advice-add 'gdb-save-breakpoints :override #'kb/gdb-save-breakpoints)
+
+  (defun kb/gdb-restore-breakpoints ()
+    "Restore the saved breakpoint definitions as a script."
+    (interactive)
+    (let* ((breakpoints-file (kb/gdb-bp-session--save-path-file)))
+      (if (file-exists-p breakpoints-file)
+          (gud-basic-call (format "source %s" breakpoints-file))
+        (user-error "No saved breakpoints file!"))))
+  (advice-add 'gdb-restore-breakpoints :override #'kb/gdb-restore-breakpoints)
+
+  (defun kb/gdb-bp-session-ask-restore-breakpoints ()
+    (when (y-or-n-p "Restore breakpoints?")
+      (kb/gdb-restore-breakpoints)))
+
+  (defun kb/gdb-delchar-or-quit (arg)
+    "Delete ARG characters or send a quit command to GDB.
+Send a quit only if point is at the end of the buffer, there is
+no input, and GDB is waiting for input."
+    (interactive "p")
+    (unless (and (eq (current-buffer) gud-comint-buffer)
+                 (eq gud-minor-mode 'gdbmi))
+      (error "Not in a GDB-MI buffer"))
+    (let ((proc (get-buffer-process gud-comint-buffer)))
+      (if (and (eobp)
+               (process-live-p proc)
+               (not gud-running)
+               (= (point) (marker-position (process-mark proc))))
+          ;; Sending an EOF does not work with GDB-MI; submit an
+          ;; explicit quit command.
+          (progn
+            (if (> gdb-control-level 0)
+                (process-send-eof proc)
+              (kb/gdb-save-breakpoints)
+              (insert "quit")
+              (comint-send-input t t)))
+        (delete-char arg))))
+  (advice-add 'gdb-delchar-or-quit :override #'kb/gdb-delchar-or-quit))
+
 ;;; programming-c-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'programming-c-rcp)
