6b38172d4612a54d96911a05e963bd8328526427
Kristoffer Balintona <krisbalintona@gmail.com>
2022-12-07 21:40:10 -0500

    Image-roll with pdf-tools for continuous scrolling

--- recipes/org-roam-other-rcp.el
+++ recipes/org-roam-other-rcp.el
@@ -14,23 +14,32 @@
 ;;;; Pdf-tools
 ;; View pdfs and interact with them. Has many dependencies
 ;; https://github.com/politza/pdf-tools#compiling-on-fedora
+(use-package image-roll
+  :demand t
+  :straight (image-roll :type git :host github :repo "dalanicolai/image-roll.el"))
 (use-package pdf-tools
-  :straight (pdf-tools :type git :host github :repo "vedang/pdf-tools") ; Repo of current maintainer
-  ;; Ensure it's installed without manually doing so
+  ;; Image roll version. See
+  ;; https://github.com/dalanicolai/image-roll.el#pdf-tools
+  :straight (pdf-tools :type git :host github :repo "dalanicolai/pdf-tools" :branch "pdf-roll"
+                       :files ("lisp/*.el"
+                               "README"
+                               ("build" "Makefile")
+                               ("build" "server")
+                               (:exclude "lisp/tablist.el" "lisp/tablist-filter.el")))
   :hook (after-init . pdf-tools-install)
+  :gfhook 'pdf-view-themed-minor-mode 'pdf-view-roll-minor-mode
   :custom
-  (pdf-view-display-size 'fit-width)
+  (pdf-view-display-size 'fit-page)
   ;; Enable hiDPI support, but at the cost of memory! See politza/pdf-tools#51
   (pdf-view-use-scaling t)
-  (pdf-view-use-imagemagick t)
-  )
+  (pdf-view-use-imagemagick t))
 
 ;;;; Org-noter
 (use-package org-noter
   :defer 10                      ; Load so it doesn't defer to noter insert call
   :general
   (:keymaps 'org-noter-doc-mode-map
-            "M-o" 'org-noter-insert-note)
+   "M-o" 'org-noter-insert-note)
   (kb/open-keys
     "n" '(org-noter :wk "Org-noter"))
   :custom
@@ -47,15 +56,15 @@
 (use-package org-remark
   :after org
   :general (:keymaps 'org-remark-mode-map
-                     :prefix "C-c r"
-                     "m" #'org-remark-mark
-                     "r" #'org-remark-remove
-                     "c" #'org-remark-change
-                     "t" #'org-remark-toggle
-                     "o" #'org-remark-open
-                     "v" #'org-remark-view
-                     "n" #'org-remark-view-next
-                     "p" #'org-remark-view-prev)
+            :prefix "C-c r"
+            "m" #'org-remark-mark
+            "r" #'org-remark-remove
+            "c" #'org-remark-change
+            "t" #'org-remark-toggle
+            "o" #'org-remark-open
+            "v" #'org-remark-view
+            "n" #'org-remark-view-next
+            "p" #'org-remark-view-prev)
   :config
   (org-remark-mode)
   (require 'org-remark-global-tracking)
@@ -112,7 +121,7 @@
 
 ;;; Delve
 (use-package delve
-  :disabled t                           ; Don't use 
+  :disabled t                           ; Don't use
   :straight (delve :type git :host github :repo "publicimageltd/delve")
   :gfhook #'delve-compact-view-mode
   ;; FIXME 2022-05-27: `delve--zettel-preview' seems necessary to prevent cmacro
@@ -121,51 +130,51 @@
   :general
   ("<f2>" 'delve)
   (:keymaps 'delve-mode-map
-            "<backtab>" 'kb/delve--key-backtab
-            )
+   "<backtab>" 'kb/delve--key-backtab
+   )
   (:keymaps 'delve-mode-map
-            :states 'visual
-            "d" 'delve--key--multi-delete
-            "DEL" 'delve--key--multi-delete
-            "y" 'kb/delve-visual-copy-nodes
-            )
+   :states 'visual
+   "d" 'delve--key--multi-delete
+   "DEL" 'delve--key--multi-delete
+   "y" 'kb/delve-visual-copy-nodes
+   )
   (:keymaps 'delve-mode-map
-            :states 'normal
-            "DEL" 'delve--key--multi-delete
-            "q" 'bury-buffer
-            "t" 'delve--key--insert-tagged
-            "T" 'delve--key--insert-node-by-tags
-            "dd" '(lambda ()
-                    (interactive)
-                    (kill-line)
-                    (forward-line -1))
-            "P" 'delve--key--yank       ; Paste above
-            "p" '(lambda ()                  ; Paste below
-                   (interactive)
-                   (save-excursion
-                     (next-line)
-                     (delve--key--yank))
-                   (next-line))
-            "r" 'delve--key--sync
-            "gr" 'delve--key--sync
-            "h" 'delve--key--insert-heading
-            "n" 'delve--node-transient-key
-            "s" 'delve--key--sort
-            "C-p" 'delve--key--collect-into-pile
-            "I" 'delve--key--insert-query-or-pile
-            "v" 'delve-compact-view-mode
-            "f" 'delve--key--fromlinks
-            "b" 'delve--key--backlinks
-            "RET" 'kb/delve--key--toggle-preview
-            "C-o" 'delve--key--open-zettel
-            "o" 'delve--key--open-zettel
-            "go" 'delve--key--open-zettel
-            "+" 'delve--key--add-tags
-            "-" 'delve--key--remove-tags
-            "M-d" 'kb/delve-mark-duplicates
-            "Y" 'kb/delve-copy-zettel-title
-            "yy" 'evil-yank-line
-            )
+   :states 'normal
+   "DEL" 'delve--key--multi-delete
+   "q" 'bury-buffer
+   "t" 'delve--key--insert-tagged
+   "T" 'delve--key--insert-node-by-tags
+   "dd" '(lambda ()
+            (interactive)
+            (kill-line)
+            (forward-line -1))
+   "P" 'delve--key--yank       ; Paste above
+   "p" '(lambda ()                  ; Paste below
+           (interactive)
+           (save-excursion
+             (next-line)
+             (delve--key--yank))
+           (next-line))
+   "r" 'delve--key--sync
+   "gr" 'delve--key--sync
+   "h" 'delve--key--insert-heading
+   "n" 'delve--node-transient-key
+   "s" 'delve--key--sort
+   "C-p" 'delve--key--collect-into-pile
+   "I" 'delve--key--insert-query-or-pile
+   "v" 'delve-compact-view-mode
+   "f" 'delve--key--fromlinks
+   "b" 'delve--key--backlinks
+   "RET" 'kb/delve--key--toggle-preview
+   "C-o" 'delve--key--open-zettel
+   "o" 'delve--key--open-zettel
+   "go" 'delve--key--open-zettel
+   "+" 'delve--key--add-tags
+   "-" 'delve--key--remove-tags
+   "M-d" 'kb/delve-mark-duplicates
+   "Y" 'kb/delve-copy-zettel-title
+   "yy" 'evil-yank-line
+   )
   :custom
   (delve-storage-paths (concat kb/roam-dir "delve-storage/"))
   (delve-dashboard-tags '("working"))
@@ -200,21 +209,21 @@ When called with PREFIX, hide all previews."
     (interactive (list lister-local-ewoc current-prefix-arg))
     (lister-walk-nodes ewoc
                        #'(lambda (ewoc new-node)
-                           (let* ((current-item (delve--current-item nil ewoc new-node))
-                                  (is-zettel (cl-typep current-item 'delve--zettel))
-                                  (has-preview (when is-zettel (delve--zettel-preview current-item)))
-                                  )
-                             ;; Hide only when...
-                             (when (and (not (lister-sublist-below-p ewoc new-node)) ; Non-parents
-                                        (not has-preview) ; No preview
-                                        (< 0 (lister-node-get-level new-node)) ; Not top-level
-                                        )
-                               (lister--outline-hide-show ewoc new-node new-node kb/delve-cycle-global-contents))
-                             ;; Then hide if with prefix, hide all previews
-                             (when (and prefix has-preview)
-                               (setf (delve--zettel-preview current-item) nil)
-                               (lister-refresh-at ewoc new-node)) ; Refresh to update visually
-                             ))
+                            (let* ((current-item (delve--current-item nil ewoc new-node))
+                                   (is-zettel (cl-typep current-item 'delve--zettel))
+                                   (has-preview (when is-zettel (delve--zettel-preview current-item)))
+                                   )
+                              ;; Hide only when...
+                              (when (and (not (lister-sublist-below-p ewoc new-node)) ; Non-parents
+                                         (not has-preview) ; No preview
+                                         (< 0 (lister-node-get-level new-node)) ; Not top-level
+                                         )
+                                (lister--outline-hide-show ewoc new-node new-node kb/delve-cycle-global-contents))
+                              ;; Then hide if with prefix, hide all previews
+                              (when (and prefix has-preview)
+                                (setf (delve--zettel-preview current-item) nil)
+                                (lister-refresh-at ewoc new-node)) ; Refresh to update visually
+                              ))
                        :first :last)
     (if (lister--outline-invisible-p ewoc :point) ; End a non-invisible node
         (lister-goto ewoc (lister-parent-node ewoc :point)))
@@ -226,11 +235,11 @@ When called with PREFIX, hide all previews."
       (lister-save-current-node ewoc
           (lister-walk-nodes ewoc
                              #'(lambda (ewoc new-node)
-                                 (let ((id (delve--zettel-id (delve--current-item nil ewoc new-node))))
-                                   (if (member id id-tracker)
-                                       ;; (lister-delete-at ewoc new-node)
-                                       (lister-mode-mark ewoc new-node)
-                                     (setq id-tracker (append id-tracker (list id))))))
+                                  (let ((id (delve--zettel-id (delve--current-item nil ewoc new-node))))
+                                    (if (member id id-tracker)
+                                        ;; (lister-delete-at ewoc new-node)
+                                        (lister-mode-mark ewoc new-node)
+                                      (setq id-tracker (append id-tracker (list id))))))
                              :first :last
                              #'(lambda (new-node) (cl-typep (delve--current-item nil ewoc new-node) 'delve--zettel))))))
   (lister-defkey kb/delve-visual-copy-nodes (ewoc pos prefix node)
@@ -250,30 +259,30 @@ When called with PREFIX, hide all previews."
 (use-package lister
   :general
   (:keymaps 'lister-mode-map
-            "M-k" 'kb/lister-mode-up
-            "M-j" 'kb/lister-mode-down
-            "M-h" 'lister-mode-left
-            "M-l" 'kb/lister-mode-right
-            ;; Use the initial versions of the functions for these
-            "M-K" '(lambda ()
-                     (interactive) ; Ignore constraint of same indentation level
-                     (funcall-interactively 'lister-mode-up lister-local-ewoc :point '(4)))
-            "M-J" '(lambda ()
-                     (interactive) ; Ignore constraint of same indentation level
-                     (funcall-interactively 'lister-mode-down lister-local-ewoc :point '(4)))
-            "M-H" 'kb/lister-mode-left-sublist
-            "M-L" 'kb/lister-mode-right-sublist
-            "gk" 'lister-mode-forward-same-level
-            "gj" 'lister-mode-backward-same-level
-            "zu" 'lister-mode-up-parent
-            "gh" 'lister-mode-up-parent
-            )
+   "M-k" 'kb/lister-mode-up
+   "M-j" 'kb/lister-mode-down
+   "M-h" 'lister-mode-left
+   "M-l" 'kb/lister-mode-right
+   ;; Use the initial versions of the functions for these
+   "M-K" '(lambda ()
+             (interactive) ; Ignore constraint of same indentation level
+             (funcall-interactively 'lister-mode-up lister-local-ewoc :point '(4)))
+   "M-J" '(lambda ()
+             (interactive) ; Ignore constraint of same indentation level
+             (funcall-interactively 'lister-mode-down lister-local-ewoc :point '(4)))
+   "M-H" 'kb/lister-mode-left-sublist
+   "M-L" 'kb/lister-mode-right-sublist
+   "gk" 'lister-mode-forward-same-level
+   "gj" 'lister-mode-backward-same-level
+   "zu" 'lister-mode-up-parent
+   "gh" 'lister-mode-up-parent
+   )
   (:keymaps 'lister-mode-map
-            :states '(normal visual)
-            "m" 'lister-mode-mark
-            "u" 'lister-mode-unmark
-            "U" 'lister-mode-unmark-all
-            )
+   :states '(normal visual)
+   "m" 'lister-mode-mark
+   "u" 'lister-mode-unmark
+   "U" 'lister-mode-unmark-all
+   )
   :init
   (require 'lister-mode) ; Require since this proves the "core" definitions for the functions below
   :config
