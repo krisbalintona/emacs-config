4a6b80ecdb363dfe92959e3c3bf175f855dc7309
Kristoffer Balintona <krisbalintona@gmail.com>
2022-12-16 02:22:39 -0500

    Install lsp-grammarly

--- recipes/checking-grammar-rcp.el
+++ recipes/checking-grammar-rcp.el
@@ -36,19 +36,22 @@
     (when (require 'popup nil t)
       ;; Do not interrupt current popup
       (unless (or popup-instances
-                 ;; suppress popup after type `C-g` .
-                 (memq last-command '(keyboard-quit)))
+                  ;; suppress popup after type `C-g` .
+                  (memq last-command '(keyboard-quit)))
         (let ((msg (langtool-details-error-message overlays)))
           (popup-tip msg)))))
   :config
   (pretty-hydra-define hydra:langtool
-    (:color green :hint t :foreign-keys run :quit-key "q" :exit t)
-    ("Correct grammar"
-     (("b" #'langtool-check-buffer "Check")
-      ("c" #'langtool-correct-buffer "Correct")
-      ("d" #'langtool-check-done "Done"))
-     ))
-  )
+                       (:color green :hint t :foreign-keys run :quit-key "q" :exit t)
+                       ("Correct grammar"
+                        (("b" #'langtool-check-buffer "Check")
+                         ("c" #'langtool-correct-buffer "Correct")
+                         ("d" #'langtool-check-done "Done"))
+                        )))
+
+;;; Lsp-grammarly
+(use-package lsp-grammarly
+  :hook (lsp-grammarly-ls-after-open . (lambda () (lsp-ui-mode -1))))
 
 ;;; checking-grammar-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
