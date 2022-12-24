a6f904e735968c96a5c8fda10baa505c44f5d59c
Kristoffer Balintona <krisbalintona@gmail.com>
2022-12-23 19:18:38 -0600

    Override org-hugo--build-toc

--- recipes/org-blogging-rcp.el
+++ recipes/org-blogging-rcp.el
@@ -685,6 +685,119 @@ the current hugo buffer if they do not exist."
         (unless (member (get-buffer (buffer-name)) (buffer-list)) ; Kill buffer unless it already exists
           (kill-buffer))))))
 
+;;;; Fix TOC including tags
+(with-eval-after-load 'ox-hugo
+  (defun kb/org-hugo--build-toc (info &optional n scope local)
+    "Return table of contents as a string.
+
+INFO is a plist used as a communication channel.
+
+Optional argument N, when non-nil, is a positive integer
+specifying the depth of the table.
+
+When optional argument SCOPE is non-nil, build a table of
+contents according to the specified element.
+
+When optional argument LOCAL is non-nil, build a table of
+contents according to the current heading.
+
+My version of this function has tags processed through
+`org-hugo-tag-processing-functions' before they are added to the
+TOC."
+    (let* ((toc-heading
+            (unless local
+              (format "\n<div class=\"heading\">%s</div>\n"
+                      (org-html--translate "Table of Contents" info))))
+           (current-level nil)
+           (toc-items
+            (mapconcat
+             (lambda (heading)
+               (let* ((level-raw (org-export-get-relative-level heading info))
+                      (level (if scope
+                                 (let* ((current-level-inner
+                                         (progn
+                                           (unless current-level
+                                             (setq current-level level-raw))
+                                           current-level))
+                                        (relative-level
+                                         (1+ (- level-raw current-level-inner))))
+                                   ;; (message (concat "[ox-hugo build-toc DBG] "
+                                   ;;                  "current-level-inner:%d relative-level:%d")
+                                   ;;          current-level-inner relative-level)
+                                   relative-level)
+                               level-raw))
+                      (indentation (make-string (* 4 (1- level)) ?\s))
+                      (todo (and (org-hugo--plist-get-true-p info :with-todo-keywords)
+                                 (org-element-property :todo-keyword heading)))
+                      (todo-str (if todo
+                                    (concat (org-hugo--todo todo info) " ")
+                                  ""))
+                      (heading-num-list (org-export-get-headline-number heading info))
+                      (number (if heading-num-list
+                                  ;; (message "[ox-hugo TOC DBG] heading-num-list: %S" heading-num-list)
+                                  (org-hugo--get-heading-number heading info :toc)
+                                ""))
+                      (toc-entry
+                       (format "[%s%s](#%s)"
+                               todo-str
+                               (org-export-data-with-backend
+                                (org-export-get-alt-title heading info)
+                                (org-export-toc-entry-backend 'hugo)
+                                info)
+                               (org-hugo--get-anchor heading info)))
+                      (tags (and (plist-get info :with-tags)
+                                 (not (eq 'not-in-toc (plist-get info :with-tags)))
+                                 (let ((tags (org-export-get-tags heading info)))
+                                   ;; I added these two lines to have tags
+                                   ;; properly processed
+                                   (dolist (fn org-hugo-tag-processing-functions tags)
+                                     (setq tags (funcall fn tags info)))
+                                   (and tags
+                                        (format ":%s:"
+                                                (mapconcat #'identity tags ":")))))))
+                 ;; (message "[ox-hugo build-toc DBG] level:%d, number:%s" level number)
+                 ;; (message "[ox-hugo build-toc DBG] indentation: %S" indentation)
+                 ;; (message "[ox-hugo build-toc DBG] todo: %s | %s" todo todo-str)
+                 (concat indentation "- " number toc-entry tags)))
+             (org-export-collect-headlines info n scope)
+             "\n"))                       ;Newline between TOC items
+           ;; Remove blank lines from in-between TOC items, which can
+           ;; get introduced when using the "UNNUMBERED: t" heading
+           ;; property.
+           (toc-items (org-string-nw-p
+                       (replace-regexp-in-string "\n\\{2,\\}" "\n" toc-items))))
+      ;; (message "[ox-hugo build-toc DBG] toc-items:%s" toc-items)
+      (when toc-items
+        (let ((toc-classes '("toc" "ox-hugo-toc"))
+              ;; `has-section-numbers' is non-nil if section numbers are
+              ;; present for even one heading.
+              (has-section-numbers (string-match-p "^\\s-*\\-\\s-<span class=\"section\\-num\"" toc-items)))
+          (when has-section-numbers
+            (push "has-section-numbers" toc-classes))
+          (when local
+            (push "local" toc-classes))
+          (concat (format "<div class=\"%s\">\n" (string-join (reverse toc-classes) " "))
+                  (unless (org-hugo--plist-get-true-p info :hugo-goldmark)
+                    "<div></div>\n") ;This is a nasty workaround till Hugo/Blackfriday support
+                  toc-heading    ;wrapping Markdown in HTML div's.
+                  "\n"
+                  toc-items ;https://github.com/kaushalmodi/ox-hugo/issues/93
+                  "\n\n"
+                  "</div>\n"
+                  ;; Special comment that can be use to filter out the TOC
+                  ;; from .Summary in Hugo templates.
+                  ;;
+                  ;;     {{ $summary_splits := split .Summary "<!--endtoc-->" }}
+                  ;;     {{ if eq (len $summary_splits) 2 }}
+                  ;;         <!-- If that endtoc special comment is present, output only the part after that comment as Summary. -->
+                  ;;         {{ index $summary_splits 1 | safeHTML }}
+                  ;;     {{ else }}
+                  ;;         <!-- Print the whole Summary if endtoc special comment is not found. -->
+                  ;;         {{ .Summary }}
+                  ;;     {{ end }}
+                  "<!--endtoc-->\n")))))
+  (advice-add 'org-hugo--build-toc :override #'kb/org-hugo--build-toc))
+
 ;;; org-blogging-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'org-blogging-rcp)
