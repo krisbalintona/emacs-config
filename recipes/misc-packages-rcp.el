51e7a31e1c7cca081b9c9cc048609c9b05015054
Kristoffer Balintona <krisbalintona@gmail.com>
2022-12-31 17:03:39 -0600

    Install engine-mode

--- recipes/misc-packages-rcp.el
+++ recipes/misc-packages-rcp.el
@@ -420,6 +420,61 @@ displayed."
   :custom
   (logos-outlines-are-pages nil))
 
+;;; Engine-mode
+;; Send arbitrary search engine queries to your browser from within Emacs
+(use-package engine-mode
+  :custom
+  (engine/browser-function 'browse-url-generic)
+  :init
+  (engine-mode)
+  :config
+  (defengine amazon
+    "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
+
+  (defengine duckduckgo
+    "https://duckduckgo.com/?q=%s"
+    :keybinding "d")
+
+  (defengine github
+    "https://github.com/search?ref=simplesearch&q=%s")
+
+  (defengine google
+    "https://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"
+    :keybinding "g")
+
+  (defengine google-images
+    "https://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
+
+  (defengine google-maps
+    "https://maps.google.com/maps?q=%s"
+    :docstring "Mappin' it up.")
+
+  (defengine project-gutenberg
+    "https://www.gutenberg.org/ebooks/search/?query=%s")
+
+  (defengine qwant
+    "https://www.qwant.com/?q=%s")
+
+  (defengine stack-overflow
+    "https://stackoverflow.com/search?q=%s")
+
+  (defengine twitter
+    "https://twitter.com/search?q=%s")
+
+  (defengine wikipedia
+    "https://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
+    :keybinding "w"
+    :docstring "Searchin' the wikis.")
+
+  (defengine wiktionary
+    "https://www.wikipedia.org/search-redirect.php?family=wiktionary&language=en&go=Go&search=%s")
+
+  (defengine wolfram-alpha
+    "https://www.wolframalpha.com/input/?i=%s")
+
+  (defengine youtube
+    "https://www.youtube.com/results?aq=f&oq=&search_query=%s"))
+
 ;;; Built-in Emacs modes/packages
 (use-package emacs
   :straight nil
