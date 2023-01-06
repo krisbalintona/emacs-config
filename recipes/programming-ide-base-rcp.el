a8ac3d5cdf5ddc53c5b3e2d3a82ecfd75ab9262c
Kristoffer Balintona <krisbalintona@gmail.com>
2023-01-06 00:07:48 -0600

    Use fourmolu to format haskell buffers

--- recipes/programming-ide-base-rcp.el
+++ recipes/programming-ide-base-rcp.el
@@ -12,11 +12,11 @@
 ;;; Quickrun
 (use-package quickrun
   :general (:prefix "<f2>"
-                    "<f2>" 'quickrun
-                    "<f3>" '(lambda ()
-                              (interactive)
-                              (let ((quickrun-focus-p t))
-                                (quickrun-shell))))
+            "<f2>" 'quickrun
+            "<f3>" '(lambda ()
+                       (interactive)
+                       (let ((quickrun-focus-p t))
+                         (quickrun-shell))))
   :custom
   (quickrun-focus-p nil))
 
@@ -41,6 +41,7 @@
    ;; Major modes
    (alist-get 'lua-mode apheleia-mode-alist) '(stylua)
    (alist-get 'ruby-mode apheleia-mode-alist) '(rufo)
+   (alist-get 'haskell-mode apheleia-mode-alist) '(fourmolu)
    ;; Formatters
    (alist-get 'black apheleia-formatters) '("black" "-l 80" "-")
    (alist-get 'google-java-format apheleia-formatters)
@@ -49,18 +50,19 @@
    `("stylua" "--indent-type" "Spaces" "--line-endings" "Unix"  "--column-width" ,(number-to-string fill-column) "--quote-style" "ForceDouble" "-")
    (alist-get 'latexindent apheleia-formatters)
    '("latexindent" "--cruft=/tmp/" "--logfile" "indent.log")
-   (alist-get 'rufo apheleia-formatters) '("rufo" "--simple-exit" "--filename" filepath)))
+   (alist-get 'rufo apheleia-formatters) '("rufo" "--simple-exit" "--filename" filepath)
+   (alist-get 'fourmolu apheleia-formatters) '("fourmolu")))
 
 ;;; Devdocs
 ;; Viewing documentation within Emacs.
 (use-package devdocs-browser
   :general (:keymaps '(eglot-mode-map lsp-mode-map lsp-bridge-mode-map)
-                     :prefix "H-d"
-                     "h" '(devdocs-browser-open :wk "Open")
-                     "H" '(devdocs-browser-open-in :wk "Open-in")
-                     "i" '(devdocs-browser-install-doc :wk "Install")
-                     "d" '(devdocs-browser-download-offline-data :wk "Download")
-                     "D" '(devdocs-browser-upgrade-all-docs :wk "Upgrade")))
+            :prefix "H-d"
+            "h" '(devdocs-browser-open :wk "Open")
+            "H" '(devdocs-browser-open-in :wk "Open-in")
+            "i" '(devdocs-browser-install-doc :wk "Install")
+            "d" '(devdocs-browser-download-offline-data :wk "Download")
+            "D" '(devdocs-browser-upgrade-all-docs :wk "Upgrade")))
 
 ;;; Dash-docs
 ;; Viewing of documentation via browser.
@@ -69,7 +71,7 @@
                         :host github
                         :repo "dash-docs-el/dash-docs"
                         :fork (:host github
-                                     :repo "krisbalintona/dash-docs"))
+                               :repo "krisbalintona/dash-docs"))
   :hook ((python-mode   . (lambda () (setq-local dash-docs-common-docsets '("Python 3"))))
          (haskell-mode  . (lambda () (setq-local dash-docs-common-docsets '("Haskell"))))
          (js2-mode      . (lambda () (setq-local dash-docs-common-docsets '("JavaScript"))))
@@ -121,13 +123,13 @@
   :requires tree-sitter
   :straight (turbo-log :type git :host github :repo "artawower/turbo-log.el")
   :general (:prefix "H-;"
-                    ";" 'turbo-log-print
-                    "i" 'turbo-log-print-immediately
-                    "h" 'turbo-log-comment-all-logs
-                    "s" 'turbo-log-uncomment-all-logs
-                    "[" 'turbo-log-paste-as-logger
-                    "]" 'turbo-log-paste-as-logger-immediately
-                    "d" 'turbo-log-delete-all-logs)
+            ";" 'turbo-log-print
+            "i" 'turbo-log-print-immediately
+            "h" 'turbo-log-comment-all-logs
+            "s" 'turbo-log-uncomment-all-logs
+            "[" 'turbo-log-paste-as-logger
+            "]" 'turbo-log-paste-as-logger-immediately
+            "d" 'turbo-log-delete-all-logs)
   :custom
   (turbo-log-msg-format-template "\"tk %s\"") ; "tk" is a rare bigram!
   (turbo-log-allow-insert-without-tree-sitter-p t))
