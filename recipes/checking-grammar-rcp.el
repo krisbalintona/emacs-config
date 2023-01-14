5783622cd24b5667704e3c5ae8cbc6e5fa16b739
Kristoffer Balintona <krisbalintona@gmail.com>
2023-01-14 03:20:45 -0600

    Don’t automatically load flymake-languagetool in org-mode buffers

--- recipes/checking-grammar-rcp.el
+++ recipes/checking-grammar-rcp.el
@@ -55,7 +55,6 @@
 (use-package flymake-languagetool
   :general (:keymaps 'flymake-mode-map
             (general-chord "``") 'flymake-languagetool-correct-dwim)
-  :hook (org-mode . flymake-languagetool-maybe-load)
   :custom
   ;; See https://github.com/languagetool-org/languagetool for installation
   ;; instructions
