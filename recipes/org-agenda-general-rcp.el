dc61c4d2dc17e4d30c35007090e0df830f3cb0d8
Kristoffer Balintona <krisbalintona@gmail.com>
2023-01-18 00:48:46 -0600

    Install and configure org-depend and org-edna

--- recipes/org-agenda-general-rcp.el
+++ recipes/org-agenda-general-rcp.el
@@ -253,6 +253,99 @@ This function makes sure that dates are aligned for easy reading."
   (org-pomodoro-keep-killed-pomodoro-time t)
   (org-pomodoro-manual-break t))     ; Allows for a workflow of going "overtime"
 
+;;; Org-depend
+;; Add blocking and triggering actions when an org-todo state is changed.
+(use-package org-depend
+  :demand
+  :after org-agenda
+  :commands kb/consult-org-id-get-create)
+
+;;; Org-edna
+;; Also look at `org-edna' with `org-linker-edna'
+;; (https://github.com/toshism/org-linker-edna) (the second of which requires
+;; `org-linker': https://github.com/toshism/org-linker). `org-super-links' can
+;; be added to see which tasks are being blocked by the current task. See
+;; https://karl-voit.at/2021/01/23/org-linker-edna/ for sample workflow
+(use-package org-edna
+  :after org
+  :diminish
+  :general
+  (:keymaps 'org-mode-map
+   "C-c d" 'kb/consult-org-depend)
+  (:keymaps 'org-agenda-mode-map
+   "C-c d" 'kb/consult-org-agenda-depend)
+  :init
+  (org-edna-mode)
+  :config
+  (with-eval-after-load 'consult
+    (defun kb/consult-org-depend--add-id (new-id)
+      "Add an ID to the current heading’s BLOCKER property.
+If none exists, automatically create the BLOCKER property. Code
+based off of `org-linker-edna’."
+      (let* ((value (org-entry-get (point) "BLOCKER"))
+             (formatted-new-id
+              (progn
+                (unless (org-id-find new-id)
+                  (error "This ID (%s) does not exist!" new-id))
+                (list (concat "\"id:" new-id "\""))))
+             (existing-ids
+              ;; Get IDs if they exist in proper `org-edna' syntax as the value
+              ;; of the BLOCKER property
+              (when (and value (string-match "ids(\\([^\\)]*\\)).*" value))
+                (split-string (match-string 1 value))))
+             (all-ids (string-join (seq-uniq (append existing-ids formatted-new-id)) " "))
+             (new-value (concat "ids(" all-ids ")")))
+        (org-set-property "BLOCKER" new-value)))
+
+    (defun kb/consult-org-depend (&optional match)
+      "Create a dependency for the `org-todo’ at point.
+A dependency is defined by `org-depend’s `BLOCKER’ property. IDs
+are created in the todo dependency with `org-id-get-create’.
+MATCH is a query sent to `org-map-entries’."
+      (interactive)
+      (let ((current-heading (org-get-heading))
+            new-id dependency)
+        (if (not (org-entry-is-todo-p))
+            ;; Error if not currently on an `org-todo'
+            (error "Not on an `org-todo’ heading!")
+          ;; Add and ID to the dependency if necessary
+          (save-excursion
+            (consult-org-agenda match)
+            (setq dependency (org-get-heading))
+            (when (equal current-heading dependency)
+              (error "Cannot depend on the same `org-todo’!"))
+            (setq new-id (org-id-get-create)))
+          ;; Modify the BLOCKER property of the current todo
+          (kb/consult-org-depend--add-id new-id)
+          (message "‘%s’ added as a dependency to this todo"
+                   (substring-no-properties dependency)))))
+
+    (defun kb/consult-org-agenda-depend (&optional match)
+      "Create a dependency for the `org-agenda’ item at point.
+See `kb/consult-org-depend’."
+      (interactive)
+      (let* ((bufname-orig (buffer-name))
+             (marker (or (org-get-at-bol 'org-marker)
+                         (org-agenda-error)))
+             (buffer (marker-buffer marker))
+             (pos (marker-position marker))
+             dependency)
+        (org-with-remote-undo buffer
+          (with-current-buffer buffer
+            (save-excursion
+              (goto-char pos)
+              ;; FIXME 2023-01-17: Janky workaround. Remove all
+              ;; `consult-after-jump-hook' hooks since we if there is a
+              ;; `recenter' hook then an error will be returned since it'll be
+              ;; attempting to `recenter' a non-present buffer
+              (let ((consult-after-jump-hook nil))
+                (setq dependency (funcall 'kb/consult-org-depend nil match))))))))
+
+    (consult-customize kb/consult-org-depend
+                       :prompt "Select dependency for the heading at point: "
+                       kb/consult-org-agenda-depend
+                       :prompt "Select dependency for this agenda item: ")))
+
 ;;; org-agenda-general-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'org-agenda-general-rcp)
