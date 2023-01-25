6f94f79630d7055eb2f250a67c9e76cd0f056332
Kristoffer Balintona <krisbalintona@gmail.com>
2023-01-25 12:59:47 -0500

    Install sticky-shell

--- recipes/shell-basic-rcp.el
+++ recipes/shell-basic-rcp.el
@@ -16,6 +16,13 @@
   :custom
   (async-shell-command-buffer 'new-buffer)) ; Don't ask, just do
 
+;;; Sticky-shell
+
+(use-package sticky-shell
+  :straight (:type git :host github :repo "andyjda/sticky-shell")
+  :init
+  (sticky-shell-global-mode))
+
 ;;; shell-basic-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'shell-basic-rcp)
