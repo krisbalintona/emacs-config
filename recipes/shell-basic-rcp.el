989180c0d53b5767b2817a204767b3441f97c8cb
Kristoffer Balintona <krisbalintona@gmail.com>
2023-07-15 21:55:24 -0500

    Add configurations for comint and shell

--- recipes/shell-basic-rcp.el
+++ recipes/shell-basic-rcp.el
@@ -9,12 +9,32 @@
 (require 'use-package-rcp)
 (require 'keybinds-general-rcp)
 
+
+;;; Comint
+(use-package comint
+  :elpaca nil
+  :custom
+  (comint-prompt-read-only t)
+  (comint-buffer-maximum-size 9999)
+  (comint-completion-autolist t)
+  :config
+  (setq-default comint-scroll-to-bottom-on-input 'all)
+  (setq-default comint-scroll-to-bottom-on-output 'all)
+  (setq-default comint-input-autoexpand 'input))
+
 ;;; Shell
 ;; Built-in shell
 (use-package shell
   :elpaca nil
   :custom
-  (async-shell-command-buffer 'new-buffer)) ; Don't ask, just do
+  (async-shell-command-buffer 'confirm-kill-process) ; Don't ask, just do
+  (ansi-color-for-comint-mode t)
+  (shell-command-prompt-show-cwd t)     ; Emacs 27.1
+  (shell-input-autoexpand 'input)
+  (shell-highlight-undef-enable t)                   ; Emacs 29.1
+  (shell-has-auto-cd nil)                            ; Emacs 29.1
+  (shell-get-old-input-include-continuation-lines t) ; Emacs 30.1
+  (shell-kill-buffer-on-exit t))                     ; Emacs 29.1
 
 ;;; Topsy
 ;; Stick a shell prompt, defun line, etc to the header line
@@ -42,12 +62,12 @@
                          (font-lock-ensure (pos-bol) (point-at-eol))
                          (buffer-substring (pos-bol) (point-at-eol))))))
      (eshell-mode . (lambda ()
-                     (when (> (window-start) 1)
-                       (save-excursion
-                         (goto-char (window-start))
-                         (goto-char (eshell-previous-prompt 1))
-                         (font-lock-ensure (pos-bol) (point-at-eol))
-                         (buffer-substring (pos-bol) (point-at-eol))))))
+                      (when (> (window-start) 1)
+                        (save-excursion
+                          (goto-char (window-start))
+                          (goto-char (eshell-previous-prompt 1))
+                          (font-lock-ensure (pos-bol) (point-at-eol))
+                          (buffer-substring (pos-bol) (point-at-eol))))))
      (nil . topsy--beginning-of-defun))))
 
 ;;; shell-basic-rcp.el ends here
