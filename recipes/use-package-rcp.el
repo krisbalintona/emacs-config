d2fa781387f9a53b9262d2ee48ce12b84acea494
Kristoffer Balintona <krisbalintona@gmail.com>
2023-08-15 09:26:38 -0500

    Update use-package configuration

--- recipes/use-package-rcp.el
+++ recipes/use-package-rcp.el
@@ -7,16 +7,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
 
-;;; This
-;; (setq use-package-always-ensure t) ; May cause issues with straight.el
-(setq use-package-expand-minimally t)   ; Less verbose
-;; (setq use-package-compute-statistics t) ; Need this at "loadup time" or else errors about undefined variables will appear
-(setq use-package-always-defer t)       ; Always try to defer
-(setq use-package-enable-imenu-support t)
+(use-package use-package
+  :custom
+  ;; (use-package-always-ensure t)   ; Causes issues with external package managers
+  ;; (use-package-expand-minimally t)      ; Less verbose
+  (use-package-always-defer t)          ; Always defer
+  (use-package-enable-imenu-support t)
 
-;; Set use-package-verbose to t for interpreted .emacs, and to nil for
-;; byte-compiled .emacs.elc.
-(setq use-package-verbose (not (bound-and-true-p byte-compile-current-file)))
+  ;; Set use-package-verbose to t for interpreted .emacs, and to nil for
+  ;; byte-compiled .emacs.elc.
+  (use-package-verbose (not (bound-and-true-p byte-compile-current-file))))
 
 ;;; use-package-rcp.el ends here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
