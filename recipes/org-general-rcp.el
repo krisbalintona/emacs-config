9488673ab5ebaa15a4b78c3a0fa941f82ec8cd3c
Kristoffer Balintona <krisbalintona@gmail.com>
2023-08-24 08:08:19 -0500

    Change org-id-link-to-org-use-id to create-if-interactive-and-no-custom-id

--- recipes/org-general-rcp.el
+++ recipes/org-general-rcp.el
@@ -174,7 +174,7 @@ have `org-warning' face."
   (org-attach-archive-delete 'query) ; If subtree is deleted or archived, ask user
   ;; Use timestamps as UUIDs and in attachment directory hierarchy
   (org-id-method 'ts)
-  (org-id-link-to-org-use-id 'use-existing)
+  (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
   (org-attach-id-to-path-function-list
    '(org-attach-id-ts-folder-format
      org-attach-id-uuid-folder-format)))
