84d556146921547be7726ecf4f9e0951c246732e
Kristoffer Balintona <krisbalintona@gmail.com>
2024-02-19 03:10:06 -0500

    Don't use kb/puni-kill-line

--- recipes/buffer-nav-rcp.el
+++ recipes/buffer-nav-rcp.el
@@ -86,7 +86,7 @@
     (let ((map (make-sparse-keymap)))
       (define-key map (kbd "M-d") 'puni-forward-kill-word)
       (define-key map (kbd "M-DEL") 'puni-backward-kill-word)
-      (define-key map [remap kill-line] 'kb/puni-kill-line)
+      (define-key map [remap kill-line] 'puni-kill-line)
       (define-key map [remap backward-sexp] 'puni-backward-sexp)
       (define-key map [remap forward-sexp] 'puni-forward-sexp)
       (define-key map [remap beginning-of-defun] 'puni-beginning-of-sexp)
@@ -104,14 +104,6 @@
       (define-key map (kbd "C-M-}") 'puni-barf-forward)
       map)
     "My own Puni keymap.")
-  (defun kb/puni-kill-line (arg)
-    "A wrapper around `puni-kill-line'.
-Call `org-kill-line' instead when in org-mode. Passes ARG to
-command."
-    (interactive "P")
-    (if (derived-mode-p 'org-mode)
-        (org-kill-line arg)
-      (puni-kill-line arg)))
 
   (define-minor-mode kb/puni-mode
     "Enable keybindings for Puni commands."
