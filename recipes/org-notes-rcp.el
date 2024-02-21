e2d9cd7dcbdaf17e542c11b43cdec8223f882b32
Kristoffer Balintona <krisbalintona@gmail.com>
2024-02-20 22:36:31 -0500

    Add template to denote-prompts

--- recipes/org-notes-rcp.el
+++ recipes/org-notes-rcp.el
@@ -45,7 +45,7 @@
   (denote-directory kb/notes-dir)
   (denote-modules '(xref ffap))
   (denote-known-keywords '("project"))
-  (denote-prompts '(subdirectory title keywords signature))
+  (denote-prompts '(subdirectory title keywords signature template))
   (denote-org-front-matter "#+title: %s
 #+date: %s
 #+filetags: %s
