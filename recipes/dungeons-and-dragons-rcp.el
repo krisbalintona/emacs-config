4f7e33a566feff5dbd0b156e9e2fec10ea90d477
Kristoffer Balintona <krisbalintona@gmail.com>
2024-02-20 21:11:56 -0500

    fix: dungeons-and-dragons-rcp causing startup errors
    Something about requiring ol or org-export-rcp

--- recipes/dungeons-and-dragons-rcp.el
+++ recipes/dungeons-and-dragons-rcp.el
@@ -23,46 +23,45 @@
 ;; My custom configuration for playing Dungeons and Dragons.
 
 ;;; Code:
-(require 'ol)
-(require 'org-export-rcp)
 
 ;;;; Bespoke keywords
-(defcustom kb/dnd-keywords
-  '(("character" . '((((background light)) (:foreground "PaleVioletRed3"))
-                     (((background dark)) (:foreground "PaleVioletRed1"))))
-    ("place" . '((((background light)) (:foreground "DeepSkyBlue"))
-                 (((background dark)) (:foreground "DeepSkyBlue"))))
-    ("designation" . '((((background light)) (:background "orange2"))
-                       (((background dark)) (:background "orange1")))))
-  "Keywords used for for playing Dungeons and Dragons.
+(with-eval-after-load 'org
+  (defcustom kb/dnd-keywords
+    '(("character" . '((((background light)) (:foreground "PaleVioletRed3"))
+                       (((background dark)) (:foreground "PaleVioletRed1"))))
+      ("place" . '((((background light)) (:foreground "DeepSkyBlue"))
+                   (((background dark)) (:foreground "DeepSkyBlue"))))
+      ("designation" . '((((background light)) (:background "orange2"))
+                         (((background dark)) (:background "orange1")))))
+    "Keywords used for for playing Dungeons and Dragons.
 Is an alist from keyword, as a string, to the foreground of the
 link whose path is that keyword.")
 
-(defface kb/dnd-faces-missing '((((background light)) (:strike-through "red"))
-                                (((background dark)) (:strike-through "red")))
-  "Face used when keyword does not exist.
+  (defface kb/dnd-faces-missing '((((background light)) (:strike-through "red"))
+                                  (((background dark)) (:strike-through "red")))
+    "Face used when keyword does not exist.
 Is the fallback face when using a keyword that does not exist in
 `kb/dnd-keywords.'")
 
-(defun kb/dnd-link-face (keyword)
-  "Calculate face used with `dnd' org link type.
+  (defun kb/dnd-link-face (keyword)
+    "Calculate face used with `dnd' org link type.
 KEYWORD is a string that should be present in `kb/dnd-keywords'.
 If not, uses the face denoted by `kb/dnd-faces-missing'.
 
 Used in `org-link-set-parameters'."
-  (or (cdr (assoc keyword kb/dnd-keywords))
-      'kb/dnd-faces-missing))
+    (or (cdr (assoc keyword kb/dnd-keywords))
+        'kb/dnd-faces-missing))
 
-(defun kb/dnd-link-complete (&optional _arg)
-  "Function for completions with `dnd' org link type.
+  (defun kb/dnd-link-complete (&optional _arg)
+    "Function for completions with `dnd' org link type.
 Used in `org-link-set-parameters'."
-  (concat "dnd:"
-          (completing-read "Which keyword?: "
-                           (s-split-words (key-description kb/dnd-keywords)))))
+    (concat "dnd:"
+            (completing-read "Which keyword?: "
+                             (s-split-words (key-description kb/dnd-keywords)))))
 
-(org-link-set-parameters "dnd"
-                         :face #'kb/dnd-link-face
-                         :complete #'kb/dnd-link-complete)
+  (org-link-set-parameters "dnd"
+                           :face #'kb/dnd-link-face
+                           :complete #'kb/dnd-link-complete))
 
 (provide 'dungeons-and-dragons-rcp)
 ;;; dungeons-and-dragons-rcp.el ends here
