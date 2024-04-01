8bf0461e09d9efa04eede08f29780b0b517c4ff2
Kristoffer Balintona <krisbalintona@gmail.com>
2024-03-31 20:26:20 -0400

    Save playback position when closing videos opened by mpv.el

--- recipes/org-notes-other-rcp.el
+++ recipes/org-notes-other-rcp.el
@@ -528,7 +528,9 @@ Uses the current annotation at point's ID."
   ;; NOTE 2024-03-31: See
   ;; https://github.com/kljohann/mpv.el/issues/31#issuecomment-1856491594 for
   ;; why I use the latest GitHub version
-  :vc (:rev :newest))
+  :vc (:rev :newest)
+  :custom
+  (mpv-default-options (list "--save-position-on-quit")))
 
 ;;;;; Custom MPV notes
 (with-eval-after-load 'org
