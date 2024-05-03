2effa38b0ed73ba2985048dc677ccf50f6efbb37
Kristoffer Balintona <krisbalintona@gmail.com>
2024-05-02 20:42:54 -0500

    Deprecated mu4e-compose-signature

--- recipes/email-mu4e-rcp.el
+++ recipes/email-mu4e-rcp.el
@@ -134,7 +134,6 @@
   (mu4e-compose-signature-auto-include t)
   (mu4e-compose-hidden-headers nil)   ; See all headers
   (mu4e-compose-dont-reply-to-self t)
-  (mu4e-compose-signature message-signature)
   (mu4e-compose-complete-only-personal t)
   (mu4e-compose-switch 'window)
 
