49039d36cc36877333f65b30107736a05c1996bd
Kristoffer Balintona <krisbalintona@gmail.com>
2024-09-05 00:35:00 -0500

    Update Dired ls listings
    The -v flag sorts listings alphanumerically according to how numbers appear in
    natural text, that is, "31" is sorted after "4."

--- recipes/programming-directories-rcp.el
+++ recipes/programming-directories-rcp.el
@@ -42,7 +42,7 @@
   (dired-dwim-target t)                 ; Guess default target directory?
   (dired-hide-details-hide-symlink-targets nil) ; Don't hide symlink targets
   (dired-kill-when-opening-new-dired-buffer t)  ; Basically `dired-single'
-  (dired-listing-switches "-alhg") ; Flags `dired' passes to `ls'; more useful and readable
+  (dired-listing-switches "-alhgv") ; Flags `dired' passes to `ls'
   (dired-omit-verbose nil)
   ;; Always copy/delete recursively?
   (dired-recursive-copies  'always)
