beff09e31b8394d966256a9df19406046ecc07b1
Kristoffer Balintona <krisbalintona@gmail.com>
2024-09-19 12:25:46 -0500

    programming-projects-rcp: Change `vc-git-root-log-format'
    Shows human-readable relative date/time and changed the order of components a
    bit.

--- recipes/programming-projects-rcp.el
+++ recipes/programming-projects-rcp.el
@@ -375,15 +375,18 @@ With a prefix argument, show NLINES of context."
   (vc-git-log-edit-summary-max-len (+ 70 (length "Summary")))
   (vc-git-diff-switches              ; Have diff headers look similar to Magit's
    '("--patch-with-stat" "--histogram"))
-  (vc-git-root-log-format               ; Taken from Prot
-   `("%d %h %ai %an: %s"
-     ;; The first shy group matches the characters drawn by --graph.
-     ;; We use numbered groups because `log-view-message-re' wants the
-     ;; revision number to be group 1.
+  (vc-git-root-log-format
+   `("%h %ad (%ar) %aN%d%n  %s"
+     ;; The first shy group matches the characters drawn by --graph. We use
+     ;; numbered groups because `log-view-message-re' wants the revision number
+     ;; to be group 1.
      ,(concat "^\\(?:[*/\\|]+\\)\\(?:[*/\\| ]+\\)?"
-              "\\(?2: ([^)]+) \\)?\\(?1:[0-9a-z]+\\) "
-              "\\(?4:[0-9]\\{4\\}-[0-9-]\\{4\\}[0-9\s+:-]\\{16\\}\\) "
-              "\\(?3:.*?\\):")
+              "\\(?1:[0-9a-z]+\\)"      ; %h
+              " "
+              "\\(?4:[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\} (.*? ago)\\)?" ; %ad (%ar)
+              " "
+              "\\(?3:\\(?:[[:alpha:]]+\\.?[\n ]\\)+\\)" ; %aN
+              "\\(?2:([^)]+)\\)?")                      ; %d
      ((1 'log-view-message)
       (2 'change-log-list nil lax)
       (3 'change-log-name)
