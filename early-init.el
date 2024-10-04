46dd667ecd9a4005a6af74ecce52896a0c9219ea
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-03 19:00:04 -0500

    Create krisb-package-management.el

--- early-init.el
+++ early-init.el
@@ -76,7 +76,41 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; Shave seconds off startup time by starting the scratch buffer in
-;; `fundamental-mode'
-(setopt initial-major-mode 'fundamental-mode
-        initial-scratch-message "Hello 👋")
+;; I defer garbage collection until after starting Emacs. See my configuration
+;; of `gcmh-mode'.
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent loading any packages prior to init.el. Speeds up startup but packages
+;; must be initizlied by `package-initialize' before any are needed.
+;; Importnatly, if this is nil, make sure to expressly call `package-initialize'
+;; later (and before I need non-built-in packages) to make other packages
+;; available.
+(setq package-enable-at-startup nil)
+
+;; NOTE 2024-10-03: From Doom Emacs. If we're disabling these graphical
+;; elements, it's faster to do so before they've been initialized.
+;; HACK: I intentionally avoid calling `menu-bar-mode', `tool-bar-mode', and
+;;   `scroll-bar-mode' because their manipulation of frame parameters can
+;;   trigger/queue a superfluous (and expensive, depending on the window system)
+;;   frame redraw at startup. The variables must be set to `nil' as well so
+;;   users don't have to call the functions twice to re-enable them.
+(push '(menu-bar-lines . 0)   default-frame-alist)
+(push '(tool-bar-lines . 0)   default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+
+;; Frame defaults
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize t
+      frame-title-format '("%b")
+      ring-bell-function 'ignore
+      use-dialog-box t                  ; For mouse events
+      use-file-dialog nil
+      use-short-answers nil
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-x-resources t     ; REVIEW 2024-10-03: Not sure the precise effect
+      inhibit-startup-echo-area-message user-login-name ; Read the docstring
+      inhibit-startup-buffer-menu t)
