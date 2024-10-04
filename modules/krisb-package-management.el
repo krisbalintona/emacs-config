1bb2bfd8d4a3a2f3e0332b2d26cda17aeee2e411
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-03 19:59:23 -0500

    Make sure use-package is installed

--- modules/krisb-package-management.el
+++ modules/krisb-package-management.el
@@ -1,17 +1,26 @@
 ;; Initialize package resources
-(setq package-archives '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
-                         ("gnu-elpa-devel" . "https://elpa.gnu.org/devel/")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                         ("melpa" . "https://melpa.org/packages/"))
-      package-archive-priorities '(("gnu-elpa" . 4)
-                                   ("melpa" . 3)
-                                   ("nongnu" . 2)
-                                   ("gnu-elpa-devel" . 1))
-      package-install-upgrade-built-in t
-      package-pinned-packages nil)
+(setopt package-archives '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+                           ("gnu-elpa-devel" . "https://elpa.gnu.org/devel/")
+                           ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+                           ("melpa" . "https://melpa.org/packages/"))
+        package-archive-priorities '(("gnu-elpa" . 4)
+                                     ("melpa" . 3)
+                                     ("nongnu" . 2)
+                                     ("gnu-elpa-devel" . 1))
+        package-install-upgrade-built-in t
+        package-pinned-packages nil)
 
 ;; We have to manually initialize package because in `early-init.el' we set
 ;; `package-enable-at-startup' to nil to speed up startup, like Doom Emacs does.
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+;; Although `use-package' is built-in starting Emacs 29.1, I should make sure
+;; it's installed just in case I test/use an earlier Emacs version
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; Then ensure use-package is available at compile time
+(eval-when-compile
+  (require 'use-package))
