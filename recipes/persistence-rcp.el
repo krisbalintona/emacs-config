e3087baa3280ee3cf29dd88b64ebc290fea558bd
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-13 16:55:09 -0500

    Remove register-alist from `desktop-globals-to-save'

--- recipes/persistence-rcp.el
+++ recipes/persistence-rcp.el
@@ -80,15 +80,28 @@
   :hook (after-init . desktop-save-mode)
   :custom
   (desktop-load-locked-desktop 'check-pid)
+  (desktop-save 'if-exists)
+  (desktop-auto-save-timeout 3)
   (desktop-files-not-to-save
    (rx (or (regexp "\\(\\`/[^/:]*:\\|(ftp)\\'\\)")
            ;; Don't save files from other Emacs repos because sometimes they
            ;; have local variables that mess with desktop's loading of files
            (literal (expand-file-name "emacs-repos/" "~")))))
-  (desktop-auto-save-timeout 3)
+  (desktop-globals-to-save '(desktop-missing-file-warning
+                             tags-file-name
+                             tags-table-list
+                             search-ring
+                             regexp-search-ring
+                             ;; REVIEW 2024-10-13: The previews in
+                             ;; `jump-to-register' cause errors when trying to
+                             ;; visit a buffer or window which no longer exists.
+                             ;; Removing it from the saved globals list is the
+                             ;; workaround I choose for now.
+                             ;; register-alist
+                             file-name-history))
+
   (desktop-restore-forces-onscreen nil)
   (desktop-restore-frames t)
-  (desktop-save 'if-exists)
   (desktop-restore-in-current-display nil))
 
 ;;;; Super-save
