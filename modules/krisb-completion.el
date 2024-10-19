2fb2375ed62a96cc62475e44b8d32c3b25511ed8
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-19 00:49:39 -0500

    Reorganize krisb-completion.el

--- modules/krisb-completion.el
+++ modules/krisb-completion.el
@@ -110,6 +110,50 @@
   :config
   (prescient-persist-mode 1))
 
+;;; Orderless
+;; Alternative and powerful completion style (i.e. filters candidates)
+(use-package orderless
+  :custom
+  (completion-styles '(orderless flex))
+  (orderless-matching-styles
+   '(orderless-regexp
+     orderless-prefixes
+     orderless-initialism
+     ;; orderless-literal
+     ;; orderless-flex
+     ;; orderless-without-literal          ; Recommended for dispatches instead
+     ))
+  (orderless-component-separator 'orderless-escapable-split-on-space)
+  (orderless-style-dispatchers '(krisb-orderless-consult-dispatch))
+  :config
+  ;; Eglot forces `flex' by default.
+  (add-to-list 'completion-category-overrides '(eglot (styles . (orderless flex))))
+
+  ;; Taken from Doom
+  (defun krisb-orderless-consult-dispatch (pattern _index _total)
+    "Basically `orderless-affix-dispatch-alist' but with prefixes too."
+    (cond
+     ;; Ensure $ works with Consult commands, which add disambiguation suffixes
+     ((string-suffix-p "$" pattern)
+      `(orderless-regexp . ,(concat (substring pattern 0 -1) "[\x200000-\x300000]*$")))
+     ;; Ignore single !
+     ((string= "!" pattern) `(orderless-literal . ""))
+     ;; Without literal
+     ((string-prefix-p "!" pattern) `(orderless-without-literal . ,(substring pattern 1)))
+     ((string-suffix-p "!" pattern) `(orderless-without-literal . ,(substring pattern 1 -1)))
+     ;; Character folding
+     ((string-prefix-p "%" pattern) `(char-fold-to-regexp . ,(substring pattern 1)))
+     ((string-suffix-p "%" pattern) `(char-fold-to-regexp . ,(substring pattern 0 -1)))
+     ;; Initialism matching
+     ((string-prefix-p "," pattern) `(orderless-initialism . ,(substring pattern 1)))
+     ((string-suffix-p "," pattern) `(orderless-initialism . ,(substring pattern 0 -1)))
+     ;; Literal matching
+     ((string-prefix-p "=" pattern) `(orderless-literal . ,(substring pattern 1)))
+     ((string-suffix-p "=" pattern) `(orderless-literal . ,(substring pattern 0 -1)))
+     ;; Flex matching
+     ((string-prefix-p "~" pattern) `(orderless-flex . ,(substring pattern 1)))
+     ((string-suffix-p "~" pattern) `(orderless-flex . ,(substring pattern 0 -1))))))
+
 ;;; Vertico
 ;;;; Itself
 (use-package vertico
@@ -182,50 +226,6 @@
      (eglot (styles prescient flex))))
   :config
   (vertico-prescient-mode 1))
-
-;;; Orderless
-;; Alternative and powerful completion style (i.e. filters candidates)
-(use-package orderless
-  :custom
-  (completion-styles '(orderless flex))
-  (orderless-matching-styles
-   '(orderless-regexp
-     orderless-prefixes
-     orderless-initialism
-     ;; orderless-literal
-     ;; orderless-flex
-     ;; orderless-without-literal          ; Recommended for dispatches instead
-     ))
-  (orderless-component-separator 'orderless-escapable-split-on-space)
-  (orderless-style-dispatchers '(krisb-orderless-consult-dispatch))
-  :config
-  ;; Eglot forces `flex' by default.
-  (add-to-list 'completion-category-overrides '(eglot (styles . (orderless flex))))
-
-  ;; Taken from Doom
-  (defun krisb-orderless-consult-dispatch (pattern _index _total)
-    "Basically `orderless-affix-dispatch-alist' but with prefixes too."
-    (cond
-     ;; Ensure $ works with Consult commands, which add disambiguation suffixes
-     ((string-suffix-p "$" pattern)
-      `(orderless-regexp . ,(concat (substring pattern 0 -1) "[\x200000-\x300000]*$")))
-     ;; Ignore single !
-     ((string= "!" pattern) `(orderless-literal . ""))
-     ;; Without literal
-     ((string-prefix-p "!" pattern) `(orderless-without-literal . ,(substring pattern 1)))
-     ((string-suffix-p "!" pattern) `(orderless-without-literal . ,(substring pattern 1 -1)))
-     ;; Character folding
-     ((string-prefix-p "%" pattern) `(char-fold-to-regexp . ,(substring pattern 1)))
-     ((string-suffix-p "%" pattern) `(char-fold-to-regexp . ,(substring pattern 0 -1)))
-     ;; Initialism matching
-     ((string-prefix-p "," pattern) `(orderless-initialism . ,(substring pattern 1)))
-     ((string-suffix-p "," pattern) `(orderless-initialism . ,(substring pattern 0 -1)))
-     ;; Literal matching
-     ((string-prefix-p "=" pattern) `(orderless-literal . ,(substring pattern 1)))
-     ((string-suffix-p "=" pattern) `(orderless-literal . ,(substring pattern 0 -1)))
-     ;; Flex matching
-     ((string-prefix-p "~" pattern) `(orderless-flex . ,(substring pattern 1)))
-     ((string-suffix-p "~" pattern) `(orderless-flex . ,(substring pattern 0 -1))))))
 
 ;;; Provide
 (provide 'krisb-completion)
