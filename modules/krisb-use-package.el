a92a97f2898fa9e699974c7ec2a0215849c76a86
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-18 19:53:32 -0500

    Set use-package-always-defer to nil

--- modules/krisb-use-package.el
+++ modules/krisb-use-package.el
@@ -1,6 +1,6 @@
 (setopt use-package-always-ensure t
         use-package-expand-minimally t  ; Verbosity of use-package macro
-        use-package-always-defer t)     ; Always defer
+        use-package-always-defer nil)
 
 ;; Only be verbose when interpreted, otherwise errors are caught at compile time
 (setopt use-package-verbose (not (bound-and-true-p byte-compile-current-file)))
