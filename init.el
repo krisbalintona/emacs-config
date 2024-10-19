21916cf5152d087296eb393df460665f22f134b5
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-18 19:56:32 -0500

    Move use-package config to init.el

--- init.el
+++ init.el
@@ -7,6 +7,16 @@
 (setopt initial-major-mode 'fundamental-mode
         initial-scratch-message "Hello 👋")
 
+(setopt use-package-always-ensure t
+        use-package-expand-minimally t  ; Verbosity of use-package macro
+        use-package-always-defer nil)
+
+;; Only be verbose when interpreted, otherwise errors are caught at compile time
+(setopt use-package-verbose (not (bound-and-true-p byte-compile-current-file)))
+
+;; Compile statistics to be shown in `use-package-report'
+(setopt use-package-compute-statistics t)
+
 ;;; Libraries
 (require 'krisb-common)
 
