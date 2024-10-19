6b0e85c3357ebb3452e33c7d519edf4aeffa73d6
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-19 07:14:33 -0500

    Transition org-super-agenda

--- modules/krisb-org-agenda.el
+++ modules/krisb-org-agenda.el
@@ -247,7 +247,7 @@
   ;; https://github.com/psamim/dotfiles/blob/master/doom/config.el#L213
   (defun krisb-org-agenda-format-date-aligned (date)
     "Format a DATE string for display in the agenda or timeline.
-This function makes sure that dates are aligned for easy reading."
+  This function makes sure that dates are aligned for easy reading."
     (require 'cal-iso)
     (let* ((dayname (calendar-day-name date 1 nil))
            (day (cadr date))
@@ -283,6 +283,134 @@ See ((org) Filtering/limiting agenda items)."
     (org-agenda-priority)
     (org-agenda-set-tags)
     (org-agenda-next-item 1)))
+
+;;; Org-super-agenda
+(use-package org-super-agenda
+  :pin gnu-elpa-devel
+  :demand
+  :after org-agenda
+  :custom
+  (org-super-agenda-hide-empty-groups t)
+  ;; FIXME 2024-10-06: When trying to set `org-super-agenda-keep-order' to
+  ;; non-nil, it causes an error when using :auto-* selectors. This doesn't seem
+  ;; to occur in an emacs -Q instances, but I have no clue what is causing the
+  ;; error in my config... Although the following PR might fix the issue:
+  ;; https://github.com/alphapapa/org-super-agenda/pull/242. Also see the NOTE
+  ;; above, near the :vc keword
+  (org-super-agenda-keep-order t)
+  (org-agenda-cmp-user-defined #'krisb-org-sort-agenda-by-created-time)
+  :init
+  (defun krisb-org-get-created-time (entry)
+    "Return the CREATED time of ENTRY, or an empty string if it doesn't exist."
+    (let ((marker (get-text-property 0 'marker entry)))
+      (if marker
+          (org-entry-get marker "CREATED")
+        "")))
+
+  (defun krisb-org-sort-agenda-by-created-time (a b)
+    "Compare two agenda items, A and B, by their CREATED property."
+    (let* ((time-a (krisb-org-get-created-time a))
+           (time-b (krisb-org-get-created-time b)))
+      (cond
+       ((string= time-a "") +1)         ; A has no CREATED property, put it last
+       ((string= time-b "") -1)         ; B has no CREATED property, put it last
+       (t
+        (if (time-less-p (date-to-time time-a) (date-to-time time-b))
+            -1 +1)))))
+  :config
+  (org-super-agenda-mode 1)
+  ;; Relevant variables to set locally in `org-agenda-custom-commands'
+  ;; - `org-agenda-overriding-header'
+  ;; - `org-agenda-show-inherited-tags'
+  ;; - `org-agenda-sorting-strategy'
+  ;; - `org-agenda-start-day'
+  ;; - `org-agenda-span'
+  ;; - `org-agenda-prefix-format'
+  ;; - `org-agenda-scheduled-leaders'
+  ;; - `org-agenda-deadline-leaders'
+  ;; - `org-agenda-skip-deadline-prewarning-if-scheduled'
+  ;; - `org-agenda-skip-scheduled-if-deadline-is-shown'
+  ;; - `org-habit-show-all-today'
+  ;; - `org-habit-show-habits-only-for-today'
+  ;; - `org-agenda-dim-blocked-tasks'
+  ;; - `org-agenda-include-diary'
+  ;; - `org-agenda-insert-diary-extract-time'
+  ;; - `org-agenda-skip-function'
+  ;; - `org-agenda-entry-types'
+  ;; - `org-deadline-warning-days'
+  ;; - `org-scheduled-delay-days'
+  (setopt org-agenda-custom-commands
+          '(("f" "FYP"
+             ((agenda ""
+                      ((org-agenda-overriding-header "Time-bound tasks")
+                       (org-agenda-show-inherited-tags t)
+                       (org-agenda-start-day "+0d")
+                       (org-agenda-span 'day)
+                       (org-habit-show-habits-only-for-today t)
+                       (org-agenda-dim-blocked-tasks t)
+                       (org-agenda-include-diary t)
+                       (org-agenda-insert-diary-extract-time t)
+                       (org-super-agenda-groups
+                        '((:discard (:and (:tag "inbox" :not (:deadline t)))) ; We want to see deadlines even if they have the inbox tag
+                          (:auto-category t)))))
+              (tags-todo "+TODO=\"NEXT\"-project-inbox"
+                         ((org-agenda-overriding-header "Next")
+                          (org-agenda-use-tag-inheritance '(todo))
+                          (org-agenda-show-inherited-tags t)
+                          (org-agenda-dim-blocked-tasks 'invisible)
+                          (org-agenda-skip-function
+                           '(org-agenda-skip-entry-if 'scheduled 'deadline))))
+              (tags-todo "+TODO=\"TODO\"-project-inbox"
+                         ((org-agenda-overriding-header "Standard")
+                          (org-agenda-use-tag-inheritance '(todo))
+                          (org-agenda-show-inherited-tags t)
+                          (org-agenda-dim-blocked-tasks 'invisible)
+                          (org-agenda-skip-function
+                           '(org-agenda-skip-entry-if 'scheduled 'deadline))))))
+            ("i" "Inbox: process entries"
+             ((agenda ""
+                      ((org-agenda-overriding-header "Time-bound inbox")
+                       (org-agenda-start-day "+0d")
+                       (org-agenda-span 5)
+                       (org-habit-show-habits nil)
+                       (org-agenda-entry-types
+                        '(:deadline :scheduled))
+                       (org-super-agenda-groups
+                        '((:tag "inbox")
+                          (:todo "MAYBE")
+                          (:discard (:anything t))))))
+              (tags-todo "+inbox"
+                         ((org-agenda-overriding-header "Regular inbox")
+                          (org-agenda-dim-blocked-tasks t)
+                          (org-agenda-skip-function
+                           '(org-agenda-skip-entry-if 'scheduled 'deadline))))
+              (todo "MAYBE"
+                    ((org-agenda-overriding-header "Regular maybes")
+                     (org-agenda-skip-function
+                      '(org-agenda-skip-entry-if 'scheduled 'deadline))))))
+            ("p" "Projects"
+             ((tags-todo "project"
+                         ((org-agenda-overriding-header "")
+                          ;; This lets project sub-tasks be discoverable by a tags
+                          ;; search. One might think :auto-parent makes this
+                          ;; redundant, but this handles cases where I have a
+                          ;; sub-task but its parent is not a project -- I do this
+                          ;; sometimes for simple dependencies between todos
+                          ;; FIXME 2024-10-07: This shows the project tag for all the
+                          ;; sub-tasks, which can be visually noisy. I'm not sure if
+                          ;; there is a workaround
+                          (org-tags-exclude-from-inheritance
+                           (remove "project" org-tags-exclude-from-inheritance))
+                          (org-agenda-prefix-format
+                           ;; FIXME 2024-10-07: Not sure if this is a tags- or
+                           ;; todo-type view
+                           '((tags  . " %i %-8:c%-5e%?-12t% s")))
+                          (org-super-agenda-groups
+                           '(( :auto-parent t
+                               :order 2)
+                             ( :name "All projects"
+                               :anything t
+                               :order 1))))))))))
 
 ;;; Provide
 (provide 'krisb-org-agenda)
