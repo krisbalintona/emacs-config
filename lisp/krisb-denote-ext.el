8c35716203444c2e380da00500d765c06ff674fe
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-20 16:56:03 -0500

    Transition my denote link updating code

--- lisp/krisb-denote-ext.el
+++ lisp/krisb-denote-ext.el
@@ -24,9 +24,12 @@
 
 ;;; Code:
 (require 'denote)
+(require 'org-element)
+(require 'denote-org-extras)
 
-;;; Standardizing note front-mattert
+;;; Standardizing note front-matter
 (defun krisb-org-set-keyword (keyword value)
+  "Set org KEYWORD in current buffer to VALUE."
   ;; Got lazy and copied `org-roam-set-keyword'
   (org-with-point-at 1
     (let ((case-fold-search t))
@@ -61,7 +64,7 @@
      ;; ID doesn't exist
      ((not (krisb-note-buffer-prop-get "identifier"))
       (save-excursion
-        (beginning-of-buffer)
+        (goto-char (point-min))
         ;; Move cursor until after the first of following
         ;; properties exists: filetags, date, or title
         (while (and (not (eobp))
@@ -111,11 +114,11 @@
   (let* ((f (buffer-file-name))
          (file-type (denote-filetype-heuristics f))
          (cur-keywords (seq-uniq (denote-retrieve-keywords-value f file-type)))
-         (sorted-keywords (denote-keywords-sort (copy-list cur-keywords))))
-    (denote--rewrite-keywords f sorted-keywords file-type)
+         (sorted-keywords (denote-keywords-sort (cl-copy-list cur-keywords))))
+    (denote-rewrite-keywords f sorted-keywords file-type)
     ;; Add empty filetags property if one isn't already present
     (unless (krisb-note-buffer-prop-get "filetags")
-      (beginning-of-buffer)
+      (goto-char (point-min))
       (while (and (not (eobp))
                   (cond
                    ((krisb-note-buffer-prop-get "date")
@@ -145,7 +148,7 @@
       (insert "#+filetags:\n"))))
 (defun krisb-denote-ensure-title-space ()
   (save-excursion
-    (beginning-of-buffer)
+    (goto-char (point-min))
     (if-let ((end-of-title-keyword
               (re-search-forward (rx bol "#+"
                                      (or "T" "t")
@@ -165,7 +168,7 @@
   (interactive)
   (let ((existing-buffers (buffer-list)))
     (save-mark-and-excursion
-      (dolist (file (denote-directory-files-matching-regexp (rx (literal ".org") eol)))
+      (dolist (file (denote-directory-files (rx (literal ".org") eol)))
         ;; Export all the files
         (with-current-buffer (find-file-noselect file)
           (read-only-mode -1)
@@ -180,6 +183,77 @@
           ;; Kill buffer unless it already exists
           (unless (member (get-buffer (buffer-name)) existing-buffers)
             (kill-buffer)))))))
+
+;;; Update link descriptions
+(defun krisb-denote-update-link-descriptions (confirmp)
+  "Recreate denote link descriptions in the current buffer.
+If called with CONFIMP, then prompt user to confirm a replacement. When
+interactively called, CONFIRMP is non-nil by default, flipping the value
+with `prefix-arg'."
+  (interactive (list (not current-prefix-arg)))
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward (denote-org-extras--get-link-type-regexp 'denote) nil :no-error)
+      (condition-case err
+          (save-match-data
+            (let* ((link-beg (match-beginning 0))
+                   (link-end (match-end 0))
+                   (s (match-string-no-properties 0))
+                   (link (with-temp-buffer
+                           (let ((org-inhibit-startup nil))
+                             (insert s)
+                             (org-mode)
+                             (goto-char (point-min))
+                             (org-element-link-parser))))
+                   (type (org-element-property :type link))
+                   (path (org-element-property :path link))
+                   (file (denote-get-path-by-id (car (string-split path "::"))))
+                   (heading-custom-id (cadr (string-split path "::")))
+                   (new-link-text
+                    ;; TODO 2024-03-04: This is a brittle way to create the
+                    ;; link. Changes to Denote might break this. Avoid that.
+                    (if (and denote-org-store-link-to-heading heading-custom-id)
+                        (format "[[denote:%s::#%s][%s]]"
+                                (denote-retrieve-filename-identifier file)
+                                (string-remove-prefix "#" heading-custom-id)
+                                (concat (denote--link-get-description file)
+                                        "::"
+                                        (save-excursion
+                                          (with-current-buffer (find-file-noselect file)
+                                            (org-link-search heading-custom-id)
+                                            (org-link-display-format
+                                             (denote-link-ol-get-heading))))))
+                      (format "[[denote:%s][%s]]"
+                              (denote-retrieve-filename-identifier file)
+                              (denote--link-get-description file))))
+                   (current-link-text (buffer-substring link-beg link-end)))
+              (when (and (not (string= (substring-no-properties current-link-text) new-link-text))
+                         (or (not confirmp)
+                             (yes-or-no-p (concat "Replace this link? " current-link-text))))
+                (goto-char link-beg)
+                (delete-region link-beg link-end)
+                (insert new-link-text))))
+        (error (message "[krisb-denote-update-link-descriptions] Error encountered:  %s"
+                        (error-message-string err))))))
+  (message "Corrected links in %s"
+           (propertize (denote-retrieve-front-matter-title-value
+                        (buffer-file-name)
+                        (denote-filetype-heuristics (buffer-file-name)))
+                       'face 'denote-faces-title)))
+
+(defun krisb-denote-update-link-descriptions-globally (dir confirmp)
+  "Update the link description of all notes in DIR.
+If CONFIRMP is non-nil, then prompt the user to confirm each
+replacement."
+  (interactive (list (denote-subdirectory-prompt) current-prefix-arg))
+  (save-window-excursion
+    (dolist (f (denote-directory-files (concat (file-name-nondirectory dir) "/") nil t))
+      (save-excursion
+        (let* ((live-buffer (get-file-buffer f)))
+          (with-current-buffer (find-file-noselect f)
+            (krisb-denote-update-link-descriptions confirmp))
+          (unless live-buffer (kill-buffer live-buffer))))))
+  (message "Updated all links in %s!" dir))
 
 ;;; Provide
 (provide 'krisb-denote-ext)
