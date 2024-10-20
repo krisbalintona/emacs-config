91a6db1b97b33330283106b73a0f527a6c035940
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-19 23:27:54 -0500

    Update custom.el

--- etc/custom.el
+++ etc/custom.el
@@ -5,9 +5,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages nil)
- '(safe-local-variable-directories '("/home/krisbalintona/Documents/org-database/agenda/"))
+ '(package-vc-selected-packages
+   '((org-super-agenda :url
+                       "https://github.com/Alexander-Miller/org-super-agenda.git")))
+ '(safe-local-variable-directories
+   '("/home/krisbalintona/Documents/org-database/notes/commonplace_book/"
+     "/home/krisbalintona/Documents/org-database/agenda/"))
  '(safe-local-variable-values
-   '((eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t))))
+   '((org-src-fontify-natively)
+     (eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
