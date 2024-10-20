b46487ff7294e673cb519a05bf719fcbfae89281
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-19 21:39:01 -0500

    Update custom.el

--- etc/custom.el
+++ etc/custom.el
@@ -5,7 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages nil)
- '(safe-local-variable-directories '("/home/krisbalintona/Documents/org-database/agenda/")))
+ '(safe-local-variable-directories '("/home/krisbalintona/Documents/org-database/agenda/"))
+ '(safe-local-variable-values
+   '((eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
