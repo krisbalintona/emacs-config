4371c4e3304e0b1cb3f2bfdc266b326fa0eac284
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-20 04:28:49 -0500

    Transition abbrev

---
+++ etc/abbrev.el
@@ -0,0 +1,39 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'emacs-lisp-mode-abbrev-table
+  '(
+    ("couterpart" "counterpart" nil :count 0)
+    ("devault" "default" nil :count 0)
+    ("fasater" "faster" nil :count 0)
+   ))
+
+(define-abbrev-table 'global-abbrev-table
+  '(
+    (";d" "" kb/abbrev-current-date :count 3)
+    (";t" "" kb/abbrev-todo-keyword :count 0)
+    (";td" "" kb/abbrev-todo-keyword-and-date :count 6)
+   ))
+
+(define-abbrev-table 'org-mode-abbrev-table
+  '(
+    ("Colordo" "Colorado" nil :count 0)
+    ("Keybinds" "Keybindings" nil :count 0)
+    ("Phillipines" "Philippines" nil :count 0)
+    ("Tenessee" "Tennessee" nil :count 0)
+    ("concpet" "concept" nil :count 0)
+    ("disambiguiate" "disambiguate" nil :count 0)
+    ("fractical" "fractal" nil :count 0)
+    ("maintanence" "maintenance" nil :count 0)
+    ("supercede" "supersede" nil :count 0)
+   ))
+
+(define-abbrev-table 'text-mode-abbrev-table
+  '(
+    ("isntances" "instances" nil :count 0)
+   ))
+
+(define-abbrev-table 'vc-git-log-edit-mode-abbrev-table
+  '(
+    ("slightlyg" "slightly" nil :count 0)
+    ("visibitilty" "visibility" nil :count 0)
+   ))
+
