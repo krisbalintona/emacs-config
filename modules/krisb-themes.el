b33d6a6f9031e89e4179fb36dcae0f1fee24651d
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-20 07:13:41 -0500

    Use prose cursory preset when writing commits

--- modules/krisb-themes.el
+++ modules/krisb-themes.el
@@ -116,7 +116,7 @@
 ;; Global and local cursor presets
 (use-package cursory
   :hook ((prog-mode . (lambda () (cursory-set-preset 'code :local)))
-         ((org-mode markdown-mode) . (lambda () (cursory-set-preset 'prose :local))))
+         ((org-mode markdown-mode git-commit-setup log-edit-mode) . (lambda () (cursory-set-preset 'prose :local))))
   :custom
   (cursory-latest-state-file (no-littering-expand-var-file-name "cursory/cursory-latest-state"))
   (cursory-presets
