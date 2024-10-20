ae36fae5b78810842c489610f40f0a067584e028
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-20 16:12:52 -0500

    Transition my mpv.el extensions

--- modules/krisb-videos.el
+++ modules/krisb-videos.el
@@ -5,8 +5,11 @@
   ;; https://github.com/kljohann/mpv.el/issues/31#issuecomment-1856491594 for
   ;; why I use the latest GitHub version
   :pin melpa
+  :bind-keymap ("C-M-s-m" . krisb-mpv-map)
   :custom
-  (mpv-default-options (list "--save-position-on-quit")))
+  (mpv-default-options (list "--save-position-on-quit"))
+  :config
+  (require 'krisb-mpv))
 
 ;;; Provide
 (require 'krisb-videos)
