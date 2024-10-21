e4b7f74418ae47de65c2bcdbc4e84a99ddf6153b
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-20 22:01:48 -0500

    Update custom.el

--- etc/custom.el
+++ etc/custom.el
@@ -7,29 +7,32 @@
  '(package-selected-packages
    '(ace-window agitate all-the-icons apheleia astute breadcrumb cape citar-denote
                 cl-generic corfu-prescient cursory darkroom denote-explore
-                denote-interface diminish editorconfig eglot elisp-demos
-                embark-consult engine-mode erc eros exec-path-from-shell faceup
-                flymake-collection fontaine form-feed gcmh
-                highlight-function-calls ht idlwave info-colors inspector jinx
-                keychain-environment kind-icon lin magit magit-file-icons map
-                marginalia mermaid-mode mixed-pitch modus-themes mpv nadvice
-                nerd-icons-completion nerd-icons-dired no-littering notmuch-addr
-                notmuch-transient ntlm ob-mermaid ol-notmuch olivetti on
-                orderless org-appear org-contrib org-mime org-modern org-remark
-                org-web-tools outshine package-lint-flymake paren-face pdf-tools
-                peg pulsar puni python recursion-indicator saveplace-pdf-view
-                scratch so-long soap-client svg system-packages tmr
-                track-changes tramp treesit-auto try ts typewriter-roll-mode
-                use-package verilog-mode vertico-prescient which-key
-                window-tool-bar wombag ytdl))
+                denote-interface diminish dired-hist editorconfig eglot
+                elisp-demos embark-consult engine-mode enlight erc eros
+                exec-path-from-shell faceup flymake-collection fontaine
+                form-feed gcmh grid highlight-function-calls ht idlwave
+                info-colors inspector jinx keychain-environment kind-icon lin
+                magit magit-file-icons map marginalia mermaid-mode mixed-pitch
+                modus-themes mpv nadvice nerd-icons-completion nerd-icons-dired
+                no-littering notmuch-addr notmuch-transient ntlm ob-mermaid
+                ol-notmuch olivetti on orderless org-appear org-contrib org-mime
+                org-modern org-remark org-web-tools outshine
+                package-lint-flymake paren-face pdf-tools peg pulsar puni python
+                recursion-indicator saveplace-pdf-view scratch so-long
+                soap-client svg system-packages tmr track-changes tramp
+                treesit-auto try ts typewriter-roll-mode use-package
+                verilog-mode vertico-prescient which-key window-tool-bar wombag
+                ytdl))
  '(package-vc-selected-packages
-   '((wombag :url "https://github.com/karthink/wombag.git")
+   '((grid :url "https://github.com/ichernyshovvv/grid.el")
+     (wombag :url "https://github.com/karthink/wombag.git")
      (denote-interface :url "git@github.com:krisbalintona/denote-interface.git")))
  '(safe-local-variable-directories
    '("/home/krisbalintona/Documents/org-database/agenda/"
      "/home/krisbalintona/Documents/org-database/notes/commonplace_book/"))
  '(safe-local-variable-values
-   '((eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t))))
+   '((eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t)))
+ '(tmr-description-list '("Stop working!" "Work time 😄") nil nil "Customized with use-package tmr"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
