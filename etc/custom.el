4c408ba4432f4aae89599b6739e1e96496d7dc97
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-21 23:25:27 -0500

    custom.el: Update

--- etc/custom.el
+++ etc/custom.el
@@ -5,29 +5,31 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window agitate all-the-icons apheleia astute breadcrumb cape citar-denote
-                cl-generic corfu-prescient cursory darkroom denote-explore
-                denote-interface diminish dired-hist edit-indirect editorconfig
-                eglot elisp-demos embark-consult engine-mode enlight erc eros
-                exec-path-from-shell faceup flymake-collection fontaine
-                form-feed gcmh grid hammy highlight-function-calls ht idlwave
-                info-colors inspector jinx keychain-environment kind-icon lin
-                magit-file-icons map marginalia mermaid-mode mixed-pitch
-                modus-themes mpv nadvice nerd-icons-completion nerd-icons-dired
-                no-littering notmuch-addr notmuch-transient ntlm ob-mermaid
-                ol-notmuch olivetti on orderless org-appear org-contrib org-mime
-                org-modern org-remark org-tidy org-web-tools outshine
-                package-lint-flymake paren-face pdf-tools peg pulsar puni python
-                recursion-indicator saveplace-pdf-view scratch so-long
-                soap-client svg system-packages tmr track-changes tramp
-                treesit-auto try typewriter-roll-mode use-package verilog-mode
-                vertico-prescient which-key window-tool-bar wombag ytdl))
+   '(ace-window agitate all-the-icons apheleia astute breadcrumb bufler cape
+                citar-denote cl-generic corfu-prescient cursory darkroom
+                denote-explore denote-interface diminish dired-hist
+                edit-indirect editorconfig eglot elisp-demos embark-consult
+                engine-mode enlight erc eros exec-path-from-shell faceup
+                flymake-collection fontaine form-feed gcmh grid hammy
+                highlight-function-calls ht idlwave info-colors inspector jinx
+                keychain-environment kind-icon lin lorem-ipsum magit-file-icons
+                map marginalia mermaid-mode mixed-pitch modus-themes mpv nadvice
+                nerd-icons-completion nerd-icons-dired no-littering notmuch-addr
+                notmuch-transient ntlm ob-mermaid ol-notmuch olivetti on
+                orderless org-appear org-contrib org-mime org-modern org-remark
+                org-tidy org-web-tools outshine package-lint-flymake paren-face
+                pdf-tools peg pulsar puni python recursion-indicator
+                saveplace-pdf-view scratch so-long soap-client sudo-edit svg
+                system-packages tmr track-changes tramp treesit-auto try
+                typewriter-roll-mode use-package verilog-mode vertico-prescient
+                which-key window-tool-bar wombag ytdl))
  '(package-vc-selected-packages
    '((grid :url "https://github.com/ichernyshovvv/grid.el")
      (wombag :url "https://github.com/karthink/wombag.git")
      (denote-interface :url "git@github.com:krisbalintona/denote-interface.git")))
  '(safe-local-variable-directories
-   '("/home/krisbalintona/Documents/org-database/agenda/"
+   '("/home/krisbalintona/Documents/org-database/notes/bib/"
+     "/home/krisbalintona/Documents/org-database/agenda/"
      "/home/krisbalintona/Documents/org-database/notes/commonplace_book/"))
  '(safe-local-variable-values
    '((eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t)))
