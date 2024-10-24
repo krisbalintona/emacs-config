beefdc2f3c61eb6aac6038761e90971ca6bee50c
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-24 13:09:25 -0500

    Increase font size in Info buffers

--- modules/krisb-info.el
+++ modules/krisb-info.el
@@ -1,8 +1,13 @@
 ;;;; Info
 (use-package info
-  :hook (Info-selection . mixed-pitch-mode)
+  :hook ((Info-selection . mixed-pitch-mode)
+         (Info-selection . krisb-info-font-resize))
   :custom
-  (Info-isearch-search nil))            ; Restore default isearch behavior
+  (Info-isearch-search nil)             ; Restore default isearch behavior
+  :config
+  (defun krisb-info-font-resize ()
+    "Increase the font size of text in Info buffers."
+    (face-remap-set-base 'default `(:height 1.2))))
 
 ;;;; Info-colors
 ;; Fontify useful parts of info buffers
