bbd366bf35c690babd6f04741526c1acc851b924
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-30 11:23:19 -0500

    abbrev.el: Update

--- etc/abbrev.el
+++ etc/abbrev.el
@@ -4,11 +4,12 @@
     ("couterpart" "counterpart" nil :count 0)
     ("devault" "default" nil :count 0)
     ("fasater" "faster" nil :count 0)
+    ("preferrable" "preferable" nil :count 0)
    ))
 
 (define-abbrev-table 'global-abbrev-table
   '(
-    (";d" "" krisb-abbrev-current-date :count 0)
+    (";d" "" krisb-abbrev-current-date :count 1)
     (";t" "" krisb-abbrev-todo-keyword :count 0)
     (";td" "" krisb-abbrev-todo-keyword-and-date :count 0)
    ))
@@ -19,7 +20,9 @@
     ("Keybinds" "Keybindings" nil :count 0)
     ("Marketting" "Marketing" nil :count 0)
     ("Phillipines" "Philippines" nil :count 0)
+    ("Poitn" "Point" nil :count 0)
     ("Tenessee" "Tennessee" nil :count 0)
+    ("catalogy" "catalog" nil :count 0)
     ("coleagues" "colleagues" nil :count 0)
     ("collaborive" "collaborative" nil :count 0)
     ("compatability" "compatibility" nil :count 0)
@@ -29,15 +32,19 @@
     ("disambiguiate" "disambiguate" nil :count 0)
     ("excusive" "exclusive" nil :count 0)
     ("fractical" "fractal" nil :count 0)
+    ("heirarhcy" "hierarchy" nil :count 0)
     ("incompability" "incompatibility" nil :count 0)
     ("labelled" "labeled" nil :count 0)
     ("maintanence" "maintenance" nil :count 0)
     ("maleability" "malleability" nil :count 0)
+    ("obiously" "obviously" nil :count 0)
+    ("recommendatiosn" "recommendations" nil :count 0)
     ("repicate" "replicate" nil :count 0)
     ("resolvese" "resolves" nil :count 0)
     ("supercede" "supersede" nil :count 0)
     ("superceded" "superseded" nil :count 0)
     ("supercedes" "supersedes" nil :count 0)
+    ("travelling" "traveling" nil :count 0)
    ))
 
 (define-abbrev-table 'text-mode-abbrev-table
@@ -47,11 +54,12 @@
 
 (define-abbrev-table 'vc-git-log-edit-mode-abbrev-table
   '(
+    ("accomodate" "accommodate" nil :count 0)
     ("activaate" "activate" nil :count 0)
+    ("caled" "called" nil :count 0)
+    ("exectuable" "executable" nil :count 0)
+    ("intersting" "interesting" nil :count 0)
     ("slightlyg" "slightly" nil :count 0)
     ("visibitilty" "visibility" nil :count 0)
-    ("accomodate" "accommodate" nil :count 0)
-    ("caled" "called" nil :count 0)
-    ("intersting" "interesting" nil :count 0)
    ))
 
