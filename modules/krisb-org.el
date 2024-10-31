d135bb2a1f8e01b6ad4a338c96b72424d26c8e36
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-31 07:15:07 -0500

    Remove org-narrow-to-subtree from org-bookmark-heading-after-jump-hook

--- modules/krisb-org.el
+++ modules/krisb-org.el
@@ -330,7 +330,6 @@
 (use-package org-bookmark-heading
   ;; TODO 2024-10-30: Consider also adding `org-cycle-set-startup-visibility' to
   ;; `org-bookmark-heading-after-jump-hook'
-  :hook (org-bookmark-heading-after-jump . org-narrow-to-subtree)
   :custom
   (org-bookmark-heading-make-ids nil))
 
