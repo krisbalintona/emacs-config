fd03e9f9dce75ea425a72274be78f29a6b138fbf
Kristoffer Balintona <krisbalintona@gmail.com>
2024-10-31 23:58:39 -0500

    custom.el: Update

--- etc/abbrev.el
+++ etc/abbrev.el
@@ -1,15 +1,17 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'emacs-lisp-mode-abbrev-table
   '(
+    ("Ovverride" "Override" nil :count 0)
     ("couterpart" "counterpart" nil :count 0)
     ("devault" "default" nil :count 0)
     ("fasater" "faster" nil :count 0)
     ("preferrable" "preferable" nil :count 0)
+    ("versison" "version" nil :count 0)
    ))
 
 (define-abbrev-table 'global-abbrev-table
   '(
-    (";d" "" krisb-abbrev-current-date :count 2)
+    (";d" "" krisb-abbrev-current-date :count 4)
     (";t" "" krisb-abbrev-todo-keyword :count 0)
     (";td" "" krisb-abbrev-todo-keyword-and-date :count 1)
    ))
@@ -22,6 +24,7 @@
     ("Phillipines" "Philippines" nil :count 0)
     ("Poitn" "Point" nil :count 0)
     ("Tenessee" "Tennessee" nil :count 0)
+    ("beheve" "behave" nil :count 0)
     ("bookpages" "book pages" nil :count 0)
     ("catalogy" "catalog" nil :count 0)
     ("coleagues" "colleagues" nil :count 0)
@@ -31,11 +34,14 @@
     ("concusion" "conclusion" nil :count 0)
     ("develoepd" "developed" nil :count 0)
     ("disambiguiate" "disambiguate" nil :count 0)
+    ("enuciated" "enunciated" nil :count 0)
     ("excusive" "exclusive" nil :count 0)
     ("fractical" "fractal" nil :count 0)
     ("heirarhcy" "hierarchy" nil :count 0)
     ("incompability" "incompatibility" nil :count 0)
+    ("itnroduces" "introduces" nil :count 0)
     ("labelled" "labeled" nil :count 0)
+    ("lughter" "laughter" nil :count 0)
     ("maintanence" "maintenance" nil :count 0)
     ("maleability" "malleability" nil :count 0)
     ("misperceives" "misconceives" nil :count 0)
