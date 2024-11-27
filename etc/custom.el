24161a41224045f8929a5a8ad7385184f6b56205
Kristoffer Balintona <krisbalintona@gmail.com>
2024-11-26 22:16:19 -0600

    Install consult-xref-stack

--- etc/custom.el
+++ etc/custom.el
@@ -7,31 +7,34 @@
  '(package-selected-packages
    '(ace-window activities adaptive-wrap agitate all-the-icons apheleia astute
                 beframe breadcrumb bufler cape citar-denote citar-org-roam
-                cl-generic cm-mode corfu-prescient cursory darkroom
-                denote-explore denote-interface diminish dired-hist dumber-jump
-                easysession edit-indirect editorconfig eglot elisp-demos
-                embark-consult engine-mode enlight erc eros eshell-atuin
-                eshell-syntax-highlighting eshell-z exec-path-from-shell faceup
-                fish-mode flymake-collection fontaine forge form-feed gcmh grid
-                hammy hide-mode-line highlight-function-calls hotfuzz
-                ibuffer-project idlwave info-colors inspector jinx
-                keychain-environment kind-icon lin lorem-ipsum magit-file-icons
-                marginalia mermaid-mode mixed-pitch modus-themes mpv nadvice
-                nerd-icons-completion nerd-icons-dired nerd-icons-ibuffer
-                no-littering notmuch-addr notmuch-transient nov ntlm ob-mermaid
-                oc-csl-activate ol-notmuch olivetti on orderless org-appear
-                org-bookmark-heading org-bulletproof org-contrib org-edna
-                org-mime org-modern org-remark org-review org-roam
-                org-roam-folgezettel org-roam-ui org-super-agenda org-tidy
-                org-web-tools outshine ox-hugo package-lint-flymake paren-face
-                pcmpl-args pdf-tools peg pinentry popper powerthesaurus psession
-                pulsar puni python rainbow-mode recursion-indicator
-                saveplace-pdf-view scratch show-font so-long soap-client
-                sudo-edit svg system-packages tmr track-changes tramp
-                treesit-auto try typewriter-roll-mode use-package verilog-mode
-                vertico-prescient which-key window-tool-bar wombag ytdl))
+                cl-generic cm-mode consult-xref-stack corfu-prescient cursory
+                darkroom denote-explore denote-interface diminish dired-hist
+                dumber-jump easysession edit-indirect editorconfig eglot
+                elisp-demos embark-consult engine-mode enlight erc eros
+                eshell-atuin eshell-syntax-highlighting eshell-z
+                exec-path-from-shell faceup fish-mode flymake-collection
+                fontaine forge form-feed gcmh grid hammy hide-mode-line
+                highlight-function-calls hotfuzz ibuffer-project idlwave
+                info-colors inspector jinx keychain-environment kind-icon lin
+                lorem-ipsum magit-file-icons marginalia mermaid-mode mixed-pitch
+                modus-themes mpv nadvice nerd-icons-completion nerd-icons-dired
+                nerd-icons-ibuffer no-littering notmuch-addr notmuch-transient
+                nov ntlm ob-mermaid oc-csl-activate ol-notmuch olivetti on
+                orderless org-appear org-bookmark-heading org-bulletproof
+                org-contrib org-edna org-mime org-modern org-remark org-review
+                org-roam org-roam-folgezettel org-roam-ui org-super-agenda
+                org-tidy org-web-tools outshine ox-hugo package-lint-flymake
+                paren-face pcmpl-args pdf-tools peg pinentry popper
+                powerthesaurus project psession pulsar puni python rainbow-mode
+                recursion-indicator saveplace-pdf-view scratch show-font so-long
+                soap-client sudo-edit svg system-packages tmr track-changes
+                tramp treesit-auto try typewriter-roll-mode use-package
+                verilog-mode vertico-prescient which-key window-tool-bar wombag
+                xref ytdl))
  '(package-vc-selected-packages
-   '((org-roam-folgezettel :url
+   '((consult-xref-stack :url
+                         "https://github.com/brett-lempereur/consult-xref-stack")
+     (org-roam-folgezettel :url
                            "git@github.com:krisbalintona/org-roam-folgezettel.git")
      (cm-mode :url "https://github.com/joostkremers/criticmarkup-emacs.git")
      (org-super-agenda :url
