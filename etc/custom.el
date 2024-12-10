60b1c7385b4f725ba8efcccf73de9898b57849c1
Kristoffer Balintona <krisbalintona@gmail.com>
2024-12-10 01:37:09 -0600

    Update files

--- etc/custom.el
+++ etc/custom.el
@@ -20,7 +20,7 @@
                 marginalia mermaid-mode mixed-pitch modus-themes mpv nadvice
                 nerd-icons-completion nerd-icons-dired nerd-icons-ibuffer
                 no-littering notmuch-addr notmuch-transient nov ntlm ob-mermaid
-                oc-csl-activate ol-notmuch olivetti on orderless org-appear
+                oc-csl-activate ol-notmuch olivetti on orderless org org-appear
                 org-bookmark-heading org-bulletproof org-contrib org-edna
                 org-mime org-modern org-remark org-review org-roam
                 org-roam-folgezettel org-roam-ui org-super-agenda org-tidy
