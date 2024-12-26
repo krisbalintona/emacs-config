558b24674006b9cbdd326a40c7d1e63229ddc711
Kristoffer Balintona <krisbalintona@gmail.com>
2024-12-26 08:19:25 -0600

    Use MELPA hyperlang-ts-mode recipe

--- modules/krisb-other-languages.el
+++ modules/krisb-other-languages.el
@@ -1,8 +1,6 @@
 ;;; Hyprlang-ts-mode
 ;; Treesitter mode for hyprland config files
 (use-package hyprlang-ts-mode
-  :vc ( :url "https://github.com/Nathan-Melaku/hyprlang-ts-mode.git"
-        :rev :newest)
   :init
   (with-eval-after-load 'treesit
     (add-to-list 'treesit-language-source-alist
