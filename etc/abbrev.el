60dcc41312835c32b31946f7a649ed5cb49235b5
Kristoffer Balintona <krisbalintona@gmail.com>
2025-03-11 20:22:14 -0500

    abbrev.el: Update

--- etc/abbrev.el
+++ etc/abbrev.el
@@ -1,13 +1,30 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'global-abbrev-table
   '(
-    (";d" "" krisb-abbrev-current-date :count 0)
+    (";d" "" krisb-abbrev-current-date :count 2)
     (";t" "" krisb-abbrev-todo-keyword :count 0)
     (";td" "" krisb-abbrev-todo-keyword-and-date :count 0)
    ))
 
 (define-abbrev-table 'org-mode-abbrev-table
   '(
+    ("Guillitines" "Guillotines" nil :count 0)
     ("Postsure" "Posture" nil :count 0)
+    ("buildling" "building" nil :count 1)
+    ("cultiavte" "cultivate" nil :count 0)
+    ("developements" "developments" nil :count 0)
+    ("diference" "difference" nil :count 0)
+    ("easiliy" "easily" nil :count 0)
+    ("entrepreneural" "entrepreneurial" nil :count 0)
+    ("foward" "forward" nil :count 0)
+    ("incompatabilities" "incompatibilities" nil :count 0)
+    ("loney" "money" nil :count 0)
+    ("pacage" "package" nil :count 0)
+    ("tremendeous" "tremendous" nil :count 0)
+   ))
+
+(define-abbrev-table 'text-mode-abbrev-table
+  '(
+    ("exectuable" "executable" nil :count 0)
    ))
 
