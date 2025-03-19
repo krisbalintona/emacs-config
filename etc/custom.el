079dcc9bc3aa2a97e9610e9712905863721b8d39
Kristoffer Balintona <krisbalintona@gmail.com>
2025-03-19 16:41:37 -0500

    Update etc/ files

--- etc/custom.el
+++ etc/custom.el
@@ -6,35 +6,41 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(ace-window activities adaptive-wrap agitate all-the-icons apheleia astute
-                beframe breadcrumb bufler cape citar-denote citar-org-roam
-                cl-generic cm-mode consult-xref-stack corfu-prescient csv-mode
-                cursory darkroom denote-explore denote-interface diff-hl
-                diminish dired-hist dired-subtree dumber-jump easysession eat
-                edit-indirect editorconfig eglot elisp-demos embark-consult
-                engine-mode enlight erc eros eshell-atuin
-                eshell-syntax-highlighting eshell-z exec-path-from-shell faceup
-                fish-mode flymake-collection fontaine forge form-feed gcmh
-                gif-screencast grid hammy hide-mode-line
-                highlight-function-calls hotfuzz hyprlang-ts-mode
-                ibuffer-project idlwave info-colors inspector jinx
-                keychain-environment kind-icon lazy-guard lin lorem-ipsum
-                magit-file-icons marginalia mermaid-mode mixed-pitch
+                beframe breadcrumb bufler cape cascading-dir-locals citar-denote
+                citar-org-roam cl-generic cm-mode cond-star consult-xref-stack
+                corfu-prescient csv-mode cursory darkroom denote-explore
+                denote-interface diff-hl diminish dired-hist dired-subtree
+                dumber-jump easysession eat edit-indirect editorconfig eglot
+                elisp-demos embark-consult engine-mode enlight erc eros
+                eshell-atuin eshell-syntax-highlighting eshell-z
+                exec-path-from-shell faceup fish-mode flymake-collection
+                fontaine forge form-feed gcmh gif-screencast git-share grid
+                hammy hide-mode-line highlight-function-calls hotfuzz
+                hyprlang-ts-mode ibuffer-project idlwave indexed info-colors
+                inspector jinx keychain-environment kind-icon lazy-guard lin
+                lorem-ipsum magit-file-icons marginalia mermaid-mode mixed-pitch
                 modus-themes mpv nadvice nerd-icons-completion nerd-icons-dired
                 nerd-icons-ibuffer no-littering notmuch-addr notmuch-transient
                 nov ntlm ob-mermaid oc-csl-activate ol-notmuch olivetti on
                 orderless org-appear org-bookmark-heading org-bulletproof
-                org-contrib org-edna org-make-toc org-mime org-modern
+                org-contrib org-edna org-make-toc org-mime org-modern org-node
                 org-node-fakeroam org-remark org-review org-roam-ql org-roam-ui
                 org-tidy org-web-tools outshine ox-hugo package-build
                 package-lint-flymake paren-face pcmpl-args pdf-meta-edit
                 pdf-tools peg pinentry popper powerthesaurus psession pulsar
                 puni python rainbow-mode recursion-indicator saveplace-pdf-view
                 scratch show-font sinister smart-mark so-long soap-client
-                sudo-edit svg system-packages tmr tramp treesit-auto try
-                typewriter-roll-mode ultra-scroll use-package vc-jj verilog-mode
-                vertico-prescient which-key window-tool-bar wombag ytdl))
+                sudo-edit svg system-packages tmr tramp transient treesit-auto
+                try typewriter-roll-mode ultra-scroll use-package vc-jj
+                verilog-mode vertico-prescient which-key window-tool-bar wombag
+                ytdl))
  '(package-vc-selected-packages
-   '((lazy-guard :url "https://codeberg.org/vifon/lazy-guard.git")
+   '((oblique :url "https://github.com/zzkt/oblique-strategies.git")
+     (oblique-strategies :url "https://github.com/zzkt/oblique-strategies.git")
+     (clever-cite :vc-backend Git :url
+                  "https://github.com/Hugo-Heagren/clever-cite.git")
+     (git-share :vc-backend Git :url "https://github.com/mgmarlow/git-share")
+     (lazy-guard :url "https://codeberg.org/vifon/lazy-guard.git")
      (sinister :url "https://github.com/positron-solutions/sinister")
      (vc-jj :url "https://codeberg.org/emacs-jj-vc/vc-jj.el.git")
      (jj-vc :url "https://codeberg.org/emacs-jj-vc/vc-jj.el.git")
@@ -58,7 +64,8 @@
      (wombag :url "https://github.com/karthink/wombag.git")
      (denote-interface :url "git@github.com:krisbalintona/denote-interface.git")))
  '(safe-local-variable-directories
-   '("/home/krisbalintona/emacs-repos/other/melpa/"
+   '("/home/krisbalintona/Documents/org-database/notes/blog/"
+     "/home/krisbalintona/emacs-repos/other/melpa/"
      "/home/krisbalintona/Documents/org-database/notes/main/"
      "/home/krisbalintona/Documents/org-database/notes/commonplace_book/buoy/"
      "/home/krisbalintona/Documents/org-database/notes/"
