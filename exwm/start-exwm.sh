aeb33bae692ed802468bf04c58934268b6bc6c67
Kristoffer Balintona <krisbalintona@gmail.com>
2025-03-25 00:59:51 -0500

    Delete /exwm/

--- exwm/start-exwm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# * Load everything from .xprofile
-source "/home/krisbalintona/.xprofile"
-
-# * Start Emacs
-exec dbus-launch --exit-with-session emacs -mm --debug-init
-#-l ~/.emacs.d/desktop.el
