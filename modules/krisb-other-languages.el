9b5e78920f662b3b0c8f412915a33d14330a6574
Kristoffer Balintona <krisbalintona@gmail.com>
2025-03-31 15:18:50 -0500

    Add instruction for installing hyprlang treesitter grammar

--- modules/krisb-other-languages.el
+++ modules/krisb-other-languages.el
@@ -2,6 +2,8 @@
 ;; Treesitter mode for hyprland config files
 (use-package hyprlang-ts-mode
   :init
+  ;; 2025-03-31: You can install this grammar with:
+  ;;     M-x treesit-install-language-grammar RET hyprlang
   (with-eval-after-load 'treesit
     (add-to-list 'treesit-language-source-alist
                  '(hyprlang "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang"))))
