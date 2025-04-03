e877fd3fa290b307e9195055a116bcf4137b9a9d
Kristoffer Balintona <krisbalintona@gmail.com>
2025-04-03 11:14:57 -0500

    Update /etc/ files

--- etc/abbrev.el
+++ etc/abbrev.el
@@ -1,13 +1,35 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'emacs-lisp-mode-abbrev-table
   '(
-    ("bcause" "because" nil :count 0)
+    ("Taekn" "Taken" nil :count 0)
+    ("shuld" "should" nil :count 0)
    ))
 
 (define-abbrev-table 'global-abbrev-table
   '(
-    (";d" "" krisb-abbrev-current-date :count 1)
+    (";d" "" krisb-abbrev-current-date :count 8)
     (";t" "" krisb-abbrev-todo-keyword :count 0)
     (";td" "" krisb-abbrev-todo-keyword-and-date :count 0)
+   ))
+
+(define-abbrev-table 'log-edit-mode-abbrev-table
+  '(
+    ("implementatioo" "implementation" nil :count 0)
+    ("recognzied" "recognized" nil :count 0)
+   ))
+
+(define-abbrev-table 'org-mode-abbrev-table
+  '(
+    ("Conceretely" "Concretely" nil :count 0)
+    ("Zettelkaten" "Zettelkasten" nil :count 0)
+    ("bibliogrophc" "bibliographic" nil :count 0)
+    ("cntext" "context" nil :count 0)
+    ("conflulence" "confluence" nil :count 0)
+    ("exisitng" "existing" nil :count 0)
+    ("heirarchically" "hierarchically" nil :count 0)
+    ("labelling" "labeling" nil :count 0)
+    ("momentu" "momentum" nil :count 0)
+    ("signiciations" "significations" nil :count 0)
+    ("signifance" "significance" nil :count 0)
    ))
 
--- etc/custom.el
+++ etc/custom.el
@@ -4,38 +4,63 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/krisbalintona/Documents/org-database/agenda/archive/2023-and-before todos.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/bui.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/buoy.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/crc.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/cs1730.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/emacs.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/emails.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/hist0244.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/hist1974i.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/archive/phil1340.org_archive"
+     "/home/krisbalintona/Documents/org-database/agenda/gcal.org"
+     "/home/krisbalintona/Documents/org-database/agenda/recurring.org"
+     "/home/krisbalintona/Documents/org-database/agenda/refile.org"
+     "/home/krisbalintona/Documents/org-database/agenda/todo.org") nil nil "Customized with use-package org-agenda")
  '(package-selected-packages
    '(ace-window activities adaptive-wrap agitate all-the-icons apheleia astute
-                beframe breadcrumb bufler cape cascading-dir-locals citar-denote
-                citar-org-roam cl-generic cm-mode cond-star consult-xref-stack
-                corfu-prescient csv-mode cursory darkroom denote denote-explore
-                denote-interface diff-hl diminish dired-hist dired-subtree
-                dumber-jump easysession eat edit-indirect editorconfig eglot
+                beframe boon breadcrumb bufler cape cascading-dir-locals
+                citar-denote citar-org-roam cl-generic cm-mode cond-star
+                consult-xref-stack corfu-prescient csv-mode cursory darkroom
+                default-text-scale denote denote-explore denote-interface
+                diff-hl diminish dired-hist dired-subtree dumber-jump
+                easysession eat edit-indirect editorconfig eglot eglot-booster
                 elisp-demos embark-consult engine-mode enlight erc eros
                 eshell-atuin eshell-syntax-highlighting eshell-z
-                exec-path-from-shell faceup fish-mode flymake-collection
-                fontaine forge form-feed gcmh gif-screencast git-share grid
-                hammy hide-mode-line highlight-function-calls hotfuzz hugoista
-                hyprlang-ts-mode ibuffer-project idlwave indexed info-colors
-                inspector jinx keychain-environment kind-icon lazy-guard lin
-                litanize lorem-ipsum marginalia mermaid-mode mixed-pitch
-                modus-themes mpv nadvice nerd-icons-completion nerd-icons-dired
+                exec-path-from-shell faceup fancy-compilation fish-mode
+                flymake-collection fontaine forge form-feed gcmh gif-screencast
+                git-share god-mode grid hammy hide-mode-line
+                highlight-function-calls hotfuzz hugoista hyprlang-ts-mode
+                ibuffer-project idlwave indexed info-colors inspector jinx
+                kbd-mode keychain-environment kind-icon lazy-guard ligature lin
+                litanize lorem-ipsum marginalia meow mermaid-mode mixed-pitch
+                mode-minder modus-themes motion-selection motion-selection-mode
+                mpv nadvice nerd-icons-completion nerd-icons-dired
                 nerd-icons-ibuffer no-littering notmuch-addr notmuch-transient
                 nov ntlm ob-mermaid oc-csl-activate ol-notmuch olivetti on
                 orderless org org-appear org-bookmark-heading org-bulletproof
                 org-contrib org-edna org-make-toc org-mime org-modern org-node
                 org-remark org-review org-roam-ql org-roam-ui org-web-tools
-                outline-indent outshine ox-hugo package-build
-                package-lint-flymake paren-face pcmpl-args pdf-meta-edit
-                pdf-tools peg pinentry popper powerthesaurus psession pulsar
-                puni python rainbow-mode recursion-indicator saveplace-pdf-view
-                scratch show-font sinister smart-mark so-long soap-client
-                sudo-edit svg system-packages tmr tramp transient treesit-auto
-                try typewriter-roll-mode ultra-scroll use-package vc-jj
-                verilog-mode vertico-prescient which-key window-tool-bar wombag
-                ytdl))
+                outli outline-indent ox-hugo package-build package-lint-flymake
+                paren-face pcmpl-args pdf-meta-edit pdf-tools peg pinentry
+                popper powerthesaurus psession pulsar puni python rainbow-mode
+                recursion-indicator saveplace-pdf-view scratch show-font
+                sinister smart-mark so-long soap-client sudo-edit suggest svg
+                system-packages tmr tramp transient treesit-auto try
+                typewriter-roll-mode ultra-scroll use-package vc-jj verilog-mode
+                vertico-prescient which-key window-tool-bar wombag ytdl))
  '(package-vc-selected-packages
-   '((oblique :url "https://github.com/zzkt/oblique-strategies.git")
+   '((mode-minder :url "https://github.com/jdtsmith/mode-minder.git")
+     (kbd-mode :url "https://github.com/kmonad/kbd-mode")
+     (flymake-collection :url
+                         "https://github.com/krisbalintona/flymake-collection.git"
+                         :branch "vale-remove-unnecessary-splice" :lisp-dir
+                         "src/")
+     (outli :url "https://github.com/jdtsmith/outli")
+     (eglot-booster :url "https://github.com/jdtsmith/eglot-booster.git")
+     (oblique :url "https://github.com/zzkt/oblique-strategies.git")
      (oblique-strategies :url "https://github.com/zzkt/oblique-strategies.git")
      (clever-cite :vc-backend Git :url
                   "https://github.com/Hugo-Heagren/clever-cite.git")
@@ -79,9 +104,8 @@
      "/home/krisbalintona/Documents/org-database/agenda/"
      "/home/krisbalintona/Documents/org-database/notes/commonplace_book/"))
  '(safe-local-variable-values
-   '((org-review-delay . "+3d")
-     (eval add-hook 'before-save-hook 'org-make-toc) (org-use-tag-inheritance)
-     (org-use-tag-inheritance . t)
+   '((org-review-delay . "+3d") (eval add-hook 'before-save-hook 'org-make-toc)
+     (org-use-tag-inheritance) (org-use-tag-inheritance . t)
      (org-time-stamp-custom-formats "<%B %d, %Y>" . "<%B %d, %Y %H:%M>")
      (org-time-stamp-custom-formats quote ("<%B %d, %Y>" . "<%B %d, %Y %H:%M>"))
      (system-time-locale . "C")
@@ -94,4 +118,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(meow-position-highlight-number-1 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#0050af")))
+ '(meow-position-highlight-number-2 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#7f1f7f")))
+ '(meow-position-highlight-number-3 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#625a00")))
+ '(meow-region-cursor-1 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#0050af")))
+ '(meow-region-cursor-2 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#7f1f7f")))
+ '(meow-region-cursor-3 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#625a00"))))
