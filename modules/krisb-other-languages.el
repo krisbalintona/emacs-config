e90fa8062f69b105da5b37a3595d7ae06fc53929
Kristoffer Balintona <krisbalintona@gmail.com>
2025-04-22 18:03:35 -0500

    perf: Hyperland-ts-mode startup burden

--- modules/krisb-other-languages.el
+++ modules/krisb-other-languages.el
@@ -27,9 +27,7 @@
   ;;     M-x treesit-install-language-grammar RET hyprlang
   (with-eval-after-load 'treesit
     (add-to-list 'treesit-language-source-alist
-                 '(hyprlang "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang"))
-    (unless (treesit-grammar-location 'hyprlang)
-      (treesit-install-language-grammar 'hyprlang))))
+                 '(hyprlang "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang"))))
 
 ;;; Nix
 (use-package nix-ts-mode)
