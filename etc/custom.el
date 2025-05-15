12032da5c385f0a2d573978f1dea20837148c405
Kristoffer Balintona <krisbalintona@gmail.com>
2025-05-14 19:54:29 -0500

    custom.el: Update

--- etc/custom.el
+++ etc/custom.el
@@ -8,36 +8,39 @@
    '(abridge-diff ace-window activities adaptive-wrap agitate all-the-icons
                   apheleia artbollocks-mode astute async auctex-cont-latexmk
                   breadcrumb bufler cape cascading-dir-locals citar cl-generic
-                  cm-mode cond-star consult-xref-stack corfu-prescient cursory
-                  darkroom default-text-scale denote diminish dired-hist
-                  dumber-jump eat editorconfig eglot-booster
+                  cm-mode compile-multi compile-multi-nerd-icons cond-star
+                  consult-compile-multi consult-xref-stack corfu-prescient
+                  cursory darkroom default-text-scale denote diminish dired-hist
+                  diredfl dumber-jump eat editorconfig eglot-booster
                   eglot-signature-eldoc-talkative el-patch elisp-demos
                   embark-consult engine-mode erc eros eshell-atuin
                   eshell-syntax-highlighting eshell-z exec-path-from-shell
                   faceup fancy-compilation fish-mode flymake-collection fontaine
                   forge form-feed gcmh git-share god-mode hammy hide-mode-line
                   highlight-function-calls highlight-parentheses hl-todo hotfuzz
-                  hugoista hyprlang-ts-mode indexed info-colors inspector jinx
-                  keychain-environment kind-icon kv lazy-guard lice ligature lin
-                  lorem-ipsum lsp-bridge marginalia markdown-mode mermaid-mode
+                  hugoista hyprlang-ts-mode info-colors inspector jinx just-mode
+                  just-ts-mode keychain-environment kind-icon kv lazy-guard lice
+                  ligature lin lorem-ipsum lsp-bridge marginalia mermaid-mode
                   mixed-pitch mode-minder modus-themes nadvice
                   nerd-icons-completion nerd-icons-dired nerd-icons-ibuffer
                   nix-mode nix-ts-mode no-littering notmuch-addr
                   notmuch-transient nov ntlm ob-mermaid ol-notmuch olivetti on
                   orderless org-appear org-bookmark-heading org-bulletproof
-                  org-contrib org-edna org-make-toc org-mime org-modern org-node
-                  org-project-capture org-remark org-review org-roam-ql
-                  org-roam-ui org-web-tools outli ox-hugo package-build
-                  package-lint-flymake paren-face pcmpl-args pdf-tools peg
-                  persist-state powerthesaurus pulsar puni python rainbow-mode
-                  realgud recursion-indicator savefold saveplace-pdf-view
-                  scratch show-font smart-mark so-long soap-client
-                  stillness-mode sudo-edit suggest svg system-packages tmr tramp
+                  org-contrib org-edna org-make-toc org-mem org-mime org-ml
+                  org-mode org-modern org-node org-project-capture org-remark
+                  org-review org-roam-ql org-roam-ui org-web-tools outli ox-hugo
+                  package-build package-lint-flymake paren-face pcmpl-args
+                  pdf-tools peg persist-state powerthesaurus pulsar puni python
+                  rainbow-mode realgud recursion-indicator savefold
+                  saveplace-pdf-view scratch show-font smart-mark so-long
+                  soap-client stillness-mode sudo-edit suggest svg
+                  system-packages tempel tempel-collection tmr tramp
                   treesit-auto try typewriter-roll-mode ultra-scroll use-package
                   vc-jj verilog-mode vertico-prescient which-key window-tool-bar
                   wombag yasnippet))
  '(package-vc-selected-packages
-   '((vc-jj :url "https://codeberg.org/emacs-jj-vc/vc-jj.el.git")
+   '((org-mem :url "https://github.com/meedstrom/org-mem.git")
+     (vc-jj :url "https://codeberg.org/emacs-jj-vc/vc-jj.el.git")
      (indexed :url "https://github.com/meedstrom/indexed.git" :branch "dev")
      (lsp-bridge :vc-backend Git :url
                  "https://github.com/manateelazycat/lsp-bridge.git")
@@ -87,7 +90,9 @@
      (wombag :url "https://github.com/karthink/wombag.git")
      (denote-interface :url "git@github.com:krisbalintona/denote-interface.git")))
  '(safe-local-variable-directories
-   '("/home/krisbalintona/Documents/org-database/notes/blog/"
+   '("/home/krisbalintona/Documents/org-database/notes/manuscripts/academic/"
+     "/home/krisbalintona/nix-config/"
+     "/home/krisbalintona/Documents/org-database/notes/blog/"
      "/home/krisbalintona/emacs-repos/other/melpa/"
      "/home/krisbalintona/Documents/org-database/notes/main/"
      "/home/krisbalintona/Documents/org-database/notes/commonplace_book/buoy/"
@@ -102,16 +107,19 @@
      "/home/krisbalintona/Documents/org-database/agenda/"
      "/home/krisbalintona/Documents/org-database/notes/commonplace_book/"))
  '(safe-local-variable-values
-   '((eval add-hook 'before-save-hook 'org-make-toc nil t)
+   '((eval setq-local org-archive-location
+           (expand-file-name "archive/slip_box.org_archive::* From %R"
+                             org-directory))
+     (eval add-hook 'before-save-hook 'org-make-toc nil t)
      (org-review-delay . "+3d") (eval add-hook 'before-save-hook 'org-make-toc)
      (org-use-tag-inheritance) (org-use-tag-inheritance . t)
      (org-time-stamp-custom-formats "<%B %d, %Y>" . "<%B %d, %Y %H:%M>")
      (org-time-stamp-custom-formats quote ("<%B %d, %Y>" . "<%B %d, %Y %H:%M>"))
      (system-time-locale . "C")
      (eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created 99 t)))
- '(tmr-description-list '("Stop working!" "Work time 😄") nil nil "Customized with use-package tmr")
  '(warning-suppress-types
-   '((files missing-lexbind-cookie
+   '((org-element org-element-cache) (org-element org-element-parser)
+     (files missing-lexbind-cookie
             "/home/krisbalintona/.emacs.d/var/prescient-save.el")
      (org-element org-element-parser) (org-element org-element-parser))))
 (custom-set-faces
@@ -119,9 +127,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(meow-position-highlight-number-1 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#0050af")))
- '(meow-position-highlight-number-2 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#7f1f7f")))
- '(meow-position-highlight-number-3 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#625a00")))
- '(meow-region-cursor-1 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#0050af")))
- '(meow-region-cursor-2 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#7f1f7f")))
- '(meow-region-cursor-3 ((((class color) (min-colors 256)) :inherit (bold modus-themes-reset-soft) :background "#625a00"))))
+ )
