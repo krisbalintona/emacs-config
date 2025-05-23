cf653437c97a9653f2b11a9f804d4b4078759fec
Kristoffer Balintona <krisbalintona@gmail.com>
2025-05-23 15:10:35 -0500

    custom.el: Update

--- etc/custom.el
+++ etc/custom.el
@@ -4,12 +4,40 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0239ee274eb680c85abc45025966b1fa4a8e8420ee1f5cc0dedaae3bf7175ad7"
+     "d6b369a3f09f34cdbaed93eeefcc6a0e05e135d187252e01b0031559b1671e97"
+     "1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac"
+     "541282f66e5cc83918994002667d2268f0a563205117860e71b7cb823c1a11e9"
+     "3d9938bbef24ecee9f2632cb25339bf2312d062b398f0dfb99b918f8f11e11b1"
+     "ac893acecb0f1cf2b6ccea5c70ea97516c13c2b80c07f3292c21d6eb0cb45239"
+     "df39cc8ecf022613fc2515bccde55df40cb604d7568cb96cd7fe1eff806b863b"
+     "ea4dd126d72d30805c083421a50544e235176d9698c8c541b824b60912275ba1"
+     "cd5f8f91cc2560c017cc9ec24a9ab637451e36afd22e00a03e08d7b1b87c29ca"
+     "00d7122017db83578ef6fba39c131efdcb59910f0fac0defbe726da8072a0729"
+     "bf4d25079f7d052cb656e099d9c2af9fb61ee377e8e72b7f13cecf8dffb74f92"
+     "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19"
+     "5c8a1b64431e03387348270f50470f64e28dfae0084d33108c33a81c1e126ad6"
+     "2ab8cb6d21d3aa5b821fa638c118892049796d693d1e6cd88cb0d3d7c3ed07fc"
+     "d12b1d9b0498280f60e5ec92e5ecec4b5db5370d05e787bc7cc49eae6fb07bc0"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+     "257fe6a129ee0fb737b4d7e1e15956a2ff4a48e32398e2cd6a8b8306afac7d07"
+     default))
  '(package-selected-packages
    '(arei bui edit-indirect ef-themes el-patch fontaine geiser-guile jinx
 	  magit-popup no-littering on tablist))
  '(safe-local-variable-directories
-   '("/home/krisbalintona/nix-config/"
-     "/home/krisbalintona/.config/guix/current/share/guile/site/3.0/")))
+   '("/home/krisbalintona/emacs-repos/configs/doom-emacs/"
+     "/home/krisbalintona/Documents/org-database/agenda/"
+     "/home/krisbalintona/Documents/org-database/notes/commonplace_book/"
+     "/home/krisbalintona/Documents/org-database/notes/manuscripts/blog/"
+     "/home/krisbalintona/.config/guix/current/share/guile/site/3.0/"
+     "/home/krisbalintona/nix-config/"))
+ '(safe-local-variable-values
+   '((eval add-hook 'org-insert-heading-hook 'org-expiry-insert-created
+	   99 t)
+     (org-use-tag-inheritance)))
+ '(xref-show-definitions-function 'xref-show-definitions-completing-read nil nil "Customized with use-package xref"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
