0ba6fc472c6a514605fd7567107921229ab8dea5
Kristoffer Balintona <krisbalintona@gmail.com>
2025-05-22 19:34:56 -0500

    Configure xref

--- init.el
+++ init.el
@@ -1394,6 +1394,54 @@ https://github.com/minad/corfu?tab=readme-ov-file#transfer-completion-to-the-min
   :config
   (add-to-list 'mode-line-collapse-minor-modes 'mixed-pitch-mode))
 
+
+;;;; Xref
+;; TODO 2025-05-22: Document:
+;; - `xref-file-name-display’
+(use-package xref
+  :ensure nil
+  :bind
+  ("C-M-?". xref-find-references-and-replace) ; Emacs 29.1
+  :custom
+  ;; TODO 2025-05-22: Revisit this.
+  ;; (xref-show-definitions-function 'xref-show-definitions-completing-read)
+  ;; (xref-show-xrefs-function 'xref-show-definitions-buffer)
+  (xref-search-program (if (executable-find "rg") 'ripgrep 'grep))
+  (xref-history-storage 'xref-window-local-history) ; Per-window history of `xref-go-*'
+  :config
+  ;; We remove the fallback backend, `etags--xref-backend', which prompts the
+  ;; user for an etags table -- this is undesirable for me.
+  (setq-default xref-backend-functions nil)
+  ;; Then add `elisp--xref-backend' as the global value of
+  ;; `xref-backend-functions', which means it is run when the local
+  ;; value ends with `t'. See (info "(elisp) Running Hooks") for an
+  ;; explanation.
+  (add-hook 'xref-backend-functions #'elisp--xref-backend)
+
+  ;; TODO 2025-05-22: Revisit this.
+  ;; ;; Revealing headings
+  ;;   (with-eval-after-load 'krisb-reveal
+  ;;     (defun krisb-reveal-xref-find-information ()
+  ;;       "Return information required by `krisb-reveal-fold-commands'.
+  ;; See the docstring of `krisb-reveal-fold-commands'."
+  ;;       (save-window-excursion
+  ;;         (save-excursion
+  ;;           (xref-goto-xref)
+  ;;           (cons (point) (current-buffer)))))
+  ;;     ;; I could also advise the following commands to call
+  ;;     ;; `xref-show-location-at-point' afterwards.  Though such a solution is
+  ;;     ;; applicable only to xref.  I wanted similar functionality for non-xref
+  ;;     ;; buffers, so I wrote krisb-reveal, and to remain idiomatic with my usage
+  ;;     ;; of it, I also do it here.
+  ;;     (dolist (command '(xref-prev-line
+  ;;                        xref-next-line
+  ;;                        xref-quit-and-goto-xref))
+  ;;       (add-to-list 'krisb-reveal-fold-commands
+  ;;                    (list :command command
+  ;;                          :location #'krisb-reveal-xref-find-information)))
+  ;;     (add-hook 'xref-after-jump-hook #'krisb-reveal-fold))
+  )
+
 ;;; Fluff
 
 ;;;; Recursion-indicator
--- modules/krisb-projects.el
+++ modules/krisb-projects.el
@@ -1,45 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
 ;;; Goto definition
-;;;; Xref
-(use-package xref
-  :bind ("C-M-?". xref-find-references-and-replace) ; Emacs 29.1
-  :custom
-  (xref-show-definitions-function #'xref-show-definitions-completing-read)
-  (xref-show-xrefs-function #'xref-show-definitions-buffer)
-  (xref-file-name-display 'project-relative)
-  (xref-search-program 'ripgrep)
-  (xref-history-storage 'xref-window-local-history) ; Per-window history of `xref-go-*'
-  :config
-  ;; We remove the fallback backend, `etags--xref-backend', which prompts the
-  ;; user for an etags table -- this is undesirable for me.
-  (setq-default xref-backend-functions nil)
-  ;; Then add `elisp--xref-backend' as the global value of
-  ;; `xref-backend-functions', which means it is run when the local value ends
-  ;; with `t'. See (info "(elisp) Running Hooks") for an explanation.
-  (add-hook 'xref-backend-functions #'elisp--xref-backend)
-
-  ;; Revealing headings
-  (with-eval-after-load 'krisb-reveal
-    (defun krisb-reveal-xref-find-information ()
-      "Return information required by `krisb-reveal-fold-commands'.
-See the docstring of `krisb-reveal-fold-commands'."
-      (save-window-excursion
-        (save-excursion
-          (xref-goto-xref)
-          (cons (point) (current-buffer)))))
-    ;; I could also advise the following commands to call
-    ;; `xref-show-location-at-point' afterwards.  Though such a solution is
-    ;; applicable only to xref.  I wanted similar functionality for non-xref
-    ;; buffers, so I wrote krisb-reveal, and to remain idiomatic with my usage
-    ;; of it, I also do it here.
-    (dolist (command '(xref-prev-line
-                       xref-next-line
-                       xref-quit-and-goto-xref))
-      (add-to-list 'krisb-reveal-fold-commands
-                   (list :command command
-                         :location #'krisb-reveal-xref-find-information)))
-    (add-hook 'xref-after-jump-hook #'krisb-reveal-fold)))
 
 ;;;; Consult-xref-stack
 (use-package consult-xref-stack
