0e1efbf8756c1aa03751b0a9235a886d8aa00704
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-04 21:13:35 -0500

    Configure `flymake-collection'

--- init.el
+++ init.el
@@ -4073,6 +4073,67 @@ The context buffer is the buffer that shows annotation contents in
      "twas"
      "tis")))
 
+;;;; Flymake-collection
+(use-package flymake-collection
+  ;; For AUR:
+  ;; :ensure-system-package vale
+  :ensure t
+  :after flymake
+  :demand t
+  :custom
+  (flymake-collection-hook-inherit-config t)
+  (flymake-collection-hook-ignore-modes nil)
+  (flymake-collection-vale-extension-function 'krisb-flymake-collection-vale-extension-function)
+  :config
+  ;; NOTE 2024-10-05: Set `flymake-collection-hook-config' immediately
+  ;; when the package loads, so the first invocation of
+  ;; `flymake-collection-hook-setup' uses my configured value.
+  (setf (alist-get 'org-mode flymake-collection-hook-config)
+        ;; NOTE 2024-10-05: I configure vale to use proselint to my
+        ;; liking, so I disable the proselint checker. One reason that
+        ;; motivates this decision is vale's performance compared to
+        ;; proselint (see
+        ;; https://github.com/errata-ai/vale?tab=readme-ov-file#benchmarks).
+        '((flymake-collection-vale
+           :depth -20)
+          (flymake-collection-proselint
+           :depth -1
+           :disabled t))
+        (alist-get 'markdown-mode flymake-collection-hook-config)
+        '((flymake-collection-markdownlint
+           :depth -50)
+          (flymake-collection-vale
+           :depth -20)
+          (flymake-collection-proselint
+           :disabled t
+           :depth -1))
+        (alist-get 'notmuch-message-mode flymake-collection-hook-config)
+        '((flymake-collection-vale
+           :depth -20)
+          (flymake-collection-proselint
+           :depth -1
+           :disabled t)))
+
+  (flymake-collection-hook-setup)
+
+  (defun krisb-flymake-collection-vale-extension-function (buffer)
+    "My own function for `flymake-collection-vale-extension-function'.
+Behaves like `flymake-collection-vale-default-extension-function' but
+with the following exceptions:
+- In org-src buffers, use the extension of the source buffer.
+- In org buffers without an associated file, return the \"org\" extension."
+    (let* ((file-name (buffer-file-name buffer))
+           (extension (and file-name (file-name-extension file-name))))
+      (cond
+       (extension
+        extension)
+       ((org-src-edit-buffer-p buffer)
+        (file-name-extension
+         (buffer-file-name
+          (org-src-source-buffer))))
+       ((equal 'org-mode (buffer-local-value 'major-mode buffer))
+        "org")))))
+
 ;;; Blogging
 (require 'krisb-org-publish)
 
