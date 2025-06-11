f578be1257c0e995f73df7855fa3334228ebbdda
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-11 18:10:33 -0500

    Bind consult-mark

--- init.el
+++ init.el
@@ -1377,6 +1377,7 @@ Taken from https://karthinks.com/software/avy-can-do-anything/."
    ("e" . consult-compile-error)
    ("l" . consult-line)
    ("a" . consult-org-agenda)
+   ("m" . consult-mark)
    :map search-map                ; The `M-s' prefix
    ("i" . consult-info)
    ("g" . consult-git-grep)
