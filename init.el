144148893616e0c1ed1cc45c0c6af55ad6cec683
Kristoffer Balintona <krisbalintona@gmail.com>
2025-10-27 12:31:42 -0500

    notmuch: Avoid loading sendmail on startup

--- init.el
+++ init.el
@@ -2764,9 +2764,10 @@ send from."
   ;; Use Lieer to send emails.  Also see
   ;; `krisb-notmuch-set-sendmail-args'.  Read
   ;; https://github.com/gauteh/lieer/wiki/Emacs-and-Lieer.
-  (setopt sendmail-program (if (executable-find "gmi") "gmi" "sendmail")
-          send-mail-function 'sendmail-send-it
-          notmuch-fcc-dirs nil) ; Gmail already copies sent emails, so don't move them elsewhere locally
+  (with-eval-after-load 'sendmail
+    (setopt sendmail-program (if (executable-find "gmi") "gmi" "sendmail")
+            send-mail-function 'sendmail-send-it))
+  (setopt notmuch-fcc-dirs nil) ; Gmail already copies sent emails, so don't move them elsewhere locally
 
   ;; TODO 2025-05-23: Revisit this.
   ;; (krisb-modus-themes-setup-faces
