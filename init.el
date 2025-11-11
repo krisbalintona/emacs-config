8ded86ebbc910f4250a8abcd5a2bbca50a18a721
Kristoffer Balintona <krisbalintona@gmail.com>
2025-11-10 21:13:58 -0600

    xref: Transfer

--- init.el
+++ init.el
@@ -3777,6 +3777,71 @@ instead."
               ("C-." . nil)
               ("C-M-i" . nil)))
 
+;;; Xref
+;; TODO 2025-05-22: Document:
+;; - `xref-file-name-display’
+(setup xref
+  
+  (bind-keys ("C-M-?". xref-find-references-and-replace)) ; Emacs 29.1
+  
+  (with-eval-after-load 'xref
+    (setopt xref-search-program (if (executable-find "rg") 'ripgrep 'grep)
+            xref-history-storage 'xref-window-local-history)) ; Per-window history of `xref-go-*'
+  
+  (with-eval-after-load 'xref
+    ;; We remove the default sole fallback backend, `etags--xref-backend',
+    ;; which prompts the user for an etags table -- this is undesirable
+    ;; for me, since I don't use etags.
+    (remove-hook 'xref-backend-functions #'etags--xref-backend)
+    ;; Then add `elisp--xref-backend' as the global value of
+    ;; `xref-backend-functions', which means it is run when the local
+    ;; value ends with `t'.  (See (info "(elisp) Running Hooks") for an
+    ;; explanation of why.)
+    ;; 
+    ;; Effectively, when combined with the above, this becomes the (only)
+    ;; backend function that is checked after all the buffer-local backend
+    ;; functions are checked.
+    (add-hook 'xref-backend-functions #'elisp--xref-backend))
+
+  (with-eval-after-load 'xref
+    ;; TODO 2025-05-22: Revisit this.
+    ;; (setopt xref-show-definitions-function 'xref-show-definitions-completing-read
+    ;;         xref-show-xrefs-function 'xref-show-definitions-buffer)
+    (add-to-list 'display-buffer-alist
+                 `((or (major-mode . xref--xref-buffer-mode)
+                       (,(rx (literal xref-buffer-name))))
+                   (display-buffer-below-selected display-buffer-at-bottom)
+                   (window-height . 0.25)))
+    (add-to-list 'display-buffer-alist
+                 '(((category . xref)
+                    (display-buffer-reuse-window display-buffer-use-some-window)
+                    (some-window . mru))))))
+
+(setup xref
+  ;; TODO 2025-05-22: Revisit this.
+  ;; ;; Revealing headings
+  ;;   (with-eval-after-load 'krisb-reveal
+  ;;     (defun krisb-reveal-xref-find-information ()
+  ;;       "Return information required by `krisb-reveal-fold-commands'.
+  ;; See the docstring of `krisb-reveal-fold-commands'."
+  ;;       (save-window-excursion
+  ;;         (save-excursion
+  ;;           (xref-goto-xref)
+  ;;           (cons (point) (current-buffer)))))
+  ;;     ;; I could also advise the following commands to call
+  ;;     ;; `xref-show-location-at-point' afterwards.  Though such a solution is
+  ;;     ;; applicable only to xref.  I wanted similar functionality for non-xref
+  ;;     ;; buffers, so I wrote krisb-reveal, and to remain idiomatic with my usage
+  ;;     ;; of it, I also do it here.
+  ;;     (dolist (command '(xref-prev-line
+  ;;                        xref-next-line
+  ;;                        xref-quit-and-goto-xref))
+  ;;       (add-to-list 'krisb-reveal-fold-commands
+  ;;                    (list :command command
+  ;;                          :location #'krisb-reveal-xref-find-information)))
+  ;;     (add-hook 'xref-after-jump-hook #'krisb-reveal-fold))
+  )
+
 ;;; Startup time
 ;; Message for total init time after startup
 (defun krisb-startup-time ()
