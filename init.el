b7f9d91766080132a2a9f9d369c1694684fc04c4
Kristoffer Balintona <krisbalintona@gmail.com>
2026-04-09 16:55:48 -0500

    orderless: Add the flex matching style

--- init.el
+++ init.el
@@ -962,8 +962,8 @@ ending the minibuffer session. (This is equivalent to the behavior of
               orderless-prefixes
               orderless-initialism
               orderless-literal
+              orderless-flex
               ;; orderless-literal-prefix
-              ;; orderless-flex
               )))
   ;; Our orderless components are split by spaces (see
   ;; `orderless-component-separator') so, for convenience, make SPC
